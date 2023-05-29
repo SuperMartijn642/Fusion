aa9a60cff4b8c2853bc1877ea2607dc522db9c90
SuperMartijn624 <martijnvandenbrand@outlook.com>
2023-05-29 15:54:48 +0200

    Fix DatagenModLoaderMixin

--- src/main/java/com/supermartijn642/fusion/mixin/DatagenModLoaderMixin.java
+++ src/main/java/com/supermartijn642/fusion/mixin/DatagenModLoaderMixin.java
@@ -7,6 +7,7 @@ import net.minecraftforge.data.loading.DatagenModLoader;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 /**
  * Created 21/05/2023 by SuperMartijn642
@@ -18,11 +19,11 @@ public class DatagenModLoaderMixin {
         method = "begin",
         at = @At(
             value = "INVOKE",
-            target = "Lnet/minecraftforge/data/event/GatherDataEvent$DataGeneratorConfig;runAll()V",
+            target = "Lnet/minecraftforge/forge/event/lifecycle/GatherDataEvent$DataGeneratorConfig;runAll()V",
             shift = At.Shift.BEFORE
         )
     )
-    private static void begin(){
+    private static void begin(CallbackInfo ci){
         TextureTypeRegistryImpl.finalizeRegistration();
         ModelTypeRegistryImpl.finalizeRegistration();
         PredicateRegistryImpl.finalizeRegistration();

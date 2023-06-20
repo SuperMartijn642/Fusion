86b77f58c7c0f3069b3ae74922a765921ea498d9
SuperMartijn624 <martijnvandenbrand@outlook.com>
2023-06-20 15:39:18 +0200

    Allocate dummy image to prevent crash with Iris https://github.com/SuperMartijn642/Rechiseled/issues/35

--- src/main/java/com/supermartijn642/fusion/texture/types/connecting/ConnectingTextureSprite.java
+++ src/main/java/com/supermartijn642/fusion/texture/types/connecting/ConnectingTextureSprite.java
@@ -1,6 +1,6 @@
 package com.supermartijn642.fusion.texture.types.connecting;
 
-import com.mojang.blaze3d.platform.NativeImage;
+import com.supermartijn642.fusion.FusionClient;
 import com.supermartijn642.fusion.api.texture.data.ConnectingTextureLayout;
 import net.minecraft.client.renderer.texture.TextureAtlasSprite;
 import net.minecraft.client.resources.metadata.animation.AnimationMetadataSection;
@@ -14,14 +14,14 @@ public class ConnectingTextureSprite extends TextureAtlasSprite {
 
     protected ConnectingTextureSprite(TextureAtlasSprite original, ConnectingTextureLayout layout){
         super(
-            original.atlas(),
-            new Info(original.getName(), original.getWidth(), original.getHeight(), AnimationMetadataSection.EMPTY),
-            0,
-            1,
-            1,
-            original.getX(),
-            original.getY(),
-            new NativeImage(NativeImage.Format.RGBA, 1, 1, true, 0)
+                original.atlas(),
+                new Info(original.getName(), original.getWidth(), original.getHeight(), AnimationMetadataSection.EMPTY),
+                0,
+                1,
+                1,
+                original.getX(),
+                original.getY(),
+                FusionClient.getDummyImage()
         );
         this.layout = layout;
         this.mainImage = original.mainImage;

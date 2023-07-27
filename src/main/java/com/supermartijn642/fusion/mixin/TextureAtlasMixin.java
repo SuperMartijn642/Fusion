816682ea13cdcdc5ca7be53214445e5b9a3125cb
SuperMartijn624 <martijnvandenbrand@outlook.com>
2023-07-27 14:12:18 +0200

    Add lock when writing metadata to map in TextureAtlasMixin

--- src/main/java/com/supermartijn642/fusion/mixin/TextureAtlasMixin.java
+++ src/main/java/com/supermartijn642/fusion/mixin/TextureAtlasMixin.java
@@ -50,7 +50,9 @@ public class TextureAtlasMixin {
         // Get the fusion metadata
         Pair<TextureType<Object>,Object> metadata = resource.getMetadata(FusionMetadataSection.INSTANCE);
         if(metadata != null){
-            this.fusionTextureMetadata.put(info.name(), metadata);
+            synchronized(this.fusionTextureMetadata){
+                this.fusionTextureMetadata.put(info.name(), metadata);
+            }
             // Adjust the frame size
             Pair<Integer,Integer> newSize;
             try{

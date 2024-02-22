0a843c3a02603a6c98b712b52dd140a42e1687a9
SuperMartijn624 <martijnvandenbrand@outlook.com>
2024-02-22 18:06:51 +0100

    Ignore `FileNotFoundException` when looking for pack.mcmeta files #37

--- src/main/java/com/supermartijn642/fusion/mixin/PackMixin.java
+++ src/main/java/com/supermartijn642/fusion/mixin/PackMixin.java
@@ -15,6 +15,7 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
+import java.io.FileNotFoundException;
 import java.util.function.Supplier;
 
 /**
@@ -33,6 +34,8 @@ public class PackMixin {
     private void init(String identifier, boolean required, Supplier<IResourcePack> resourcesSupplier, ITextComponent title, ITextComponent description, PackCompatibility compatibility, ResourcePackInfo.Priority position, boolean fixedPosition, IPackNameDecorator packSource, boolean hidden, CallbackInfo ci){
         try(IResourcePack resources = resourcesSupplier.get()){
             this.overridesFolder = resources.getMetadataSection(FusionPackMetadataSection.INSTANCE);
+        }catch(FileNotFoundException ignore){
+            // Ignore resource packs which don't have a pack.mcmeta file
         }catch(Exception e){
             FusionClient.LOGGER.error("Encountered an exception whilst reading fusion metadata for pack '" + identifier + "':", e);
         }

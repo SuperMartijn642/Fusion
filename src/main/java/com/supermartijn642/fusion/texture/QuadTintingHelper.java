522d3b896a12579d8caef72b3cd35a137ef5b993
SuperMartijn624 <martijnvandenbrand@outlook.com>
2024-09-08 21:10:34 +0200

    Add grass, foliage, and water texture tinting options

--- src/main/java/com/supermartijn642/fusion/texture/QuadTintingHelper.java
+++ src/main/java/com/supermartijn642/fusion/texture/QuadTintingHelper.java
@@ -4,7 +4,10 @@ import com.supermartijn642.fusion.api.texture.data.BaseTextureData;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.client.renderer.color.IBlockColor;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.ColorizerFoliage;
+import net.minecraft.world.ColorizerGrass;
 import net.minecraft.world.IBlockAccess;
+import net.minecraft.world.biome.BiomeColorHelper;
 
 /**
  * Created 07/09/2024 by SuperMartijn642
@@ -15,7 +18,21 @@ public class QuadTintingHelper {
      * @see com.supermartijn642.fusion.api.texture.data.BaseTextureData.QuadTinting
      */
     private static final IBlockColor[] TINT_FUNCTIONS = new IBlockColor[]{
-        // TODO
+        (state, level, pos, tintIndex) -> {
+            if(level == null || pos == null)
+                return ColorizerGrass.getGrassColor(0.5, 1.0);
+            return BiomeColorHelper.getGrassColorAtPos(level, pos);
+        },
+        (state, level, pos, tintIndex) -> {
+            if(level == null || pos == null)
+                return ColorizerFoliage.getFoliageColorBasic();
+            return BiomeColorHelper.getFoliageColorAtPos(level, pos);
+        },
+        (state, level, pos, tintIndex) -> {
+            if(level == null || pos == null)
+                return -1;
+            return BiomeColorHelper.getWaterColorAtPos(level, pos);
+        }
     };
 
     static{

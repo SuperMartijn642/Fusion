17e2ecfb5594efcbc7d625328d54a1aab432afd1
SuperMartijn624 <martijnvandenbrand@outlook.com>
2024-09-12 17:12:31 +0200

    Make sure to use the block state argument given in `BakedModel#getQuads`

--- src/main/java/com/supermartijn642/fusion/model/types/connecting/ConnectingBakedModel.java
+++ src/main/java/com/supermartijn642/fusion/model/types/connecting/ConnectingBakedModel.java
@@ -331,6 +331,9 @@ public class ConnectingBakedModel implements IBakedModel {
         // If the block cache is absent, the connected textures cannot be updated, so just push the mesh
         if(blockCache == null)
             return quads.stream().map(q -> q.bakedQuad).collect(Collectors.toList());
+        // Make sure to use the block state argument for the model's own block
+        if(state != null)
+            blockCache.setSelf(state);
 
         // Only compute connections for each predicate once
         TextureConnections[] connectionsCache = new TextureConnections[this.predicates.size()];

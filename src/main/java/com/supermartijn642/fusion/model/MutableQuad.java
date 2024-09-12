8ad933894543d7f232974c81961e872207f6828d
SuperMartijn624 <martijnvandenbrand@outlook.com>
2024-09-12 16:12:38 +0200

    Move `MutableQuad` and add `OrientedMutableQuad` similar to the NeoForge branch
    Signed-off-by: SuperMartijn624 <martijnvandenbrand@outlook.com>

--- src/main/java/com/supermartijn642/fusion/model/MutableQuad.java
+++ src/main/java/com/supermartijn642/fusion/model/MutableQuad.java
@@ -1,4 +1,4 @@
-package com.supermartijn642.fusion.model.types.connecting;
+package com.supermartijn642.fusion.model;
 
 import net.fabricmc.fabric.api.renderer.v1.mesh.MutableQuadView;
 import org.joml.Vector3f;
@@ -9,39 +9,37 @@ import org.joml.Vector3f;
 public class MutableQuad {
 
     private MutableQuadView quadView = null;
-    private int[] permutation = null;
 
-    public void set(MutableQuadView quadView, int[] permutation){
+    public void set(MutableQuadView quadView){
         this.quadView = quadView;
-        this.permutation = permutation;
     }
 
     public float u(int vertexIndex){
-        return this.quadView.u(this.permutation[vertexIndex]);
+        return this.quadView.u(vertexIndex);
     }
 
     public float v(int vertexIndex){
-        return this.quadView.v(this.permutation[vertexIndex]);
+        return this.quadView.v(vertexIndex);
     }
 
     public void uv(int vertexIndex, float u, float v){
-        this.quadView.uv(this.permutation[vertexIndex], u, v);
+        this.quadView.uv(vertexIndex, u, v);
     }
 
     public float x(int vertexIndex){
-        return this.quadView.x(this.permutation[vertexIndex]);
+        return this.quadView.x(vertexIndex);
     }
 
     public float y(int vertexIndex){
-        return this.quadView.y(this.permutation[vertexIndex]);
+        return this.quadView.y(vertexIndex);
     }
 
     public float z(int vertexIndex){
-        return this.quadView.z(this.permutation[vertexIndex]);
+        return this.quadView.z(vertexIndex);
     }
 
     public void pos(int vertexIndex, float x, float y, float z){
-        this.quadView.pos(this.permutation[vertexIndex], x, y, z);
+        this.quadView.pos(vertexIndex, x, y, z);
     }
 
     public Vector3f faceNormal(){

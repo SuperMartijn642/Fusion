28ebf519142bc72d855891ec2da262a746f0055f
SuperMartijn624 <martijnvandenbrand@outlook.com>
2024-09-12 17:12:31 +0200

    Make sure to use the block state argument given in `BakedModel#getQuads`

--- src/main/java/com/supermartijn642/fusion/model/types/connecting/SurroundingBlockCache.java
+++ src/main/java/com/supermartijn642/fusion/model/types/connecting/SurroundingBlockCache.java
@@ -30,6 +30,10 @@ public class SurroundingBlockCache {
         }
     }
 
+    public void setSelf(BlockState self){
+        this.states[13] = self;
+    }
+
     public ILightReader getLevel(){
         return this.level;
     }

c3ef447675c418f3c6d77751ef82d502441e66c9
SuperMartijn624 <martijnvandenbrand@outlook.com>
2024-09-12 15:49:58 +0200

    Avoid using streams in `BaseBakedModel` and `ConnectingBakedModel`

--- src/main/java/com/supermartijn642/fusion/model/types/base/BaseBakedModel.java
+++ src/main/java/com/supermartijn642/fusion/model/types/base/BaseBakedModel.java
@@ -22,7 +22,6 @@ import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 import java.util.*;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 /**
  * Created 06/09/2024 by SuperMartijn642
@@ -107,7 +106,10 @@ public class BaseBakedModel implements IBakedModel {
             if(additionalQuads != null){
                 if(quads == null)
                     quads = additionalQuads;
-                quads = Stream.concat(quads.stream(), additionalQuads.stream()).collect(Collectors.toList());
+                List<BakedQuad> combined = new ArrayList<>(quads.size() + additionalQuads.size());
+                combined.addAll(quads);
+                combined.addAll(additionalQuads);
+                quads = combined;
             }
         }
         return quads == null ? Collections.emptyList() : quads;

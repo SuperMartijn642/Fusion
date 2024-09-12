16bfaedbfe304caa30684c304140570a787783d0
SuperMartijn624 <martijnvandenbrand@outlook.com>
2024-09-12 14:31:49 +0200

    Add base model type and base texture type, and redo `ConnectingBakedModel`

--- src/main/java/com/supermartijn642/fusion/mixin/forge/DatagenModLoaderMixin.java
+++ src/main/java/com/supermartijn642/fusion/mixin/forge/DatagenModLoaderMixin.java
@@ -1,4 +1,4 @@
-package com.supermartijn642.fusion.mixin;
+package com.supermartijn642.fusion.mixin.forge;
 
 import com.supermartijn642.fusion.model.ModelTypeRegistryImpl;
 import com.supermartijn642.fusion.predicate.PredicateRegistryImpl;

2cad00aa2873c4d2846b671997fea4c1042d41ad
SuperMartijn624 <martijnvandenbrand@outlook.com>
2024-09-15 17:34:36 +0200

    Properly gather texture dependencies for connecting and base model types

--- src/main/java/com/supermartijn642/fusion/model/SpriteIdentifierImpl.java
+++ src/main/java/com/supermartijn642/fusion/model/SpriteIdentifierImpl.java
@@ -1,13 +1,17 @@
 package com.supermartijn642.fusion.model;
 
 import com.supermartijn642.fusion.api.model.SpriteIdentifier;
+import com.supermartijn642.fusion.util.TextureAtlases;
 import net.minecraft.client.renderer.model.RenderMaterial;
+import net.minecraft.client.renderer.texture.MissingTextureSprite;
 import net.minecraft.util.ResourceLocation;
 
 /**
  * Created 30/04/2023 by SuperMartijn642
  */
 public class SpriteIdentifierImpl implements SpriteIdentifier {
+
+    public static final SpriteIdentifier MISSING = SpriteIdentifier.of(TextureAtlases.getBlocks(), MissingTextureSprite.getLocation());
 
     private final ResourceLocation atlas, texture;
     private RenderMaterial material;
@@ -35,5 +39,21 @@ public class SpriteIdentifierImpl implements SpriteIdentifier {
     @Override
     public RenderMaterial toMaterial(){
         return this.material == null ? (this.material = SpriteIdentifier.super.toMaterial()) : this.material;
+    }
+
+    @Override
+    public final boolean equals(Object o){
+        if(this == o) return true;
+        if(!(o instanceof SpriteIdentifierImpl)) return false;
+
+        SpriteIdentifierImpl that = (SpriteIdentifierImpl)o;
+        return this.atlas.equals(that.atlas) && this.texture.equals(that.texture);
+    }
+
+    @Override
+    public int hashCode(){
+        int result = this.atlas.hashCode();
+        result = 31 * result + this.texture.hashCode();
+        return result;
     }
 }

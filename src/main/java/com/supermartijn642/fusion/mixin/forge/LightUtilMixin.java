81c894ec8d1e982ac8596c3166d631047f811d91
SuperMartijn624 <martijnvandenbrand@outlook.com>
2024-09-16 16:28:19 +0200

    Add quad tinting for items and patch more quad tinting

---
+++ src/main/java/com/supermartijn642/fusion/mixin/forge/LightUtilMixin.java
@@ -0,0 +1,41 @@
+package com.supermartijn642.fusion.mixin.forge;
+
+import com.supermartijn642.fusion.api.texture.data.BaseTextureData;
+import com.supermartijn642.fusion.extensions.VertexLighterFlatExtension;
+import com.supermartijn642.fusion.texture.types.base.BaseTextureSprite;
+import net.minecraft.client.renderer.block.model.BakedQuad;
+import net.minecraft.client.renderer.texture.TextureAtlasSprite;
+import net.minecraftforge.client.model.pipeline.IVertexConsumer;
+import net.minecraftforge.client.model.pipeline.LightUtil;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+/**
+ * Created 14/09/2024 by SuperMartijn642
+ */
+@Mixin(LightUtil.class)
+public class LightUtilMixin {
+
+    @Inject(
+        method = "putBakedQuad",
+        at = @At(
+            value = "INVOKE",
+            target = "Lnet/minecraftforge/client/model/pipeline/IVertexConsumer;setQuadTint(I)V",
+            remap = false
+        ),
+        remap = false
+    )
+    private static void putBakedQuad(IVertexConsumer consumer, BakedQuad quad, CallbackInfo ci){
+        // In case texture has a custom tinting set, mark the vertex consumer
+        if(quad.tintIndex == 39216){
+            TextureAtlasSprite sprite = quad.getSprite();
+            if(sprite instanceof BaseTextureSprite){
+                BaseTextureData.QuadTinting tinting = ((BaseTextureSprite)sprite).data().getTinting();
+                if(tinting != null && consumer instanceof VertexLighterFlatExtension)
+                    ((VertexLighterFlatExtension)consumer).setFusionCustomTinting(tinting);
+            }
+        }
+    }
+}
---
+++ src/main/java/com/supermartijn642/fusion/mixin/forge/VertexLighterFlatMixin.java
@@ -0,0 +1,58 @@
+package com.supermartijn642.fusion.mixin.forge;
+
+import com.supermartijn642.fusion.api.texture.data.BaseTextureData;
+import com.supermartijn642.fusion.extensions.VertexLighterFlatExtension;
+import com.supermartijn642.fusion.texture.QuadTintingHelper;
+import net.minecraftforge.client.model.pipeline.BlockInfo;
+import net.minecraftforge.client.model.pipeline.VertexLighterFlat;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+/**
+ * Created 14/09/2024 by SuperMartijn642
+ */
+@Mixin(VertexLighterFlat.class)
+public class VertexLighterFlatMixin implements VertexLighterFlatExtension {
+
+    @Unique
+    private BaseTextureData.QuadTinting tinting;
+    @Final
+    @Shadow(remap = false)
+    private BlockInfo blockInfo;
+
+    @Override
+    public void setFusionCustomTinting(BaseTextureData.QuadTinting tinting){
+        this.tinting = tinting;
+    }
+
+    @ModifyVariable(
+        method = "processQuad",
+        at = @At(
+            value = "INVOKE_ASSIGN",
+            target = "Lnet/minecraftforge/client/model/pipeline/BlockInfo;getColorMultiplier(I)I"
+        ),
+        ordinal = 0,
+        remap = false
+    )
+    private int tintQuad(int multiplier){
+        // Apply custom tinting
+        if(this.tinting != null)
+            return QuadTintingHelper.getColor(this.tinting, this.blockInfo.getState(), this.blockInfo.getLevel(), this.blockInfo.getBlockPos());
+        return multiplier;
+    }
+
+    @Inject(
+        method = "processQuad",
+        at = @At("RETURN"),
+        remap = false
+    )
+    private void clearTinting(CallbackInfo ci){
+        this.tinting = null;
+    }
+}

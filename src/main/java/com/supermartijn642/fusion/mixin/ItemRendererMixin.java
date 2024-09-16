f2e5bcd8ca26326b70688870efaa22c5d9f1d859
SuperMartijn624 <martijnvandenbrand@outlook.com>
2024-09-16 16:28:19 +0200

    Add quad tinting for items and patch more quad tinting

---
+++ src/main/java/com/supermartijn642/fusion/mixin/ItemRendererMixin.java
@@ -0,0 +1,47 @@
+package com.supermartijn642.fusion.mixin;
+
+import com.mojang.blaze3d.vertex.PoseStack;
+import com.mojang.blaze3d.vertex.VertexConsumer;
+import com.supermartijn642.fusion.api.texture.data.BaseTextureData;
+import com.supermartijn642.fusion.texture.QuadTintingHelper;
+import com.supermartijn642.fusion.texture.types.base.BaseTextureSprite;
+import net.minecraft.client.renderer.block.model.BakedQuad;
+import net.minecraft.client.renderer.entity.ItemRenderer;
+import net.minecraft.client.renderer.texture.TextureAtlasSprite;
+import net.minecraft.util.FastColor;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+/**
+ * Created 16/09/2024 by SuperMartijn642
+ */
+@Mixin(ItemRenderer.class)
+public class ItemRendererMixin {
+
+    @Redirect(
+        method = "renderQuadList",
+        at = @At(
+            value = "INVOKE",
+            target = "Lcom/mojang/blaze3d/vertex/VertexConsumer;putBulkData(Lcom/mojang/blaze3d/vertex/PoseStack$Pose;Lnet/minecraft/client/renderer/block/model/BakedQuad;FFFFIIZ)V"
+        )
+    )
+    private void renderQuadList(VertexConsumer vertexConsumer, PoseStack.Pose pose, BakedQuad quad, float red, float green, float blue, float alpha, int lightmap, int overlay, boolean readExistingColor){
+        // In case texture has a custom tinting set, replace the original tinting
+        if(quad.tintIndex == 39216){
+            TextureAtlasSprite sprite = quad.getSprite();
+            if(sprite instanceof BaseTextureSprite){
+                BaseTextureData.QuadTinting tinting = ((BaseTextureSprite)sprite).data().getTinting();
+                if(tinting != null){
+                    int color = QuadTintingHelper.getColor(tinting, null, null, null);
+                    alpha = FastColor.ARGB32.alpha(color) / 255f;
+                    red = FastColor.ARGB32.red(color) / 255f;
+                    green = FastColor.ARGB32.green(color) / 255f;
+                    blue = FastColor.ARGB32.blue(color) / 255f;
+                }
+            }
+        }
+        // Call the original method
+        vertexConsumer.putBulkData(pose, quad, red, green, blue, alpha, lightmap, overlay, readExistingColor);
+    }
+}

6e2ac70f3335cd38735683f1bf7698dc77d4ae1d
SuperMartijn624 <martijnvandenbrand@outlook.com>
2024-09-16 15:35:56 +0200

    Add quad tinting for items and patch more quad tinting

---
+++ src/main/java/com/supermartijn642/fusion/mixin/fabric/ItemRenderContextMixin.java
@@ -0,0 +1,44 @@
+package com.supermartijn642.fusion.mixin.fabric;
+
+import com.supermartijn642.fusion.api.texture.data.BaseTextureData;
+import com.supermartijn642.fusion.texture.QuadTintingHelper;
+import com.supermartijn642.fusion.texture.types.base.BaseTextureSprite;
+import com.supermartijn642.fusion.util.TextureAtlases;
+import net.fabricmc.fabric.api.renderer.v1.model.SpriteFinder;
+import net.fabricmc.fabric.impl.client.indigo.renderer.helper.ColorHelper;
+import net.fabricmc.fabric.impl.client.indigo.renderer.mesh.MutableQuadViewImpl;
+import net.fabricmc.fabric.impl.client.indigo.renderer.render.ItemRenderContext;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.texture.TextureAtlasSprite;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+/**
+ * Created 16/09/2024 by SuperMartijn642
+ */
+@SuppressWarnings("UnstableApiUsage")
+@Mixin(ItemRenderContext.class)
+public class ItemRenderContextMixin {
+
+    @Inject(
+        method = "colorizeQuad",
+        at = @At("TAIL"),
+        remap = false
+    )
+    private void colorizeQuad(MutableQuadViewImpl quad, int colorIndex, CallbackInfo ci){
+        // In case texture has a custom tinting set, replace the original tinting
+        if(colorIndex == 39216){
+            TextureAtlasSprite sprite = SpriteFinder.get(Minecraft.getInstance().getModelManager().getAtlas(TextureAtlases.getBlocks())).find(quad, 0);
+            if(sprite instanceof BaseTextureSprite){
+                BaseTextureData.QuadTinting tinting = ((BaseTextureSprite)sprite).data().getTinting();
+                if(tinting != null){
+                    int color = 0xFF000000 | QuadTintingHelper.getColor(tinting, null, null, null);
+                    for(int i = 0; i < 4; i++)
+                        quad.spriteColor(i, 0, ColorHelper.swapRedBlueIfNeeded(ColorHelper.multiplyColor(color, quad.spriteColor(i, 0))));
+                }
+            }
+        }
+    }
+}

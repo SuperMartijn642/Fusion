8a84ccfe89e1ba28211190f9e87f9d132fda9b9f
SuperMartijn624 <martijnvandenbrand@outlook.com>
2024-09-18 23:20:20 +0200

    Add custom entity models

---
+++ src/main/java/com/supermartijn642/fusion/mixin/EntityModelSetMixin.java
@@ -0,0 +1,43 @@
+package com.supermartijn642.fusion.mixin;
+
+import com.google.common.collect.ImmutableMap;
+import com.supermartijn642.fusion.entity.model.FusionEntityModelLoader;
+import net.minecraft.client.model.geom.EntityModelSet;
+import net.minecraft.client.model.geom.ModelLayerLocation;
+import net.minecraft.client.model.geom.builders.LayerDefinition;
+import net.minecraft.server.packs.resources.ResourceManager;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created 17/09/2024 by SuperMartijn642
+ */
+@Mixin(EntityModelSet.class)
+public class EntityModelSetMixin {
+
+    @Shadow
+    private Map<ModelLayerLocation,LayerDefinition> roots;
+
+    @Inject(
+        method = "onResourceManagerReload",
+        at = @At("TAIL")
+    )
+    private void loadFusionEntityModels(ResourceManager resourceManager, CallbackInfo ci) {
+        // Gather the identifier of existing models
+        List<ModelLayerLocation> identifiers = new ArrayList<>(this.roots.keySet());
+        identifiers.sort(Comparator.comparing(ModelLayerLocation::toString));
+        // Try to load models from resource packs
+        ImmutableMap.Builder<ModelLayerLocation,LayerDefinition> builder = ImmutableMap.builderWithExpectedSize(this.roots.size());
+        builder.putAll(this.roots);
+        FusionEntityModelLoader.loadModels(identifiers, resourceManager).forEach(builder::put);
+        this.roots = builder.buildKeepingLast();
+    }
+}

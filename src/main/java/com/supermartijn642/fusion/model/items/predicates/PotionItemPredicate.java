18e6c35259a4b4c3df550fa0f9e7bd6bda7a4853
SuperMartijn624 <martijnvandenbrand@outlook.com>
2024-09-22 15:01:53 +0200

    Add item model predicates

---
+++ src/main/java/com/supermartijn642/fusion/model/items/predicates/PotionItemPredicate.java
@@ -0,0 +1,56 @@
+package com.supermartijn642.fusion.model.items.predicates;
+
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParseException;
+import com.supermartijn642.fusion.api.util.Serializer;
+import com.supermartijn642.fusion.util.IdentifierUtil;
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.alchemy.Potion;
+import net.minecraft.world.item.alchemy.PotionUtils;
+import net.minecraft.world.item.alchemy.Potions;
+
+/**
+ * Created 20/09/2024 by SuperMartijn642
+ */
+public class PotionItemPredicate implements ItemPredicate {
+
+    public static final Serializer<PotionItemPredicate> SERIALIZER = new Serializer<>() {
+        @Override
+        public PotionItemPredicate deserialize(JsonObject json) throws JsonParseException{
+            if(!json.has("potion") || !json.get("potion").isJsonPrimitive() || !json.getAsJsonPrimitive("potion").isString())
+                throw new JsonParseException("Potion-predicate must have string property 'enchantment'!");
+            if(!IdentifierUtil.isValidIdentifier(json.get("potion").getAsString()))
+                throw new JsonParseException("Property 'enchantment' must be a valid identifier, not '" + json.get("potion").getAsString() + "'!");
+            ResourceLocation potionIdentifier = new ResourceLocation(json.get("potion").getAsString());
+            Potion potion = BuiltInRegistries.POTION.get(potionIdentifier);
+            if(potion == null || potion == Potions.EMPTY)
+                throw new JsonParseException("Unknown potion '" + potionIdentifier + "'!");
+            return new PotionItemPredicate(potion);
+        }
+
+        @Override
+        public JsonObject serialize(PotionItemPredicate value){
+            JsonObject json = new JsonObject();
+            json.addProperty("potion", BuiltInRegistries.POTION.getKey(value.potion).toString());
+            return json;
+        }
+    };
+
+    private final Potion potion;
+
+    public PotionItemPredicate(Potion potion){
+        this.potion = potion;
+    }
+
+    @Override
+    public boolean test(ItemStack stack){
+        return PotionUtils.getPotion(stack) == this.potion;
+    }
+
+    @Override
+    public Serializer<? extends ItemPredicate> getSerializer(){
+        return SERIALIZER;
+    }
+}
--- src/main/java/com/supermartijn642/fusion/model/overlays/BlockModelOverlayReloadListener.java
+++ src/main/java/com/supermartijn642/fusion/model/overlays/BlockModelOverlayReloadListener.java
@@ -51,6 +51,7 @@ public class BlockModelOverlayReloadListener implements PreparableReloadListener
             UnbakedModel unbakedModel = bakery.getModel(model);
             bakery.unbakedCache.put(model, unbakedModel);
             bakery.topLevelModels.put(model, unbakedModel);
+            unbakedModel.resolveParents(bakery::getModel);
         }
     }
 
@@ -218,6 +219,6 @@ public class BlockModelOverlayReloadListener implements PreparableReloadListener
 
     @Override
     public String getName(){
-        return "Fusion Model Appender Reload Listener";
+        return "Fusion Block Model Overlay Reload Listener";
     }
 }

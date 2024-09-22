b0a3f1f175d4c3535ac035d5baf29f6fe5318aec
SuperMartijn624 <martijnvandenbrand@outlook.com>
2024-09-22 15:01:53 +0200

    Add item model predicates

--- src/main/java/com/supermartijn642/fusion/FusionClient.java
+++ src/main/java/com/supermartijn642/fusion/FusionClient.java
@@ -7,6 +7,8 @@ import com.supermartijn642.fusion.api.predicate.FusionPredicateRegistry;
 import com.supermartijn642.fusion.api.texture.DefaultTextureTypes;
 import com.supermartijn642.fusion.api.texture.FusionTextureTypeRegistry;
 import com.supermartijn642.fusion.api.texture.data.BaseTextureData;
+import com.supermartijn642.fusion.model.items.ItemModelPredicatesReloadListener;
+import com.supermartijn642.fusion.model.items.predicates.*;
 import com.supermartijn642.fusion.model.overlays.BlockModelOverlayReloadListener;
 import com.supermartijn642.fusion.model.types.connecting.ConnectingBakedModel;
 import com.supermartijn642.fusion.predicate.*;
@@ -56,6 +58,14 @@ public class FusionClient {
         FusionPredicateRegistry.registerConnectionPredicate(new ResourceLocation("fusion", "is_same_state"), IsSameStateConnectionPredicate.SERIALIZER);
         FusionPredicateRegistry.registerConnectionPredicate(new ResourceLocation("fusion", "match_block"), MatchBlockConnectionPredicate.SERIALIZER);
         FusionPredicateRegistry.registerConnectionPredicate(new ResourceLocation("fusion", "match_state"), MatchStateConnectionPredicate.SERIALIZER);
+        // Register default item model predicates
+        ItemPredicateRegistry.registerItemPredicate(new ResourceLocation("fusion", "and"), AndItemPredicate.SERIALIZER);
+        ItemPredicateRegistry.registerItemPredicate(new ResourceLocation("fusion", "or"), OrItemPredicate.SERIALIZER);
+        ItemPredicateRegistry.registerItemPredicate(new ResourceLocation("fusion", "not"), NotItemPredicate.SERIALIZER);
+        ItemPredicateRegistry.registerItemPredicate(new ResourceLocation("fusion", "count"), CountItemPredicate.SERIALIZER);
+        ItemPredicateRegistry.registerItemPredicate(new ResourceLocation("fusion", "durability"), DurabilityItemPredicate.SERIALIZER);
+        ItemPredicateRegistry.registerItemPredicate(new ResourceLocation("fusion", "enchantment"), EnchantmentItemPredicate.SERIALIZER);
+        ItemPredicateRegistry.registerItemPredicate(new ResourceLocation("fusion", "potion"), PotionItemPredicate.SERIALIZER);
 
         // Add Fusion's metadata section
         SpriteLoader.DEFAULT_METADATA_SECTIONS = ImmutableSet.<MetadataSectionSerializer<?>>builder()
@@ -87,6 +97,7 @@ public class FusionClient {
             if(index == -1)
                 throw new RuntimeException("Fusion could not find model manager in resource manager reload listeners!");
             resourceManager.listeners.add(index, BlockModelOverlayReloadListener.INSTANCE);
+            resourceManager.listeners.add(index, ItemModelPredicatesReloadListener.INSTANCE);
         });
 
         // Integration with FramedBlocks
---
+++ src/main/java/com/supermartijn642/fusion/model/items/predicates/NotItemPredicate.java
@@ -0,0 +1,46 @@
+package com.supermartijn642.fusion.model.items.predicates;
+
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParseException;
+import com.supermartijn642.fusion.api.util.Serializer;
+import net.minecraft.world.item.ItemStack;
+
+/**
+ * Created 20/09/2024 by SuperMartijn642
+ */
+public class NotItemPredicate implements ItemPredicate {
+
+    public static final Serializer<NotItemPredicate> SERIALIZER = new Serializer<>() {
+        @Override
+        public NotItemPredicate deserialize(JsonObject json) throws JsonParseException{
+            if(!json.has("predicate") || !json.get("predicate").isJsonObject())
+                throw new JsonParseException("Not-predicate must have object property 'predicate'!");
+            // Deserialize the predicate
+            ItemPredicate predicate = ItemPredicateRegistry.deserializeItemPredicate(json.getAsJsonObject("predicate"));
+            return new NotItemPredicate(predicate);
+        }
+
+        @Override
+        public JsonObject serialize(NotItemPredicate value){
+            JsonObject json = new JsonObject();
+            json.add("predicates", ItemPredicateRegistry.serializeItemPredicate(value.predicate));
+            return json;
+        }
+    };
+
+    private final ItemPredicate predicate;
+
+    public NotItemPredicate(ItemPredicate predicate){
+        this.predicate = predicate;
+    }
+
+    @Override
+    public boolean test(ItemStack stack){
+        return !this.predicate.test(stack);
+    }
+
+    @Override
+    public Serializer<? extends ItemPredicate> getSerializer(){
+        return SERIALIZER;
+    }
+}

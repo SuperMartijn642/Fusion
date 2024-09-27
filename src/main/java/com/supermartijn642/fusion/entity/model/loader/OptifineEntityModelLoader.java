89f1925d42bbcb09cd6bd76fe44818b6d7f9aedb
SuperMartijn624 <martijnvandenbrand@outlook.com>
2024-09-27 21:03:56 +0200

    Add entity model modifiers and redo custom models

--- src/main/java/com/supermartijn642/fusion/entity/model/loader/OptifineEntityModelLoader.java
+++ src/main/java/com/supermartijn642/fusion/entity/model/loader/OptifineEntityModelLoader.java
@@ -2,14 +2,14 @@ package com.supermartijn642.fusion.entity.model.loader;
 
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParseException;
-import net.minecraft.client.model.geom.builders.LayerDefinition;
+import net.minecraft.client.model.geom.ModelPart;
 
 /**
  * Created 17/09/2024 by SuperMartijn642
  */
-public class OptifineEntityModelLoader implements EntityModelLoader{
+public class OptifineEntityModelLoader implements EntityModelLoader {
     @Override
-    public LayerDefinition loadModel(JsonObject json){
+    public ModelPart loadModel(JsonObject json){
         throw new JsonParseException("OptiFine entity models are currently not supported.");
     }
 }

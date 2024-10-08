245d1ac12c0aa433cec071c75aea060be22ed6f7
SuperMartijn624 <martijnvandenbrand@outlook.com>
2024-10-08 21:52:16 +0200

    Make animation metadata work with connecting textures

---
+++ src/main/java/com/supermartijn642/fusion/extensions/ResourceMetadataExtension.java
@@ -0,0 +1,9 @@
+package com.supermartijn642.fusion.extensions;
+
+/**
+ * Created 09/10/2024 by SuperMartijn642
+ */
+public interface ResourceMetadataExtension {
+
+    void disableFusionOverwrite();
+}
--- src/main/java/com/supermartijn642/fusion/mixin/SpriteContentsMixin.java
+++ src/main/java/com/supermartijn642/fusion/mixin/SpriteContentsMixin.java
@@ -3,11 +3,13 @@ package com.supermartijn642.fusion.mixin;
 import com.mojang.blaze3d.platform.NativeImage;
 import com.supermartijn642.fusion.api.texture.TextureType;
 import com.supermartijn642.fusion.api.util.Pair;
+import com.supermartijn642.fusion.extensions.ResourceMetadataExtension;
 import com.supermartijn642.fusion.extensions.SpriteContentsExtension;
 import com.supermartijn642.fusion.texture.FusionTextureMetadataSection;
 import com.supermartijn642.fusion.texture.SpritePreparationContextImpl;
 import com.supermartijn642.fusion.texture.TextureTypeRegistryImpl;
 import net.minecraft.client.renderer.texture.SpriteContents;
+import net.minecraft.client.resources.metadata.animation.AnimationMetadataSection;
 import net.minecraft.client.resources.metadata.animation.FrameSize;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.packs.resources.ResourceMetadata;
@@ -50,15 +52,24 @@ public class SpriteContentsMixin implements SpriteContentsExtension {
         Pair<TextureType<Object>,Object> metadata = resourceMetadata.getSection(FusionTextureMetadataSection.INSTANCE).orElse(null);
         if(metadata != null){
             this.fusionTextureMetadata = metadata;
+            // Get the animation metadata
+            if(resourceMetadata instanceof ResourceMetadataExtension)
+                ((ResourceMetadataExtension)resourceMetadata).disableFusionOverwrite();
+            AnimationMetadataSection animationMetadata = resourceMetadata.getSection(AnimationMetadataSection.SERIALIZER).orElse(null);
             // Adjust the frame size
             Pair<Integer,Integer> newSize;
             try{
-                newSize = metadata.left().getFrameSize(new SpritePreparationContextImpl(originalSize.width(), originalSize.height(), image.getWidth(), image.getHeight(), identifier), metadata.right());
+                newSize = metadata.left().getFrameSize(new SpritePreparationContextImpl(originalSize.width(), originalSize.height(), image.getWidth(), image.getHeight(), identifier, animationMetadata), metadata.right());
             }catch(Exception e){
                 throw new RuntimeException("Encountered an exception whilst getting frame size from texture type '" + TextureTypeRegistryImpl.getIdentifier(metadata.left()) + "' for texture '" + identifier + "'!", e);
             }
             if(newSize == null)
                 throw new RuntimeException("Received null frame size from texture type '" + TextureTypeRegistryImpl.getIdentifier(metadata.left()) + "' for texture '" + identifier + "'!");
+
+            // There is no way to make this content loading fail when the frame size is incorrect as Forge's Mixin version doesn't
+            // allow for mixins into interfaces and the entire sprite contents loading happens in a static interface method
+            // So just give up and see what happens
+
             // Replace the current size
             return new FrameSize(newSize.left(), newSize.right());
         }

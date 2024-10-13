6ee9659bda5dbc6ea1475d95a11d37af9b60f3f3
SuperMartijn624 <martijnvandenbrand@outlook.com>
2024-10-13 02:08:32 +0200

    Fix uv-mapping for connecting texture layout with non (0,0) default tile

--- src/main/java/com/supermartijn642/fusion/model/types/connecting/ConnectingBakedModel.java
+++ src/main/java/com/supermartijn642/fusion/model/types/connecting/ConnectingBakedModel.java
@@ -179,7 +179,7 @@ public class ConnectingBakedModel implements BakedModel, FabricBakedModel {
                 if(layoutHandler != null){
                     mutableQuad.set(emitter);
                     mutableQuad.set(TextureOrientation.NORMAL_0.vertexIndexPermutation);
-                    boolean keepQuad = layoutHandler.processItemQuad(quadIndex, mutableQuad, quad.bakedQuad().getSprite());
+                    boolean keepQuad = layoutHandler.processItemQuad(quadIndex, mutableQuad, (ConnectingTextureSprite)quad.bakedQuad().getSprite());
                     if(!keepQuad)
                         continue;
                 }
@@ -297,7 +297,7 @@ public class ConnectingBakedModel implements BakedModel, FabricBakedModel {
                 // Remap the quad's uv
                 mutableQuad.set(quad);
                 mutableQuad.set(predicate.orientation.vertexIndexPermutation);
-                return ConnectingTextureLayoutHandler.get(layout).processBlockQuad(quadIndex, mutableQuad, sprite, connections);
+                return ConnectingTextureLayoutHandler.get(layout).processBlockQuad(quadIndex, mutableQuad, (ConnectingTextureSprite)sprite, connections);
             }
             return true;
         });

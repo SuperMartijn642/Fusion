329f9cdf56d5577e4f76023bc4859a47886495d5
SuperMartijn624 <martijnvandenbrand@outlook.com>
2024-10-13 16:46:34 +0200

    Move frame size check to the texture types themselves

--- src/main/java/com/supermartijn642/fusion/mixin/SpriteResourceLoaderMixin.java
+++ src/main/java/com/supermartijn642/fusion/mixin/SpriteResourceLoaderMixin.java
@@ -1,13 +1,14 @@
 package com.supermartijn642.fusion.mixin;
 
-import com.llamalad7.mixinextras.sugar.Local;
-import com.llamalad7.mixinextras.sugar.ref.LocalRef;
 import com.mojang.blaze3d.platform.NativeImage;
+import com.supermartijn642.fusion.FusionClient;
+import com.supermartijn642.fusion.api.texture.TextureErrorException;
 import com.supermartijn642.fusion.api.texture.TextureType;
 import com.supermartijn642.fusion.api.util.Pair;
 import com.supermartijn642.fusion.texture.FusionTextureMetadataSection;
 import com.supermartijn642.fusion.texture.SpritePreparationContextImpl;
 import com.supermartijn642.fusion.texture.TextureTypeRegistryImpl;
+import net.minecraft.client.renderer.texture.SpriteContents;
 import net.minecraft.client.renderer.texture.atlas.SpriteResourceLoader;
 import net.minecraft.client.resources.metadata.animation.AnimationMetadataSection;
 import net.minecraft.client.resources.metadata.animation.FrameSize;
@@ -35,24 +36,29 @@ public interface SpriteResourceLoaderMixin {
             target = "Lnet/minecraft/client/resources/metadata/animation/AnimationMetadataSection;calculateFrameSize(II)Lnet/minecraft/client/resources/metadata/animation/FrameSize;",
             shift = At.Shift.AFTER
         ),
+        cancellable = true,
         locals = LocalCapture.CAPTURE_FAILHARD
     )
-    private static void modifyFrameSize(Collection<?> metadataSerializers, ResourceLocation identifier, Resource resource, CallbackInfoReturnable<?> ci, ResourceMetadata resourceMetadata, NativeImage image, @Local LocalRef<FrameSize> frameSize){
+    private static void modifyFrameSize(Collection<?> metadataSerializers, ResourceLocation identifier, Resource resource, CallbackInfoReturnable<SpriteContents> ci, ResourceMetadata resourceMetadata, NativeImage image, AnimationMetadataSection animationMetadata, FrameSize originalSize){
         // Get the fusion metadata
         Pair<TextureType<Object>,Object> metadata = resourceMetadata.getSection(FusionTextureMetadataSection.INSTANCE).orElse(null);
         if(metadata != null){
-            FrameSize originalSize = frameSize.get();
             // Adjust the frame size
             Pair<Integer,Integer> newSize;
             try{
-                newSize = metadata.left().getFrameSize(new SpritePreparationContextImpl(originalSize.width(), originalSize.height(), image.getWidth(), image.getHeight(), identifier, resourceMetadata.getSection(AnimationMetadataSection.SERIALIZER).orElse(null)), metadata.right());
+                newSize = metadata.left().getFrameSize(new SpritePreparationContextImpl(originalSize.width(), originalSize.height(), image.getWidth(), image.getHeight(), identifier, animationMetadata), metadata.right());
+            }catch(TextureErrorException e){
+                FusionClient.LOGGER.error("Error for texture '{}': {}", identifier, e.getMessage());
+                image.close();
+                ci.setReturnValue(null);
+                return;
             }catch(Exception e){
                 throw new RuntimeException("Encountered an exception whilst getting frame size from texture type '" + TextureTypeRegistryImpl.getIdentifier(metadata.left()) + "' for texture '" + identifier + "'!", e);
             }
             if(newSize == null)
                 throw new RuntimeException("Received null frame size from texture type '" + TextureTypeRegistryImpl.getIdentifier(metadata.left()) + "' for texture '" + identifier + "'!");
-            // Replace the current size
-            frameSize.set(new FrameSize(newSize.left(), newSize.right()));
+            // Create the sprite contents
+            ci.setReturnValue(new SpriteContents(identifier, new FrameSize(newSize.left(), newSize.right()), image, resourceMetadata));
         }
     }
 }

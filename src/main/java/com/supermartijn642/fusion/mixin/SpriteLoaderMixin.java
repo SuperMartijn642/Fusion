6f181e906fc3ba4530465474d735f69e8c746969
SuperMartijn624 <martijnvandenbrand@outlook.com>
2024-10-13 16:46:34 +0200

    Move frame size check to the texture types themselves

--- src/main/java/com/supermartijn642/fusion/mixin/SpriteLoaderMixin.java
+++ src/main/java/com/supermartijn642/fusion/mixin/SpriteLoaderMixin.java
@@ -1,8 +1,8 @@
 package com.supermartijn642.fusion.mixin;
 
-import com.llamalad7.mixinextras.sugar.Local;
-import com.llamalad7.mixinextras.sugar.ref.LocalRef;
 import com.mojang.blaze3d.platform.NativeImage;
+import com.supermartijn642.fusion.FusionClient;
+import com.supermartijn642.fusion.api.texture.TextureErrorException;
 import com.supermartijn642.fusion.api.texture.TextureType;
 import com.supermartijn642.fusion.api.util.Pair;
 import com.supermartijn642.fusion.extensions.TextureAtlasSpriteExtension;
@@ -10,6 +10,7 @@ import com.supermartijn642.fusion.texture.FusionTextureMetadataSection;
 import com.supermartijn642.fusion.texture.SpriteCreationContextImpl;
 import com.supermartijn642.fusion.texture.SpritePreparationContextImpl;
 import com.supermartijn642.fusion.texture.TextureTypeRegistryImpl;
+import net.minecraft.client.renderer.texture.SpriteContents;
 import net.minecraft.client.renderer.texture.SpriteLoader;
 import net.minecraft.client.renderer.texture.TextureAtlasSprite;
 import net.minecraft.client.resources.metadata.animation.AnimationMetadataSection;
@@ -46,9 +47,10 @@ public class SpriteLoaderMixin {
             target = "Lnet/minecraft/client/resources/metadata/animation/AnimationMetadataSection;calculateFrameSize(II)Lnet/minecraft/client/resources/metadata/animation/FrameSize;",
             shift = At.Shift.AFTER
         ),
+        cancellable = true,
         locals = LocalCapture.CAPTURE_FAILHARD
     )
-    private static void gatherMetadata(ResourceLocation identifier, Resource resource, CallbackInfoReturnable<?> ci, AnimationMetadataSection animationMetadata, NativeImage image, @Local LocalRef<FrameSize> frameSize) {
+    private static void gatherMetadata(ResourceLocation identifier, Resource resource, CallbackInfoReturnable<SpriteContents> ci, AnimationMetadataSection animationMetadata, NativeImage image, FrameSize originalSize){
         // Get the fusion metadata
         Pair<TextureType<Object>,Object> metadata = null;
         try{
@@ -56,18 +58,22 @@ public class SpriteLoaderMixin {
         }catch(IOException ignored){ /* Metadata will always be cached already, so need to worry about exceptions */ }
         if(metadata != null){
             fusionTextureMetadata.put(identifier, metadata);
-            FrameSize originalSize = frameSize.get();
             // Adjust the frame size
             Pair<Integer,Integer> newSize;
             try{
                 newSize = metadata.left().getFrameSize(new SpritePreparationContextImpl(originalSize.width(), originalSize.height(), image.getWidth(), image.getHeight(), identifier, animationMetadata), metadata.right());
+            }catch(TextureErrorException e){
+                FusionClient.LOGGER.error("Error for texture '{}': {}", identifier, e.getMessage());
+                image.close();
+                ci.setReturnValue(null);
+                return;
             }catch(Exception e){
                 throw new RuntimeException("Encountered an exception whilst getting frame size from texture type '" + TextureTypeRegistryImpl.getIdentifier(metadata.left()) + "' for texture '" + identifier + "'!", e);
             }
             if(newSize == null)
                 throw new RuntimeException("Received null frame size from texture type '" + TextureTypeRegistryImpl.getIdentifier(metadata.left()) + "' for texture '" + identifier + "'!");
-            // Replace the current size
-            frameSize.set(new FrameSize(newSize.left(), newSize.right()));
+            // Create the sprite contents
+            ci.setReturnValue(new SpriteContents(identifier, new FrameSize(newSize.left(), newSize.right()), image, animationMetadata));
         }
     }
 
--- src/main/java/com/supermartijn642/fusion/texture/types/base/BaseTextureType.java
+++ src/main/java/com/supermartijn642/fusion/texture/types/base/BaseTextureType.java
@@ -3,9 +3,13 @@ package com.supermartijn642.fusion.texture.types.base;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParseException;
 import com.supermartijn642.fusion.api.texture.SpriteCreationContext;
+import com.supermartijn642.fusion.api.texture.SpritePreparationContext;
+import com.supermartijn642.fusion.api.texture.TextureErrorException;
 import com.supermartijn642.fusion.api.texture.TextureType;
 import com.supermartijn642.fusion.api.texture.data.BaseTextureData;
+import com.supermartijn642.fusion.api.util.Pair;
 import net.minecraft.client.renderer.texture.TextureAtlasSprite;
+import net.minecraft.util.Mth;
 
 import java.util.Arrays;
 import java.util.Locale;
@@ -14,6 +18,14 @@ import java.util.Locale;
  * Created 07/09/2024 by SuperMartijn642
  */
 public class BaseTextureType implements TextureType<BaseTextureData> {
+
+    @Override
+    public Pair<Integer,Integer> getFrameSize(SpritePreparationContext context, BaseTextureData data){
+        if(!Mth.isMultipleOf(context.getTextureWidth(), context.getOriginalFrameWith())
+            || !Mth.isMultipleOf(context.getTextureHeight(), context.getOriginalFrameHeight()))
+            throw new TextureErrorException("Image size {},{} is not a multiple of frame size {},{}");
+        return context.getOriginalFrameSize();
+    }
 
     @Override
     public TextureAtlasSprite createSprite(SpriteCreationContext context, BaseTextureData data){

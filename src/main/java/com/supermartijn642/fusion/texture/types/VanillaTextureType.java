5dca83da4793eb7ea062cb7f18738091b7fec071
SuperMartijn624 <martijnvandenbrand@outlook.com>
2024-10-13 17:17:39 +0200

    Move frame size check to the texture types themselves

--- src/main/java/com/supermartijn642/fusion/texture/types/VanillaTextureType.java
+++ src/main/java/com/supermartijn642/fusion/texture/types/VanillaTextureType.java
@@ -4,6 +4,7 @@ import com.google.gson.JsonObject;
 import com.google.gson.JsonParseException;
 import com.supermartijn642.fusion.api.texture.SpriteCreationContext;
 import com.supermartijn642.fusion.api.texture.SpritePreparationContext;
+import com.supermartijn642.fusion.api.texture.TextureErrorException;
 import com.supermartijn642.fusion.api.texture.TextureType;
 import com.supermartijn642.fusion.api.util.Pair;
 import net.minecraft.client.renderer.texture.TextureAtlasSprite;
@@ -15,6 +16,9 @@ public class VanillaTextureType implements TextureType<Void> {
 
     @Override
     public Pair<Integer,Integer> getFrameSize(SpritePreparationContext context, Void data){
+        if(context.getTextureWidth() % context.getOriginalFrameWith() != 0
+            || context.getTextureHeight() % context.getOriginalFrameHeight() != 0)
+            throw new TextureErrorException("Image size {},{} is not a multiple of frame size {},{}");
         return context.getOriginalFrameSize();
     }
 

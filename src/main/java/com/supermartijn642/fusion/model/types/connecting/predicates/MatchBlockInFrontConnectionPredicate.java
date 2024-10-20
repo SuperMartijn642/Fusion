772b97f7bbf78317891e0d5d7f508c84eca0ffb1
SuperMartijn624 <martijnvandenbrand@outlook.com>
2024-10-20 20:20:19 +0200

    Move connection predicates

--- src/main/java/com/supermartijn642/fusion/model/types/connecting/predicates/MatchBlockInFrontConnectionPredicate.java
+++ src/main/java/com/supermartijn642/fusion/model/types/connecting/predicates/MatchBlockInFrontConnectionPredicate.java
@@ -1,4 +1,4 @@
-package com.supermartijn642.fusion.predicate;
+package com.supermartijn642.fusion.model.types.connecting.predicates;
 
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParseException;
@@ -14,13 +14,13 @@ import net.minecraft.world.level.block.state.BlockState;
 import org.jetbrains.annotations.Nullable;
 
 /**
- * Created 28/04/2023 by SuperMartijn642
+ * Created 12/10/2024 by SuperMartijn642
  */
-public class MatchBlockConnectionPredicate implements ConnectionPredicate {
+public class MatchBlockInFrontConnectionPredicate implements ConnectionPredicate {
 
-    public static final Serializer<MatchBlockConnectionPredicate> SERIALIZER = new Serializer<>() {
+    public static final Serializer<MatchBlockInFrontConnectionPredicate> SERIALIZER = new Serializer<>() {
         @Override
-        public MatchBlockConnectionPredicate deserialize(JsonObject json) throws JsonParseException{
+        public MatchBlockInFrontConnectionPredicate deserialize(JsonObject json) throws JsonParseException{
             if(!json.has("block") || !json.get("block").isJsonPrimitive() || !json.getAsJsonPrimitive("block").isString())
                 throw new JsonParseException("Match block predicate must have string property 'block'!");
             if(!IdentifierUtil.isValidIdentifier(json.get("block").getAsString()))
@@ -29,11 +29,11 @@ public class MatchBlockConnectionPredicate implements ConnectionPredicate {
             if(!BuiltInRegistries.BLOCK.containsKey(identifier))
                 throw new JsonParseException("Unknown block '" + identifier + "'!");
             Block block = BuiltInRegistries.BLOCK.get(identifier);
-            return new MatchBlockConnectionPredicate(block);
+            return new MatchBlockInFrontConnectionPredicate(block);
         }
 
         @Override
-        public JsonObject serialize(MatchBlockConnectionPredicate value){
+        public JsonObject serialize(MatchBlockInFrontConnectionPredicate value){
             JsonObject json = new JsonObject();
             json.addProperty("block", BuiltInRegistries.BLOCK.getKey(value.block).toString());
             return json;
@@ -42,13 +42,13 @@ public class MatchBlockConnectionPredicate implements ConnectionPredicate {
 
     private final Block block;
 
-    public MatchBlockConnectionPredicate(Block block){
+    public MatchBlockInFrontConnectionPredicate(Block block){
         this.block = block;
     }
 
     @Override
     public boolean shouldConnect(Direction side, @Nullable BlockState ownState, BlockState otherState, BlockState blockInFront, ConnectionDirection direction){
-        return otherState.getBlock() == this.block;
+        return blockInFront.getBlock() == this.block;
     }
 
     @Override
@@ -59,7 +59,7 @@ public class MatchBlockConnectionPredicate implements ConnectionPredicate {
     @Override
     public final boolean equals(Object o){
         if(this == o) return true;
-        if(!(o instanceof MatchBlockConnectionPredicate that)) return false;
+        if(!(o instanceof MatchBlockInFrontConnectionPredicate that)) return false;
 
         return this.block.equals(that.block);
     }

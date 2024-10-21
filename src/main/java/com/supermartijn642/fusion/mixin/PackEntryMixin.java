868fe7c404203681af9d2e24a4793ccebc52cb57
SuperMartijn624 <martijnvandenbrand@outlook.com>
2024-10-22 00:46:50 +0200

    Swap confirm and cancel buttons in warning screen

--- src/main/java/com/supermartijn642/fusion/mixin/PackEntryMixin.java
+++ src/main/java/com/supermartijn642/fusion/mixin/PackEntryMixin.java
@@ -98,6 +98,8 @@ public class PackEntryMixin {
         cancellable = true
     )
     private void showFusionWarningScreen(CallbackInfoReturnable<Boolean> ci){
+        if(this.metadata == null)
+            return;
         Consumer<Boolean> callback = select -> {
             Minecraft.getInstance().setScreen(this.parent.screen);
             if(select)

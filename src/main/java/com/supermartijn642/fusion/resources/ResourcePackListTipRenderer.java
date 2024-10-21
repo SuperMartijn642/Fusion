f0173c989dc658d8a05c4ad177fbbae864880cb0
SuperMartijn624 <martijnvandenbrand@outlook.com>
2024-10-21 18:34:56 +0200

    Add minimum Fusion version option and warning screen

---
+++ src/main/java/com/supermartijn642/fusion/resources/ResourcePackListTipRenderer.java
@@ -0,0 +1,46 @@
+package com.supermartijn642.fusion.resources;
+
+import com.mojang.blaze3d.systems.RenderSystem;
+import com.mojang.blaze3d.vertex.PoseStack;
+import net.minecraft.ChatFormatting;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.GuiComponent;
+import net.minecraft.client.gui.screens.packs.PackSelectionModel;
+import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.util.FastColor;
+
+import java.util.function.Consumer;
+
+/**
+ * Created 21/10/2024 by SuperMartijn642
+ */
+public class ResourcePackListTipRenderer {
+
+    private static final ResourceLocation FUSION_LOGO = new ResourceLocation("fusion", "textures/resourcepacks/fusion_icon_blurred.png");
+
+    public static void renderBackground(FusionPackMetadata metadata, boolean isVanillaCompatible, PoseStack poseStack, int x, int y, int width, int height){
+        if(isVanillaCompatible && !metadata.isMinVersionSatisfied())
+            GuiComponent.fill(poseStack, x - 1, y - 1, x + width - 3, y + height + 1, FastColor.ARGB32.color(255, 114, 83, 0));
+    }
+
+    public static void renderIcon(FusionPackMetadata metadata, boolean isVanillaCompatible, PoseStack poseStack, int x, int y, int width, int height){
+        RenderSystem.enableBlend();
+        RenderSystem.setShaderTexture(0, FUSION_LOGO);
+        GuiComponent.blit(poseStack, x, y, 0, 0, 12, 12, 12, 12);
+        RenderSystem.disableBlend();
+    }
+
+    public static Component getWarningMessage(FusionPackMetadata metadata, boolean isVanillaCompatible){
+        if(isVanillaCompatible && !metadata.isMinVersionSatisfied())
+            return Component.translatable("fusion.resource_packs.requires_newer_version").withStyle(ChatFormatting.GRAY);
+        return null;
+    }
+
+    public static boolean showWarningScreen(FusionPackMetadata metadata, boolean isVanillaCompatible, PackSelectionModel.EntryBase entry, Consumer<Boolean> confirmation){
+        if(!isVanillaCompatible || metadata.isMinVersionSatisfied())
+            return false;
+        Minecraft.getInstance().setScreen(new MinimumVersionWarningScreen(entry, confirmation));
+        return true;
+    }
+}

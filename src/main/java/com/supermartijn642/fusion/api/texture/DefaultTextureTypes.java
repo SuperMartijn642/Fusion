454042c748c27aa17dce150155ad5873a67a66c8
SuperMartijn624 <martijnvandenbrand@outlook.com>
2024-10-22 18:04:31 +0200

    Add 'random' and 'continuous' texture types

--- src/main/java/com/supermartijn642/fusion/api/texture/DefaultTextureTypes.java
+++ src/main/java/com/supermartijn642/fusion/api/texture/DefaultTextureTypes.java
@@ -1,12 +1,12 @@
 package com.supermartijn642.fusion.api.texture;
 
 import com.supermartijn642.fusion.api.model.DefaultModelTypes;
-import com.supermartijn642.fusion.api.texture.data.BaseTextureData;
-import com.supermartijn642.fusion.api.texture.data.ConnectingTextureData;
-import com.supermartijn642.fusion.api.texture.data.ScrollingTextureData;
+import com.supermartijn642.fusion.api.texture.data.*;
 import com.supermartijn642.fusion.texture.types.VanillaTextureType;
 import com.supermartijn642.fusion.texture.types.base.BaseTextureType;
 import com.supermartijn642.fusion.texture.types.connecting.ConnectingTextureType;
+import com.supermartijn642.fusion.texture.types.continuous.ContinuousTextureType;
+import com.supermartijn642.fusion.texture.types.random.RandomTextureType;
 import com.supermartijn642.fusion.texture.types.scrolling.ScrollingTextureType;
 
 /**
@@ -35,4 +35,14 @@ public final class DefaultTextureTypes {
      * @see ScrollingTextureData#builder()
      */
     public static final TextureType<ScrollingTextureData> SCROLLING = new ScrollingTextureType();
+    /**
+     * Texture type which picks a random tile from a tile sheet. Should be used in conjunction with {@link DefaultModelTypes#BASE} model type.
+     * @see RandomTextureData#builder()
+     */
+    public static final TextureType<RandomTextureData> RANDOM = new RandomTextureType();
+    /**
+     * Texture type for which the texture continues over multiple block spaces. Should be used in conjunction with {@link DefaultModelTypes#BASE} model type.
+     * @see ContinuousTextureData#builder()
+     */
+    public static final TextureType<ContinuousTextureData> CONTINUOUS = new ContinuousTextureType();
 }

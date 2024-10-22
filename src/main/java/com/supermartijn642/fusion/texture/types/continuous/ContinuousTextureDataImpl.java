09b865fa5d629651d6e62c5a2815cfcae0a68277
SuperMartijn624 <martijnvandenbrand@outlook.com>
2024-10-22 18:04:31 +0200

    Add 'random' and 'continuous' texture types

---
+++ src/main/java/com/supermartijn642/fusion/texture/types/continuous/ContinuousTextureDataImpl.java
@@ -0,0 +1,28 @@
+package com.supermartijn642.fusion.texture.types.continuous;
+
+import com.supermartijn642.fusion.api.texture.data.ContinuousTextureData;
+import com.supermartijn642.fusion.texture.types.base.BaseTextureDataImpl;
+
+/**
+ * Created 22/10/2024 by SuperMartijn642
+ */
+public class ContinuousTextureDataImpl extends BaseTextureDataImpl implements ContinuousTextureData {
+
+    private final int rows, columns;
+
+    public ContinuousTextureDataImpl(RenderType renderType, boolean emissive, QuadTinting tinting, int rows, int columns){
+        super(renderType, emissive, tinting);
+        this.rows = rows;
+        this.columns = columns;
+    }
+
+    @Override
+    public int getRows(){
+        return this.rows;
+    }
+
+    @Override
+    public int getColumns(){
+        return this.columns;
+    }
+}

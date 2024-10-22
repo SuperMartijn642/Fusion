77a761d23eb0f9a3b1705e177c04ecaa17cf7813
SuperMartijn624 <martijnvandenbrand@outlook.com>
2024-10-22 16:48:49 +0200

    Properly filter empty animation metadata in `SpritePreparationContextImpl`

--- src/main/java/com/supermartijn642/fusion/texture/SpritePreparationContextImpl.java
+++ src/main/java/com/supermartijn642/fusion/texture/SpritePreparationContextImpl.java
@@ -21,7 +21,7 @@ public class SpritePreparationContextImpl implements SpritePreparationContext {
         this.textureWidth = textureWidth;
         this.textureHeight = textureHeight;
         this.identifier = identifier;
-        this.animationMetadata = animationMetadata;
+        this.animationMetadata = animationMetadata == AnimationMetadataSection.EMPTY ? null : animationMetadata;
     }
 
     @Override

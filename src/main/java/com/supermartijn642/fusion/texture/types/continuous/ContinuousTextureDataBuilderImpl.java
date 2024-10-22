53569e56911e10fef998fe148b4013385090171b
SuperMartijn624 <martijnvandenbrand@outlook.com>
2024-10-22 18:04:31 +0200

    Add 'random' and 'continuous' texture types

---
+++ src/main/java/com/supermartijn642/fusion/texture/types/continuous/ContinuousTextureDataBuilderImpl.java
@@ -0,0 +1,52 @@
+package com.supermartijn642.fusion.texture.types.continuous;
+
+import com.supermartijn642.fusion.api.texture.data.BaseTextureData;
+import com.supermartijn642.fusion.api.texture.data.ContinuousTextureData;
+
+import javax.annotation.Nullable;
+
+/**
+ * Created 07/09/2024 by SuperMartijn642
+ */
+public class ContinuousTextureDataBuilderImpl implements ContinuousTextureData.Builder {
+
+    private BaseTextureData.RenderType renderType;
+    private boolean emissive = false;
+    private BaseTextureData.QuadTinting tinting;
+    private int rows = 1, columns = 1;
+
+    @Override
+    public ContinuousTextureDataBuilderImpl renderType(@Nullable BaseTextureData.RenderType renderType){
+        this.renderType = renderType;
+        return this;
+    }
+
+    @Override
+    public ContinuousTextureDataBuilderImpl emissive(boolean emissive){
+        this.emissive = emissive;
+        return this;
+    }
+
+    @Override
+    public ContinuousTextureDataBuilderImpl tinting(@Nullable BaseTextureData.QuadTinting tinting){
+        this.tinting = tinting;
+        return this;
+    }
+
+    @Override
+    public ContinuousTextureData.Builder rows(int rows){
+        this.rows = rows;
+        return this;
+    }
+
+    @Override
+    public ContinuousTextureData.Builder columns(int columns){
+        this.columns = columns;
+        return this;
+    }
+
+    @Override
+    public ContinuousTextureData build(){
+        return new ContinuousTextureDataImpl(this.renderType, this.emissive, this.tinting, this.rows, this.columns);
+    }
+}
---
+++ src/main/java/com/supermartijn642/fusion/texture/types/random/RandomTextureDataBuilderImpl.java
@@ -0,0 +1,73 @@
+package com.supermartijn642.fusion.texture.types.random;
+
+import com.supermartijn642.fusion.api.texture.data.BaseTextureData;
+import com.supermartijn642.fusion.api.texture.data.RandomTextureData;
+
+import javax.annotation.Nullable;
+
+/**
+ * Created 07/09/2024 by SuperMartijn642
+ */
+public class RandomTextureDataBuilderImpl implements RandomTextureData.Builder {
+
+    private BaseTextureData.RenderType renderType;
+    private boolean emissive = false;
+    private BaseTextureData.QuadTinting tinting;
+    private int rows = 1, columns = 1, count;
+    private Long seed;
+
+    @Override
+    public RandomTextureDataBuilderImpl renderType(@Nullable BaseTextureData.RenderType renderType){
+        this.renderType = renderType;
+        return this;
+    }
+
+    @Override
+    public RandomTextureDataBuilderImpl emissive(boolean emissive){
+        this.emissive = emissive;
+        return this;
+    }
+
+    @Override
+    public RandomTextureDataBuilderImpl tinting(@Nullable BaseTextureData.QuadTinting tinting){
+        this.tinting = tinting;
+        return this;
+    }
+
+    @Override
+    public RandomTextureData.Builder rows(int rows){
+        if(rows < 1 || rows > 10)
+            throw new IllegalArgumentException("rows must be between 1 and 10");
+        this.rows = rows;
+        return this;
+    }
+
+    @Override
+    public RandomTextureData.Builder columns(int columns){
+        if(columns < 1 || columns > 10)
+            throw new IllegalArgumentException("columns must be between 1 and 10");
+        this.columns = columns;
+        return this;
+    }
+
+    @Override
+    public RandomTextureData.Builder count(int count){
+        if(count < 1 || count > 100)
+            throw new IllegalArgumentException("count must be between 1 and 100");
+        this.count = count;
+        return this;
+    }
+
+    @Override
+    public RandomTextureData.Builder seed(Long seed){
+        this.seed = seed;
+        return this;
+    }
+
+    @Override
+    public RandomTextureData build(){
+        if(this.count > this.rows * this.columns)
+            throw new IllegalArgumentException("Count cannot be greater than rows * columns!");
+        return new RandomTextureDataImpl(this.renderType, this.emissive, this.tinting, this.rows, this.columns, this.count, this.seed);
+    }
+}

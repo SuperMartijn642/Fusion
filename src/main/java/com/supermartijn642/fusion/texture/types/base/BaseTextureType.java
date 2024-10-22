85f77363c982126d26a4ff7ef1cf91038881c53b
SuperMartijn624 <martijnvandenbrand@outlook.com>
2024-10-22 18:12:01 +0200

    Fix frame size error messages

--- src/main/java/com/supermartijn642/fusion/texture/types/base/BaseTextureType.java
+++ src/main/java/com/supermartijn642/fusion/texture/types/base/BaseTextureType.java
@@ -22,7 +22,7 @@ public class BaseTextureType implements TextureType<BaseTextureData> {
     public Pair<Integer,Integer> getFrameSize(SpritePreparationContext context, BaseTextureData data){
         if(context.getTextureWidth() % context.getOriginalFrameWith() != 0
             || context.getTextureHeight() % context.getOriginalFrameHeight() != 0)
-            throw new TextureErrorException("Image size {},{} is not a multiple of frame size {},{}");
+            throw new TextureErrorException("Image size " + context.getTextureWidth() + "x" + context.getTextureHeight() + " is not a multiple of frame size " + context.getOriginalFrameWith() + "x" + context.getOriginalFrameHeight() + "!");
         return context.getOriginalFrameSize();
     }
 

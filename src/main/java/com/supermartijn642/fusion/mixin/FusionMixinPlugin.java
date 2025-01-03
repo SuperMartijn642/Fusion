42fb03500b2828d789222a01bb5cadfb317c6f75
SuperMartijn624 <martijnvandenbrand@outlook.com>
2025-01-03 17:29:58 +0100

    Add mixins for when Rubidium or Embeddium are installed

---
+++ src/main/java/com/supermartijn642/fusion/mixin/FusionMixinPlugin.java
@@ -0,0 +1,70 @@
+package com.supermartijn642.fusion.mixin;
+
+import org.objectweb.asm.tree.ClassNode;
+import org.spongepowered.asm.mixin.extensibility.IMixinConfigPlugin;
+import org.spongepowered.asm.mixin.extensibility.IMixinInfo;
+import org.spongepowered.asm.service.MixinService;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * Created 21/06/2023 by SuperMartijn642
+ */
+public class FusionMixinPlugin implements IMixinConfigPlugin {
+
+    private boolean isEmbeddiumLoaded;
+    private boolean isRubidiumLoaded;
+
+    @Override
+    public void onLoad(String mixinPackage){
+        this.isEmbeddiumLoaded = isClassAvailable("org.embeddedt.embeddium.api.eventbus.EmbeddiumEvent");
+        this.isRubidiumLoaded = !this.isEmbeddiumLoaded && isClassAvailable("me.jellysquid.mods.sodium.client.SodiumClientMod");
+    }
+
+    private static boolean isClassAvailable(String className){
+        try{
+            MixinService.getService().getBytecodeProvider().getClassNode(className);
+            return true;
+        }catch(Exception ignored){
+            return false;
+        }
+    }
+
+    @Override
+    public String getRefMapperConfig(){
+        return null;
+    }
+
+    @Override
+    public boolean shouldApplyMixin(String targetClassName, String mixinClassName){
+        if(this.isEmbeddiumLoaded && mixinClassName.endsWith(".ItemRendererMixin"))
+            return false;
+        if(this.isRubidiumLoaded && mixinClassName.endsWith(".ItemRendererMixin"))
+            return false;
+        return true;
+    }
+
+    @Override
+    public void acceptTargets(Set<String> myTargets, Set<String> otherTargets){
+    }
+
+    @Override
+    public List<String> getMixins(){
+        List<String> mixins = new ArrayList<>();
+        if(this.isEmbeddiumLoaded)
+            mixins.add("embeddium.BlockRendererMixinEmbeddium");
+        if(this.isRubidiumLoaded)
+            mixins.add("rubidium.BlockRendererMixinRubidium");
+        return mixins;
+    }
+
+    @Override
+    public void preApply(String targetClassName, ClassNode targetClass, String mixinClassName, IMixinInfo mixinInfo){
+    }
+
+    @Override
+    public void postApply(String targetClassName, ClassNode targetClass, String mixinClassName, IMixinInfo mixinInfo){
+    }
+}
---
+++ src/main/java/com/supermartijn642/fusion/mixin/embeddium/BlockRendererMixinEmbeddium.java
@@ -0,0 +1,53 @@
+package com.supermartijn642.fusion.mixin.embeddium;
+
+import com.supermartijn642.fusion.api.texture.data.BaseTextureData;
+import com.supermartijn642.fusion.texture.QuadTintingHelper;
+import com.supermartijn642.fusion.texture.types.base.BaseTextureSprite;
+import me.jellysquid.mods.sodium.client.model.color.ColorProvider;
+import me.jellysquid.mods.sodium.client.model.quad.BakedQuadView;
+import me.jellysquid.mods.sodium.client.render.chunk.compile.pipeline.BlockRenderContext;
+import me.jellysquid.mods.sodium.client.render.chunk.compile.pipeline.BlockRenderer;
+import net.caffeinemc.mods.sodium.api.util.ColorARGB;
+import net.minecraft.client.renderer.block.model.BakedQuad;
+import net.minecraft.client.renderer.texture.TextureAtlasSprite;
+import net.minecraft.world.level.block.state.BlockState;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import java.util.Arrays;
+
+/**
+ * Created 02/01/2025 by SuperMartijn642
+ */
+@Mixin(BlockRenderer.class)
+public class BlockRendererMixinEmbeddium {
+
+    @Final
+    @Shadow(remap = false)
+    private int[] quadColors;
+
+    @Inject(
+        method = "getVertexColors",
+        at = @At("HEAD"),
+        cancellable = true,
+        remap = false
+    )
+    private void getBlockTint(BlockRenderContext ctx, ColorProvider<BlockState> colorProvider, BakedQuadView quad, CallbackInfoReturnable<int[]> ci){
+        // In case texture has a custom tinting set, replace the original tinting
+        if(((BakedQuad)quad).tintIndex == 39216){
+            TextureAtlasSprite sprite = quad.getSprite();
+            if(sprite instanceof BaseTextureSprite){
+                BaseTextureData.QuadTinting tinting = ((BaseTextureSprite)sprite).data().getTinting();
+                if(tinting != null){
+                    int color = ColorARGB.toABGR(QuadTintingHelper.getColor(tinting, ctx.state(), ctx.world(), ctx.pos()));
+                    Arrays.fill(this.quadColors, color | -16777216);
+                    ci.setReturnValue(this.quadColors);
+                }
+            }
+        }
+    }
+}

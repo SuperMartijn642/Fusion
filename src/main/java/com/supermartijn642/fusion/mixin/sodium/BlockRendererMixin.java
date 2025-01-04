b6b9e79df60875f0e8193f10d73e436233db8847
SuperMartijn624 <martijnvandenbrand@outlook.com>
2025-01-04 03:56:12 +0100

    Add mixins for when Sodium is installed

---
+++ src/main/java/com/supermartijn642/fusion/mixin/sodium/BlockRendererMixin.java
@@ -0,0 +1,52 @@
+package com.supermartijn642.fusion.mixin.sodium;
+
+import com.supermartijn642.fusion.api.texture.data.BaseTextureData;
+import com.supermartijn642.fusion.texture.QuadTintingHelper;
+import com.supermartijn642.fusion.texture.types.base.BaseTextureSprite;
+import com.supermartijn642.fusion.util.TextureAtlases;
+import net.caffeinemc.mods.sodium.api.util.ColorMixer;
+import net.caffeinemc.mods.sodium.client.render.chunk.compile.pipeline.BlockRenderer;
+import net.caffeinemc.mods.sodium.client.render.frapi.mesh.MutableQuadViewImpl;
+import net.caffeinemc.mods.sodium.client.render.frapi.render.AbstractBlockRenderContext;
+import net.fabricmc.fabric.api.renderer.v1.model.SpriteFinder;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.texture.TextureAtlasSprite;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+/**
+ * Created 02/01/2025 by SuperMartijn642
+ */
+@Mixin(BlockRenderer.class)
+public abstract class BlockRendererMixin extends AbstractBlockRenderContext {
+
+    @Final
+    @Shadow(remap = false)
+    private int[] vertexColors;
+
+    @Inject(
+        method = "colorizeQuad",
+        at = @At("HEAD"),
+        cancellable = true,
+        remap = false
+    )
+    private void colorizeQuad(MutableQuadViewImpl quad, int colorIndex, CallbackInfo ci){
+        // In case texture has a custom tinting set, replace the original tinting
+        if(colorIndex == 39216){
+            TextureAtlasSprite sprite = quad.sprite(SpriteFinder.get(Minecraft.getInstance().getModelManager().getAtlas(TextureAtlases.getBlocks())));
+            if(sprite instanceof BaseTextureSprite){
+                BaseTextureData.QuadTinting tinting = ((BaseTextureSprite)sprite).data().getTinting();
+                if(tinting != null){
+                    int color = -16777216 | QuadTintingHelper.getColor(tinting, this.state, this.slice, this.pos);
+                    for(int i = 0; i < 4; ++i)
+                        quad.color(i, ColorMixer.mulComponentWise(color, quad.color(i)));
+                    ci.cancel();
+                }
+            }
+        }
+    }
+}

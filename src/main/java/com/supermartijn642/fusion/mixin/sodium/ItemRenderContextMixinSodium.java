7a8c1ffc5a5141f87f8ce85365051eac405a5854
SuperMartijn624 <martijnvandenbrand@outlook.com>
2025-01-04 04:02:35 +0100

    Add mixins for when Sodium is installed

---
+++ src/main/java/com/supermartijn642/fusion/mixin/sodium/ItemRenderContextMixinSodium.java
@@ -0,0 +1,45 @@
+package com.supermartijn642.fusion.mixin.sodium;
+
+import com.supermartijn642.fusion.api.texture.data.BaseTextureData;
+import com.supermartijn642.fusion.texture.QuadTintingHelper;
+import com.supermartijn642.fusion.texture.types.base.BaseTextureSprite;
+import com.supermartijn642.fusion.util.TextureAtlases;
+import net.caffeinemc.mods.sodium.api.util.ColorMixer;
+import net.caffeinemc.mods.sodium.client.render.frapi.mesh.MutableQuadViewImpl;
+import net.caffeinemc.mods.sodium.client.render.frapi.render.ItemRenderContext;
+import net.fabricmc.fabric.api.renderer.v1.model.SpriteFinder;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.texture.TextureAtlasSprite;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+/**
+ * Created 16/09/2024 by SuperMartijn642
+ */
+@Mixin(ItemRenderContext.class)
+public class ItemRenderContextMixinSodium {
+
+    @Inject(
+        method = "tintQuad",
+        at = @At("HEAD"),
+        cancellable = true,
+        remap = false
+    )
+    private void tintQuad(MutableQuadViewImpl quad, CallbackInfo ci){
+        // In case texture has a custom tinting set, replace the original tinting
+        if(quad.tintIndex() == 39216){
+            TextureAtlasSprite sprite = quad.sprite(SpriteFinder.get(Minecraft.getInstance().getModelManager().getAtlas(TextureAtlases.getBlocks())));
+            if(sprite instanceof BaseTextureSprite){
+                BaseTextureData.QuadTinting tinting = ((BaseTextureSprite)sprite).data().getTinting();
+                if(tinting != null){
+                    int color = 0xFF000000 | QuadTintingHelper.getColor(tinting, null, null, null);
+                    for(int i = 0; i < 4; i++)
+                        quad.color(i, ColorMixer.mulComponentWise(color, quad.color(i)));
+                    ci.cancel();
+                }
+            }
+        }
+    }
+}

3b077489f477ea1b58ef10286f7812183be68998
SuperMartijn624 <martijnvandenbrand@outlook.com>
2025-01-05 17:04:41 +0100

    Add workaround for tinting multi tile texture atlas sprites

--- src/main/java/com/supermartijn642/fusion/model/types/connecting/ConnectingBakedModel.java
+++ src/main/java/com/supermartijn642/fusion/model/types/connecting/ConnectingBakedModel.java
@@ -312,6 +312,9 @@ public class ConnectingBakedModel implements BakedModel {
                         // Get the sprite
                         TextureAtlasSprite sprite = this.sprites.get(spriteIndex);
 
+                        // TODO fix this workaround
+                        quad.tag((int)Math.floor((sprite.u1 + sprite.u0) / 2 * 65535) | (int)Math.floor((sprite.v1 + sprite.v0) / 2 * 65535) << 16);
+
                         // Handle random texture type
                         if(type == 2){
                             mutableQuad.set(quad);
@@ -367,6 +370,9 @@ public class ConnectingBakedModel implements BakedModel {
                     // Get the sprite and the texture layout
                     TextureAtlasSprite sprite = this.sprites.get(spriteIndex);
                     ConnectingTextureLayout layout = ((ConnectingTextureSprite)sprite).data().getLayout();
+
+                    // TODO fix this workaround
+                    quad.tag((int)Math.floor((sprite.u1 + sprite.u0) / 2 * 65535) | (int)Math.floor((sprite.v1 + sprite.v0) / 2 * 65535) << 16);
 
                     // Remap the quad's uv
                     mutableQuad.set(quad);

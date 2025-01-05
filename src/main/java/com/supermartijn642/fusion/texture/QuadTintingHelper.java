68ea798c3a5b28d0db6828a73ec9c8c1d8a5086a
SuperMartijn624 <martijnvandenbrand@outlook.com>
2025-01-05 17:02:55 +0100

    Always use 255 alpha for quad tinting

--- src/main/java/com/supermartijn642/fusion/texture/QuadTintingHelper.java
+++ src/main/java/com/supermartijn642/fusion/texture/QuadTintingHelper.java
@@ -42,6 +42,6 @@ public class QuadTintingHelper {
 
     public static int getColor(BaseTextureData.QuadTinting tinting, BlockState state, BlockAndTintGetter level, BlockPos pos){
         BlockColor tintFunction = TINT_FUNCTIONS[tinting.ordinal()];
-        return tintFunction.getColor(state, level, pos, 0);
+        return tintFunction.getColor(state, level, pos, 0) | 0xff000000;
     }
 }

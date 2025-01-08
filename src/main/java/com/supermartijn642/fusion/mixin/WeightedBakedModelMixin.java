4e5785b4352975b7daf96a431870d01c48b03c7d
embeddedt <42941056+embeddedt@users.noreply.github.com>
2025-01-08 23:21:45 +0100

    Improve performance of WeightedBakedModel model data injector (#83)

--- src/main/java/com/supermartijn642/fusion/mixin/WeightedBakedModelMixin.java
+++ src/main/java/com/supermartijn642/fusion/mixin/WeightedBakedModelMixin.java
@@ -7,6 +7,7 @@ import net.minecraft.util.RandomSource;
 import net.minecraft.util.random.SimpleWeightedRandomList;
 import net.minecraft.world.level.BlockAndTintGetter;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.levelgen.SingleThreadedRandomSource;
 import net.minecraftforge.client.extensions.IForgeBakedModel;
 import net.minecraftforge.client.model.data.ModelData;
 import org.jetbrains.annotations.NotNull;
@@ -14,24 +15,48 @@ import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * Created 26/10/2023 by SuperMartijn642
  */
 @Mixin(WeightedBakedModel.class)
 public class WeightedBakedModelMixin implements IForgeBakedModel {
-
     @Final
     @Shadow
     private SimpleWeightedRandomList<BakedModel> list;
+
     @Unique
-    private final ThreadLocal<RandomSource> RANDOM = ThreadLocal.withInitial(RandomSource::create);
+    private static final ConcurrentHashMap<Class<? extends IForgeBakedModel>,Boolean> MODELS_PRODUCING_DATA = new ConcurrentHashMap<>();
+
+    @Unique
+    private boolean fusion$innerModelProducesData;
+
+    @Inject(method = "<init>", at = @At("RETURN"))
+    private void onInit(CallbackInfo ci){
+        this.fusion$innerModelProducesData = this.list.unwrap().stream().anyMatch(w -> w.data() != null && MODELS_PRODUCING_DATA.computeIfAbsent(w.data().getClass(), clz -> {
+            try{
+                var method = clz.getMethod("getModelData", BlockAndTintGetter.class, BlockPos.class, BlockState.class, ModelData.class);
+                return method.getDeclaringClass() != IForgeBakedModel.class;
+            }catch(NoSuchMethodException e){
+                // This should not happen, but if so, assume it does produce data
+                return true;
+            }
+        }));
+    }
 
     @Override
     public @NotNull ModelData getModelData(@NotNull BlockAndTintGetter level, @NotNull BlockPos pos, @NotNull BlockState state, @NotNull ModelData modelData){
+        // Skip expensive computations below if none of the inner models need model data
+        if(state == null || !this.fusion$innerModelProducesData)
+            return modelData;
+
         // Get the seed for the given block position
-        RandomSource randomSource = this.RANDOM.get();
-        randomSource.setSeed(state.getSeed(pos));
+        RandomSource randomSource = new SingleThreadedRandomSource(state.getSeed(pos));
         // Update the model data for the selected sub model
         BakedModel model = this.list.getRandomValue(randomSource).orElse(null);
         return model == null ? modelData : model.getModelData(level, pos, state, modelData);

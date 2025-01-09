df40bd03c28aa707d030733f2d8473aa0fd2a336
SuperMartijn624 <martijnvandenbrand@outlook.com>
2025-01-09 20:37:36 +0100

    Fix 'append' check in BlockModelModifierReloadListener#parseResource

--- src/main/java/com/supermartijn642/fusion/model/modifiers/block/BlockModelModifierReloadListener.java
+++ src/main/java/com/supermartijn642/fusion/model/modifiers/block/BlockModelModifierReloadListener.java
@@ -136,7 +136,7 @@ public class BlockModelModifierReloadListener {
 
         // Get the models
         Set<ResourceLocation> models = new LinkedHashSet<>(); // This should maintain order
-        if(json.has("pane_culling_fix")){
+        if(json.has("append")){
             if(!json.get("append").isJsonArray())
                 throw new JsonParseException("Property 'append' must be an array!");
             JsonArray appendJson = json.getAsJsonArray("append");

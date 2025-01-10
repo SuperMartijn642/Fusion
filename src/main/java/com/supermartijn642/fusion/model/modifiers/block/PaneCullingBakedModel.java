45d35ed01d4e872e6fd7834ff00c95503f99038f
SuperMartijn624 <martijnvandenbrand@outlook.com>
2025-01-10 18:18:45 +0100

    Fix inverted culling check in `PaneCullingBakedModel`

--- src/main/java/com/supermartijn642/fusion/model/modifiers/block/PaneCullingBakedModel.java
+++ src/main/java/com/supermartijn642/fusion/model/modifiers/block/PaneCullingBakedModel.java
@@ -68,7 +68,7 @@ public class PaneCullingBakedModel extends WrappedBakedModel {
         List<BakedQuad> quads = super.getQuads(state, cullDirection, random, data);
         List<BakedQuad> culledQuads = new ArrayList<>(quads.size());
         for(BakedQuad quad : quads){
-            if(!this.filterQuad(quad, stateAbove, stateBelow))
+            if(this.filterQuad(quad, stateAbove, stateBelow))
                 culledQuads.add(quad);
         }
         return culledQuads;

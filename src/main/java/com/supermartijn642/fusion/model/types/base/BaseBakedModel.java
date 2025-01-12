78b4d1ceed43ddfc7f31af0022e34634e88af22e
SuperMartijn624 <martijnvandenbrand@outlook.com>
2025-01-12 20:59:53 +0100

    Apply custom texture render types for item models as well

--- src/main/java/com/supermartijn642/fusion/model/types/base/BaseBakedModel.java
+++ src/main/java/com/supermartijn642/fusion/model/types/base/BaseBakedModel.java
@@ -84,7 +84,7 @@ public class BaseBakedModel implements BakedModel {
         builder.clear();
         emitter = builder.emitter();
         for(BaseModelQuad quad : quads){
-            RenderMaterial material = FusionClient.getRenderTypeMaterial(null, null, quad.emissive());
+            RenderMaterial material = FusionClient.getRenderTypeMaterial(null, quad.renderType(), quad.emissive());
             emitter.fromVanilla(quad.bakedQuad(), material, quad.cullDirection());
             emitter.emit();
         }

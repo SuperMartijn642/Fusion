112553bcae1c09cd90e2a06786d8f57e3e71c464
SuperMartijn624 <martijnvandenbrand@outlook.com>
2025-01-20 19:39:12 +0100

    Implement `CustomRenderTypeBakedModel` in `WrappedBakedModel`

--- src/main/java/com/supermartijn642/fusion/model/WrappedBakedModel.java
+++ src/main/java/com/supermartijn642/fusion/model/WrappedBakedModel.java
@@ -1,23 +1,26 @@
 package com.supermartijn642.fusion.model;
 
+import com.supermartijn642.fusion.model.types.base.CustomRenderTypeBakedModel;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.client.renderer.block.model.BakedQuad;
 import net.minecraft.client.renderer.block.model.IBakedModel;
 import net.minecraft.client.renderer.block.model.ItemCameraTransforms;
 import net.minecraft.client.renderer.block.model.ItemOverrideList;
 import net.minecraft.client.renderer.texture.TextureAtlasSprite;
+import net.minecraft.util.BlockRenderLayer;
 import net.minecraft.util.EnumFacing;
 import org.apache.commons.lang3.tuple.Pair;
 
 import javax.annotation.Nullable;
 import javax.vecmath.Matrix4f;
+import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 
 /**
  * Created 27/04/2023 by SuperMartijn642
  */
-public class WrappedBakedModel implements IBakedModel {
+public class WrappedBakedModel implements IBakedModel, CustomRenderTypeBakedModel {
 
     protected final IBakedModel original;
 
@@ -68,5 +71,10 @@ public class WrappedBakedModel implements IBakedModel {
     @Override
     public ItemOverrideList getOverrides(){
         return this.original.getOverrides();
+    }
+
+    @Override
+    public Collection<BlockRenderLayer> getBlockRenderTypes(){
+        return this.original instanceof CustomRenderTypeBakedModel ? ((CustomRenderTypeBakedModel)this.original).getBlockRenderTypes() : Collections.emptyList();
     }
 }

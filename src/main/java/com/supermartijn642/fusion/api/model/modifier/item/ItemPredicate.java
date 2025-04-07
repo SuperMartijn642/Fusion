8d60f1af978067d7247f6b28f151ca17dae6833e
SuperMartijn624 <martijnvandenbrand@outlook.com>
2025-04-07 20:30:31 +0200

    Add data provider for item model modifiers

---
+++ src/main/java/com/supermartijn642/fusion/api/model/modifier/item/ItemPredicate.java
@@ -0,0 +1,46 @@
+package com.supermartijn642.fusion.api.model.modifier.item;
+
+import com.supermartijn642.fusion.api.util.Serializer;
+import net.minecraft.world.item.ItemStack;
+
+/**
+ * A predicate which takes an item stack.
+ * <p>
+ * Created 20/09/2024 by SuperMartijn642
+ */
+public interface ItemPredicate {
+
+    boolean test(ItemStack stack);
+
+    /**
+     * @return the serializer for this predicate
+     */
+    Serializer<? extends ItemPredicate> getSerializer();
+
+    /**
+     * Adds a requirement to this predicate.
+     */
+    default ItemPredicate and(ItemPredicate... predicates){
+        ItemPredicate[] allPredicates = new ItemPredicate[predicates.length + 1];
+        allPredicates[0] = this;
+        System.arraycopy(predicates, 0, allPredicates, 1, predicates.length);
+        return DefaultItemPredicates.and(allPredicates);
+    }
+
+    /**
+     * Adds an alternative to this predicate.
+     */
+    default ItemPredicate or(ItemPredicate... predicates){
+        ItemPredicate[] allPredicates = new ItemPredicate[predicates.length + 1];
+        allPredicates[0] = this;
+        System.arraycopy(predicates, 0, allPredicates, 1, predicates.length);
+        return DefaultItemPredicates.or(allPredicates);
+    }
+
+    /**
+     * Negates the output of this predicate.
+     */
+    default ItemPredicate negate(){
+        return DefaultItemPredicates.not(this);
+    }
+}

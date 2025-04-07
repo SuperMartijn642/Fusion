a2b14987882b0c93f9db971771071070a0633787
SuperMartijn624 <martijnvandenbrand@outlook.com>
2025-04-07 20:14:10 +0200

    Add data provider for item model modifiers

--- src/main/java/com/supermartijn642/fusion/model/modifiers/item/ItemModelModifierBakedModel.java
+++ src/main/java/com/supermartijn642/fusion/model/modifiers/item/ItemModelModifierBakedModel.java
@@ -1,8 +1,8 @@
 package com.supermartijn642.fusion.model.modifiers.item;
 
 import com.mojang.blaze3d.vertex.PoseStack;
+import com.supermartijn642.fusion.api.model.modifier.item.ItemPredicate;
 import com.supermartijn642.fusion.api.util.Pair;
-import com.supermartijn642.fusion.model.modifiers.item.predicates.ItemPredicate;
 import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.block.model.BakedOverrides;
 import net.minecraft.client.renderer.block.model.BakedQuad;

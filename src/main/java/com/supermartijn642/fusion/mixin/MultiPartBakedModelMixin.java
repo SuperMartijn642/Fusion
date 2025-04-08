1dcb1b06adb2f528f9d10663984e0255057a7f37
SuperMartijn624 <martijnvandenbrand@outlook.com>
2025-04-08 04:11:27 +0200

    Fix `MultiPartBakedModel` mixin

--- src/main/java/com/supermartijn642/fusion/mixin/MultiPartBakedModelMixin.java
+++ src/main/java/com/supermartijn642/fusion/mixin/MultiPartBakedModelMixin.java
@@ -7,8 +7,7 @@ import net.minecraft.world.level.BlockAndTintGetter;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraftforge.client.extensions.IForgeBakedModel;
 import net.minecraftforge.client.model.data.ModelData;
-import net.minecraftforge.client.model.data.ModelProperty;
-import org.apache.commons.lang3.tuple.Pair;
+import net.minecraftforge.client.model.data.MultipartModelData;
 import org.jetbrains.annotations.NotNull;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
@@ -16,9 +15,10 @@ import org.spongepowered.asm.mixin.Shadow;
 
 import java.util.BitSet;
 import java.util.List;
-import java.util.function.Predicate;
 
 /**
+ * TODO: this mixin should be removed once <a href="https://github.com/MinecraftForge/MinecraftForge/issues/10523">MinecraftForge#10523</a> is fixed
+ * <p>
  * Created 04/07/2023 by SuperMartijn642
  */
 @Mixin(value = MultiPartBakedModel.class, priority = 900)
@@ -26,26 +26,28 @@ public class MultiPartBakedModelMixin implements IForgeBakedModel {
 
     @Final
     @Shadow
-    private List<Pair<Predicate<BlockState>, BakedModel>> selectors;
+    private List<MultiPartBakedModel.Selector> selectors;
 
     @Override
     public @NotNull ModelData getModelData(@NotNull BlockAndTintGetter level, @NotNull BlockPos pos, @NotNull BlockState state, @NotNull ModelData modelData){
         if(state == null)
-            return IForgeBakedModel.super.getModelData(level, pos, state, modelData);
+            return modelData;
 
         // Combine the model data from all the sub-models, so it doesn't get lost
-        ModelData.Builder builder = ModelData.builder();
-
+        MultipartModelData.Builder builder = null;
         //noinspection DataFlowIssue
         BitSet bitSet = ((MultiPartBakedModel)(Object)this).getSelectors(state);
         for(int i = 0; i < bitSet.length(); ++i){
             if(bitSet.get(i)){
-                ModelData subData = this.selectors.get(i).getRight().getModelData(level, pos, state, modelData);
-                //noinspection unchecked
-                subData.getProperties().forEach(property -> builder.with((ModelProperty<Object>)property, subData.get(property)));
+                BakedModel model = this.selectors.get(i).model();
+                ModelData subData = model.getModelData(level, pos, state, modelData);
+                if(subData != modelData){
+                    if(builder == null)
+                        builder = MultipartModelData.builder();
+                    builder.with(model, subData);
+                }
             }
         }
-
-        return builder.build();
+        return builder == null ? modelData : ModelData.builder().with(MultipartModelData.PROPERTY, builder.build()).build();
     }
 }

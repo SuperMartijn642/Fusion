adef8e3af4f662ffc7c405d3c87d76c688643b4a
SuperMartijn624 <martijnvandenbrand@outlook.com>
2025-04-21 23:23:10 +0200

    Combine position model data property for base and connecting models

--- src/main/java/com/supermartijn642/fusion/FusionClient.java
+++ src/main/java/com/supermartijn642/fusion/FusionClient.java
@@ -9,6 +9,7 @@ import com.supermartijn642.fusion.api.texture.FusionTextureTypeRegistry;
 import com.supermartijn642.fusion.api.texture.data.BaseTextureData;
 import com.supermartijn642.fusion.entity.model.predicates.*;
 import com.supermartijn642.fusion.model.modifiers.item.predicates.*;
+import com.supermartijn642.fusion.model.types.base.BaseBakedModel;
 import com.supermartijn642.fusion.model.types.connecting.ConnectingBakedModel;
 import com.supermartijn642.fusion.model.types.connecting.predicates.*;
 import com.supermartijn642.fusion.texture.FusionTextureMetadataSection;
@@ -91,7 +92,7 @@ public class FusionClient {
         // Integration with FramedBlocks
         FMLJavaModLoadingContext.get().getModEventBus().addListener((Consumer<InterModEnqueueEvent>)event -> {
             InterModComms.sendTo("framedblocks", "add_ct_property", () -> ConnectingBakedModel.PREDICATES_EVALUATION_PROPERTY);
-            InterModComms.sendTo("framedblocks", "add_ct_property", () -> ConnectingBakedModel.POSITION_PROPERTY);
+            InterModComms.sendTo("framedblocks", "add_ct_property", () -> BaseBakedModel.POSITION_PROPERTY);
         });
     }
 

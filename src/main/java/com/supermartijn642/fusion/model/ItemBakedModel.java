74df3c65b9cb9999646626521720822e6c400bfe
SuperMartijn624 <martijnvandenbrand@outlook.com>
2025-07-13 01:09:57 +0200

    Only emit item quads for null cull direction

--- src/main/java/com/supermartijn642/fusion/model/ItemBakedModel.java
+++ src/main/java/com/supermartijn642/fusion/model/ItemBakedModel.java
@@ -15,6 +15,7 @@ import net.neoforged.neoforge.client.model.data.ModelData;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
+import java.util.Collections;
 import java.util.List;
 
 /**
@@ -34,11 +35,15 @@ public abstract class ItemBakedModel extends WrappedBakedModel {
 
     @Override
     public @NotNull List<BakedQuad> getQuads(@Nullable BlockState state, @Nullable Direction cullDirection, @NotNull RandomSource random, @NotNull ModelData data, @Nullable RenderType renderType){
+        if(cullDirection != null)
+            return Collections.emptyList();
         return this.getQuads(this.stack, this.fabulous, random, data, renderType);
     }
 
     @Override
     public List<BakedQuad> getQuads(@Nullable BlockState state, @Nullable Direction cullDirection, RandomSource random){
+        if(cullDirection != null)
+            return Collections.emptyList();
         return this.getQuads(this.stack, this.fabulous, random, ModelData.EMPTY, null);
     }
 

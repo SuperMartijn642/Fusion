00b18d8850e1408825054018d3fef143d8195abc
SuperMartijn624 <martijnvandenbrand@outlook.com>
2025-07-13 01:10:28 +0200

    Make sure render type order of item quads is maintained

--- src/main/java/com/supermartijn642/fusion/model/types/base/BaseBakedModel.java
+++ src/main/java/com/supermartijn642/fusion/model/types/base/BaseBakedModel.java
@@ -64,7 +64,7 @@ public class BaseBakedModel implements BakedModel {
         Map<RenderType,List<TaggedBakedQuad>[]> blockMesh = new HashMap<>();
         Set<RenderType> blockRenderTypes = new HashSet<>();
         Map<RenderType,List<BakedQuad>> itemMesh = new HashMap<>();
-        Set<RenderType> itemRenderTypes = new HashSet<>();
+        Set<RenderType> itemRenderTypes = new LinkedHashSet<>();
         HashMap<TextureAtlasSprite,Integer> sprites = new HashMap<>();
         boolean hasSpecialQuads = false;
         MutableQuad mutableQuad = new MutableQuad();
--- src/main/java/com/supermartijn642/fusion/model/types/connecting/ConnectingBakedModel.java
+++ src/main/java/com/supermartijn642/fusion/model/types/connecting/ConnectingBakedModel.java
@@ -126,7 +126,7 @@ public class ConnectingBakedModel implements BakedModel {
         Map<RenderType,List<TaggedBakedQuad>[]> blockMesh = new HashMap<>();
         Set<RenderType> blockRenderTypes = new HashSet<>();
         Map<RenderType,List<BakedQuad>> itemMesh = new HashMap<>();
-        Set<RenderType> itemRenderTypes = new HashSet<>();
+        Set<RenderType> itemRenderTypes = new LinkedHashSet<>();
         HashMap<QuadPredicates,Integer> predicates = new HashMap<>();
         HashMap<TextureAtlasSprite,Integer> sprites = new HashMap<>();
         boolean hasSpecialQuads = false;

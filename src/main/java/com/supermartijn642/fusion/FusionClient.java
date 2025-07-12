3cc12292968490820f71902a48489bd5f6a1db0b
SuperMartijn624 <martijnvandenbrand@outlook.com>
2025-07-12 23:56:43 +0200

    Disable ambient occlusion for emissive quads #110

--- src/main/java/com/supermartijn642/fusion/FusionClient.java
+++ src/main/java/com/supermartijn642/fusion/FusionClient.java
@@ -96,7 +96,7 @@ public class FusionClient implements ClientModInitializer {
                 materialFinder.blendMode(0, mode);
             }
             if(emissive)
-                materialFinder.emissive(true).disableDiffuse(true);
+                materialFinder.emissive(true).disableDiffuse(true).ambientOcclusion(TriState.FALSE);
             material = materialFinder.find();
             RENDER_MATERIALS[index] = material;
         }

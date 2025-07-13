02ed384bc914e3c8181cb862fa42e8de87e540e9
SuperMartijn624 <martijnvandenbrand@outlook.com>
2025-07-13 16:57:15 +0200

    Use `ModifyReturnValue` instead of cancelling at return #138

--- src/main/java/com/supermartijn642/fusion/mixin/ItemModelShaperMixin.java
+++ src/main/java/com/supermartijn642/fusion/mixin/ItemModelShaperMixin.java
@@ -1,13 +1,12 @@
 package com.supermartijn642.fusion.mixin;
 
+import com.llamalad7.mixinextras.injector.ModifyReturnValue;
 import com.supermartijn642.fusion.model.modifiers.item.ItemModelModifierBakedModel;
 import net.minecraft.client.renderer.ItemModelShaper;
 import net.minecraft.client.resources.model.BakedModel;
 import net.minecraft.world.item.ItemStack;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 /**
  * Created 20/09/2024 by SuperMartijn642
@@ -15,14 +14,13 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 @Mixin(ItemModelShaper.class)
 public class ItemModelShaperMixin {
 
-    @Inject(
+    @ModifyReturnValue(
         method = "getItemModel(Lnet/minecraft/world/item/ItemStack;)Lnet/minecraft/client/resources/model/BakedModel;",
-        at = @At("RETURN"),
-        cancellable = true
+        at = @At("RETURN")
     )
-    public void resolveItemPredicatesModel(ItemStack stack, CallbackInfoReturnable<BakedModel> ci){
-        BakedModel model = ci.getReturnValue();
+    public BakedModel resolveItemPredicatesModel(BakedModel model, ItemStack stack){
         if(model instanceof ItemModelModifierBakedModel)
-            ci.setReturnValue(((ItemModelModifierBakedModel)model).forStack(stack));
+            return ((ItemModelModifierBakedModel)model).forStack(stack);
+        return model;
     }
 }

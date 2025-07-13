f8e2defb93f8ffddcb12e584b98efe2844da2d45
SuperMartijn624 <martijnvandenbrand@outlook.com>
2025-07-13 18:59:03 +0200

    Clear fusion texture metadata cache #143

--- src/main/java/com/supermartijn642/fusion/mixin/SpriteLoaderMixin.java
+++ src/main/java/com/supermartijn642/fusion/mixin/SpriteLoaderMixin.java
@@ -111,6 +111,7 @@ public class SpriteLoaderMixin {
                     textures.put(identifier, newTexture);
                 }
             }
+            fusionTextureMetadata.clear();
             return preparations;
         });
     }

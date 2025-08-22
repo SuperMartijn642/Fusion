08cdb83241ec9bf3e153722a820fe608e799a18c
SuperMartijn624 <martijnvandenbrand@outlook.com>
2025-08-22 14:25:17 +0200

    Only overwrite models that are actually present in model modifiers #157

--- src/main/java/com/supermartijn642/fusion/model/modifiers/item/ItemModelModifierReloadListener.java
+++ src/main/java/com/supermartijn642/fusion/model/modifiers/item/ItemModelModifierReloadListener.java
@@ -56,6 +56,7 @@ public class ItemModelModifierReloadListener {
         for(Map.Entry<ModelResourceLocation,ItemModelPredicatesProperties> entry : this.models.entrySet()){
             ModelResourceLocation target = entry.getKey();
             ItemModelPredicatesProperties properties = entry.getValue();
+            if(!bakedModels.containsKey(target)) continue;
             IBakedModel defaultModel = properties.defaultModel == null ? bakedModels.get(target) : bakedModels.get(properties.defaultModel);
             List<Pair<ItemPredicate,IBakedModel>> models = properties.models.stream()
                 .map(pair -> pair.mapRight(bakedModels::get))

fcbd3329121c4b90ac42c5badc921b7a8bc1d3b6
SuperMartijn624 <martijnvandenbrand@outlook.com>
2025-08-22 14:11:50 +0200

    Fix wrong texture being used for tinting in some cases #155 #158

--- src/main/java/com/supermartijn642/fusion/mixin/indium/ItemRenderContextMixinIndium.java
+++ src/main/java/com/supermartijn642/fusion/mixin/indium/ItemRenderContextMixinIndium.java
@@ -31,9 +31,10 @@ public class ItemRenderContextMixinIndium {
         // In case texture has a custom tinting set, replace the original tinting
         if(colorIndex == 39216){
             TextureAtlasSprite sprite = quad.cachedSprite();
-            if(sprite == null){
-                sprite = SpriteFinder.get(Minecraft.getInstance().getModelManager().getAtlas(TextureAtlases.getBlocks())).find(quad);
-                if(!(sprite instanceof BaseTextureSprite)){
+            if(!(sprite instanceof BaseTextureSprite)){
+                if(quad.tag() == 0)
+                    sprite = SpriteFinder.get(Minecraft.getInstance().getModelManager().getAtlas(TextureAtlases.getBlocks())).find(quad);
+                else{
                     float u = (quad.tag() & 65535) / 65535f;
                     float v = (quad.tag() >> 16) / 65535f;
                     sprite = SpriteFinder.get(Minecraft.getInstance().getModelManager().getAtlas(TextureAtlases.getBlocks())).find(u, v);

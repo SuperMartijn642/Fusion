effaa297d23aa55617df444dcc241dc697bab673
SuperMartijn624 <martijnvandenbrand@outlook.com>
2025-12-20 16:40:23 +0100

    Update Sodium integration #202

--- src/main/java/com/supermartijn642/fusion/mixin/sodium/ItemRenderContextMixinSodium.java
+++ src/main/java/com/supermartijn642/fusion/mixin/sodium/ItemRenderContextMixinSodium.java
@@ -5,8 +5,9 @@ import com.supermartijn642.fusion.texture.QuadTintingHelper;
 import com.supermartijn642.fusion.texture.types.base.BaseTextureSprite;
 import com.supermartijn642.fusion.util.TextureAtlases;
 import net.caffeinemc.mods.sodium.api.util.ColorMixer;
-import net.caffeinemc.mods.sodium.client.render.frapi.mesh.MutableQuadViewImpl;
 import net.caffeinemc.mods.sodium.client.render.frapi.render.ItemRenderContext;
+import net.caffeinemc.mods.sodium.client.render.model.MutableQuadViewImpl;
+import net.caffeinemc.mods.sodium.client.render.texture.ExtendedTextureAtlas;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.texture.TextureAtlasSprite;
 import org.spongepowered.asm.mixin.Mixin;
@@ -28,22 +29,24 @@ public class ItemRenderContextMixinSodium {
     )
     private void tintQuad(MutableQuadViewImpl quad, CallbackInfo ci){
         // In case texture has a custom tinting set, replace the original tinting
-        if(quad.tintIndex() == 39216){
-            TextureAtlasSprite sprite;
-            if(quad.tag() == 0)
-                sprite = quad.sprite(Minecraft.getInstance().getAtlasManager().getAtlasOrThrow(TextureAtlases.getBlocks()).spriteFinder());
-            else{
-                float u = ((quad.tag() >> 4) & 16383) / 16383f;
-                float v = (quad.tag() >> 18) / 16383f;
-                sprite = Minecraft.getInstance().getAtlasManager().getAtlasOrThrow(TextureAtlases.getBlocks()).spriteFinder().find(u, v);
-                quad.cachedSprite(sprite);
+        if(quad.getTintIndex() == 39216){
+            TextureAtlasSprite sprite = quad.cachedSprite();
+            if(!(sprite instanceof BaseTextureSprite)){
+                if(quad.getTag() == 0)
+                    sprite = quad.sprite(((ExtendedTextureAtlas)Minecraft.getInstance().getAtlasManager().getAtlasOrThrow(TextureAtlases.getBlocks())).sodium$getSpriteFinder());
+                else{
+                    float u = ((quad.getTag() >> 4) & 16383) / 16383f;
+                    float v = (quad.getTag() >> 18) / 16383f;
+                    sprite = Minecraft.getInstance().getAtlasManager().getAtlasOrThrow(TextureAtlases.getBlocks()).spriteFinder().find(u, v);
+                    quad.cachedSprite(sprite);
+                }
             }
             if(sprite instanceof BaseTextureSprite){
                 BaseTextureData.QuadTinting tinting = ((BaseTextureSprite)sprite).data().getTinting();
                 if(tinting != null){
                     int color = 0xFF000000 | QuadTintingHelper.getColor(tinting, null, null, null);
                     for(int i = 0; i < 4; i++)
-                        quad.color(i, ColorMixer.mulComponentWise(color, quad.color(i)));
+                        quad.setColor(i, ColorMixer.mulComponentWise(color, quad.getColor(i)));
                     ci.cancel();
                 }
             }

55f2f28c6c82ac6d713b89a1e935c735d49c2c88
SuperMartijn624 <martijnvandenbrand@outlook.com>
2026-01-13 05:52:35 +0100

    Fix handling of connections key references in connecting models

--- src/main/java/com/supermartijn642/fusion/model/types/connecting/ConnectingModelDataBuilderImpl.java
+++ src/main/java/com/supermartijn642/fusion/model/types/connecting/ConnectingModelDataBuilderImpl.java
@@ -5,13 +5,13 @@ import com.supermartijn642.fusion.api.model.data.BaseModelDataBuilder;
 import com.supermartijn642.fusion.api.model.data.ConnectingModelData;
 import com.supermartijn642.fusion.api.model.data.ConnectingModelDataBuilder;
 import com.supermartijn642.fusion.api.predicate.ConnectionPredicate;
-import com.supermartijn642.fusion.api.predicate.DefaultConnectionPredicates;
-import com.supermartijn642.fusion.api.util.Pair;
+import com.supermartijn642.fusion.api.util.Either;
 import com.supermartijn642.fusion.model.types.base.BaseModelDataImpl;
 import net.minecraft.resources.Identifier;
 
-import java.util.*;
-import java.util.stream.Collectors;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * Created 02/05/2023 by SuperMartijn642
@@ -19,7 +19,7 @@ import java.util.stream.Collectors;
 public class ConnectingModelDataBuilderImpl implements ConnectingModelDataBuilder {
 
     private final BaseModelDataBuilder<?,BaseModelData> baseModel = BaseModelData.builder();
-    private final Map<String,List<ConnectionPredicate>> predicates = new HashMap<>();
+    private final Map<String,Either<ConnectionPredicate,String>> connections = new LinkedHashMap<>(); // This should maintain order
 
     @Override
     public ConnectingModelDataBuilder parent(Identifier parent){
@@ -46,23 +46,40 @@ public class ConnectingModelDataBuilderImpl implements ConnectingModelDataBuilde
     }
 
     @Override
-    public ConnectingModelDataBuilder connection(ConnectionPredicate predicate){
-        return this.connection("default", predicate);
+    public ConnectingModelDataBuilder defaultConnections(ConnectionPredicate predicate){
+        return this.connections("default", predicate);
     }
 
     @Override
-    public ConnectingModelDataBuilder connection(String texture, ConnectionPredicate predicate){
-        this.predicates.computeIfAbsent("default", s -> new ArrayList<>()).add(predicate);
+    public ConnectingModelDataBuilder connections(String key, ConnectionPredicate predicate){
+        if(!key.matches("[a-zA-Z_]*"))
+            throw new IllegalArgumentException("Connections key must only contain characters [a-zA-Z_]!");
+        if(this.connections.containsKey(key))
+            throw new RuntimeException("Duplicate connections for key '" + key + "'!");
+
+        this.connections.put(key, Either.left(predicate));
+        return this;
+    }
+
+    @Override
+    public ConnectingModelDataBuilder connections(String key, String reference){
+        if(!key.matches("[a-zA-Z_]*"))
+            throw new IllegalArgumentException("Connections key must only contain characters [a-zA-Z_]!");
+        if(this.connections.containsKey(key))
+            throw new RuntimeException("Duplicate connections entry for key '" + key + "'!");
+
+        // Remove leading '#'
+        if(reference.charAt(0) == '#')
+            reference = reference.substring(1);
+
+        this.connections.put(key, Either.right(reference));
         return this;
     }
 
     @Override
     public ConnectingModelData build(){
         BaseModelDataImpl baseData = (BaseModelDataImpl)this.baseModel.build();
-        Map<String,ConnectionPredicate> predicates = this.predicates.entrySet().stream()
-            .map(entry -> Pair.of(entry.getKey(), DefaultConnectionPredicates.or(entry.getValue().toArray(ConnectionPredicate[]::new))))
-            .collect(Collectors.toUnmodifiableMap(Pair::left, Pair::right));
         //noinspection rawtypes,unchecked
-        return new ConnectingModelDataImpl(baseData.getVanillaModel(), baseData.getParents(), (List)baseData.getElements(), predicates, Collections.emptyMap());
+        return new ConnectingModelDataImpl(baseData.getVanillaModel(), baseData.getParents(), (List)baseData.getElements(), this.connections);
     }
 }

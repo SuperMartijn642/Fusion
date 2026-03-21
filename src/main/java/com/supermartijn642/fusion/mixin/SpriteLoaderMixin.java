a7a9feeb0f7d4b5c49673e2e6481e24d9a0ea50e
SuperMartijn624 <martijnvandenbrand@outlook.com>
2026-03-21 02:58:37 +0100

    Overhaul `TextureType`s and how textures are loaded
    Allow `TextureType`s to submit multiple texture atlas entries.
    Abstract static vs animated textures.
    Separate random and connecting texture tiles into separate atlas entries.

--- src/main/java/com/supermartijn642/fusion/mixin/SpriteLoaderMixin.java
+++ src/main/java/com/supermartijn642/fusion/mixin/SpriteLoaderMixin.java
@@ -1,23 +1,19 @@
 package com.supermartijn642.fusion.mixin;
 
-import com.supermartijn642.fusion.api.texture.TextureType;
-import com.supermartijn642.fusion.api.util.Pair;
-import com.supermartijn642.fusion.extensions.SpriteContentsExtension;
-import com.supermartijn642.fusion.extensions.TextureAtlasSpriteExtension;
-import com.supermartijn642.fusion.texture.SpriteCreationContextImpl;
-import com.supermartijn642.fusion.texture.TextureTypeRegistryImpl;
+import com.supermartijn642.fusion.texture.TextureCreationHandler;
+import net.minecraft.client.renderer.texture.SpriteContents;
 import net.minecraft.client.renderer.texture.SpriteLoader;
+import net.minecraft.client.renderer.texture.Stitcher;
 import net.minecraft.client.renderer.texture.TextureAtlasSprite;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.server.packs.resources.ResourceManager;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
+import java.util.List;
 import java.util.Map;
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.Executor;
 
 /**
  * Created 21/05/2023 by SuperMartijn642
@@ -25,35 +21,20 @@ import java.util.concurrent.Executor;
 @Mixin(value = SpriteLoader.class, priority = 900)
 public class SpriteLoaderMixin {
 
+    @ModifyVariable(
+        method = "stitch",
+        at = @At("HEAD"),
+        ordinal = 0
+    )
+    private List<SpriteContents> flattenSprites(List<SpriteContents> sprites){
+        return TextureCreationHandler.onStitchSprites(sprites);
+    }
+
     @Inject(
-        method = "loadAndStitch",
+        method = "getStitchedSprites",
         at = @At("RETURN")
     )
-    private void initializeTextures(ResourceManager resourceManager, ResourceLocation atlas, int i, Executor executor, CallbackInfoReturnable<CompletableFuture<SpriteLoader.Preparations>> ci){
-        ci.getReturnValue().thenApply(preparations -> {
-            // Replace sprites
-            Map<ResourceLocation,TextureAtlasSprite> textures = preparations.regions();
-            for(Map.Entry<ResourceLocation,TextureAtlasSprite> entry : textures.entrySet()){
-                ResourceLocation identifier = entry.getKey();
-                TextureAtlasSprite texture = entry.getValue();
-                Pair<TextureType<Object>,Object> textureData = ((SpriteContentsExtension)texture.contents()).fusionTextureMetadata();
-                if(textureData != null){
-                    // Create the sprite
-                    TextureAtlasSprite newTexture;
-                    try(SpriteCreationContextImpl context = new SpriteCreationContextImpl(preparations, atlas, texture)){
-                        newTexture = textureData.left().createSprite(context, textureData.right());
-                    }catch(Exception e){
-                        throw new RuntimeException("Encountered an exception whilst initialising texture '" + identifier + "' for texture type '" + TextureTypeRegistryImpl.getIdentifier(textureData.left()) + "'!", e);
-                    }
-                    if(newTexture == null)
-                        throw new RuntimeException("Received null texture from texture type '" + TextureTypeRegistryImpl.getIdentifier(textureData.left()) + "' for texture '" + identifier + "'!");
-                    ((TextureAtlasSpriteExtension)newTexture).setFusionTextureType(textureData.left());
-                    // Replace the current texture
-                    textures.put(identifier, newTexture);
-                    ((SpriteContentsExtension)texture.contents()).clearFusionTextureMetadata();
-                }
-            }
-            return preparations;
-        });
+    private void initializeTextures(Stitcher<SpriteContents> stitcher, int atlasWidth, int atlasHeight, CallbackInfoReturnable<Map<ResourceLocation,TextureAtlasSprite>> ci){
+        TextureCreationHandler.afterLoadSprites(ci.getReturnValue(), atlasWidth, atlasHeight, stitcher);
     }
 }

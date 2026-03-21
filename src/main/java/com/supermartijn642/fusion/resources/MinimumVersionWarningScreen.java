9cbba51912cade4328e5373e9603439fed51d87c
SuperMartijn624 <martijnvandenbrand@outlook.com>
2026-03-21 14:06:33 +0100

    Add `Fusion.MODID` field

--- src/main/java/com/supermartijn642/fusion/resources/MinimumVersionWarningScreen.java
+++ src/main/java/com/supermartijn642/fusion/resources/MinimumVersionWarningScreen.java
@@ -1,5 +1,6 @@
 package com.supermartijn642.fusion.resources;
 
+import com.supermartijn642.fusion.Fusion;
 import com.supermartijn642.fusion.FusionClient;
 import com.supermartijn642.fusion.extensions.PackExtension;
 import net.minecraft.ChatFormatting;
@@ -27,7 +28,7 @@ import java.util.function.Consumer;
  */
 public class MinimumVersionWarningScreen extends Screen {
 
-    private static final ResourceLocation FUSION_LOGO = ResourceLocation.fromNamespaceAndPath("fusion", "textures/resourcepacks/fusion_icon.png");
+    private static final ResourceLocation FUSION_LOGO = Fusion.identifier("textures/resourcepacks/fusion_icon.png");
 
     private final PackSelectionModel.EntryBase pack;
     private final Consumer<Boolean> confirmation;

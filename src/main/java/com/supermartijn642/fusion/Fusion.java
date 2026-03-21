fb8755e34a9024f05b1c95b482639955861cc8c3
SuperMartijn624 <martijnvandenbrand@outlook.com>
2026-03-21 20:40:18 +0100

    Add `Fusion.MODID` field

--- src/main/java/com/supermartijn642/fusion/Fusion.java
+++ src/main/java/com/supermartijn642/fusion/Fusion.java
@@ -1,5 +1,6 @@
 package com.supermartijn642.fusion;
 
+import net.minecraft.resources.ResourceLocation;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.fml.DistExecutor;
 import net.minecraftforge.fml.IExtensionPoint;
@@ -12,9 +13,15 @@ import net.minecraftforge.fml.common.Mod;
 @Mod("fusion")
 public class Fusion {
 
+    public static final String MODID = "fusion";
+
+    public static ResourceLocation identifier(String path){
+        return new ResourceLocation(MODID, path);
+    }
+
     public Fusion(){
         // Accept any version from the server
-        ModLoadingContext.get().registerExtensionPoint(IExtensionPoint.DisplayTest.class, () -> new IExtensionPoint.DisplayTest(() -> "", (a,b) -> true));
+        ModLoadingContext.get().registerExtensionPoint(IExtensionPoint.DisplayTest.class, () -> new IExtensionPoint.DisplayTest(() -> "", (a, b) -> true));
         // Initialize Fusion stuff if this is on the client
         DistExecutor.safeRunWhenOn(Dist.CLIENT, () -> FusionClient::init);
     }

9764e973ddc603aa3dc3a2427b6fc9114a402584
SuperMartijn624 <martijnvandenbrand@outlook.com>
2026-03-21 20:40:18 +0100

    Add `Fusion.MODID` field

--- src/main/java/com/supermartijn642/fusion/resources/ResourcePackListTipRenderer.java
+++ src/main/java/com/supermartijn642/fusion/resources/ResourcePackListTipRenderer.java
@@ -1,6 +1,7 @@
 package com.supermartijn642.fusion.resources;
 
 import com.mojang.blaze3d.systems.RenderSystem;
+import com.supermartijn642.fusion.Fusion;
 import net.minecraft.ChatFormatting;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiGraphics;
@@ -16,7 +17,7 @@ import java.util.function.Consumer;
  */
 public class ResourcePackListTipRenderer {
 
-    private static final ResourceLocation FUSION_LOGO = new ResourceLocation("fusion", "textures/resourcepacks/fusion_icon_blurred.png");
+    private static final ResourceLocation FUSION_LOGO = Fusion.identifier("textures/resourcepacks/fusion_icon_blurred.png");
 
     public static void renderBackground(FusionPackMetadata metadata, boolean isVanillaCompatible, GuiGraphics graphics, int x, int y, int width, int height){
         if(isVanillaCompatible && !metadata.isMinVersionSatisfied())

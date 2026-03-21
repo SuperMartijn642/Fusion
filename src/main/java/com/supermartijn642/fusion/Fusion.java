8d4c4c38d586db9dd2b1ba59f912e9fe5500b28d
SuperMartijn624 <martijnvandenbrand@outlook.com>
2026-03-21 14:06:33 +0100

    Add `Fusion.MODID` field

--- src/main/java/com/supermartijn642/fusion/Fusion.java
+++ src/main/java/com/supermartijn642/fusion/Fusion.java
@@ -1,11 +1,18 @@
 package com.supermartijn642.fusion;
 
 import net.fabricmc.api.ModInitializer;
+import net.minecraft.resources.ResourceLocation;
 
 /**
  * Created 26/04/2023 by SuperMartijn642
  */
 public class Fusion implements ModInitializer {
+
+    public static final String MODID = "fusion";
+
+    public static ResourceLocation identifier(String path){
+        return ResourceLocation.fromNamespaceAndPath(MODID, path);
+    }
 
     @Override
     public void onInitialize(){

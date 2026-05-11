2f44656ad49524d7b98c7558c8e82f4e096c36c2
SuperMartijn624 <martijnvandenbrand@outlook.com>
2026-05-12 00:25:14 +0200

    Make logging for user errors more easily readable

--- src/main/java/com/supermartijn642/fusion/entity/EntityModelModifierReloadListener.java
+++ src/main/java/com/supermartijn642/fusion/entity/EntityModelModifierReloadListener.java
@@ -1,13 +1,13 @@
 package com.supermartijn642.fusion.entity;
 
 import com.google.gson.*;
-import com.supermartijn642.fusion.FusionClient;
 import com.supermartijn642.fusion.api.util.Either;
 import com.supermartijn642.fusion.api.util.Pair;
 import com.supermartijn642.fusion.entity.model.predicates.AndEntityModelPredicate;
 import com.supermartijn642.fusion.entity.model.predicates.EntityModelPredicate;
 import com.supermartijn642.fusion.entity.model.predicates.EntityModelPredicateRegistry;
 import com.supermartijn642.fusion.util.IdentifierUtil;
+import com.supermartijn642.fusion.util.LoggingHelper;
 import net.minecraft.client.model.geom.ModelLayerLocation;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.resources.ResourceLocation;
@@ -53,12 +53,7 @@ public class EntityModelModifierReloadListener {
             try{
                 parseResource(json);
             }catch(JsonParseException e){
-                FusionClient.LOGGER.error("Failed to parse entity model modifier '{}': {}", location, e.getMessage());
-                while(e.getCause() instanceof JsonParseException){
-                    //noinspection AssignmentToCatchBlockParameter
-                    e = (JsonParseException)e.getCause();
-                    FusionClient.LOGGER.error("   {}", e.getMessage());
-                }
+                LoggingHelper.logUserError(e, "Failed to parse entity model modifier '%s':", location);
             }
         }
     }

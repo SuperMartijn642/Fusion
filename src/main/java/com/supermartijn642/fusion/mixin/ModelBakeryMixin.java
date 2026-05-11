013fa1e0579cad63194cba95fa5d45b064fa4693
SuperMartijn624 <martijnvandenbrand@outlook.com>
2026-05-12 00:25:14 +0200

    Make logging for user errors more easily readable

--- src/main/java/com/supermartijn642/fusion/mixin/ModelBakeryMixin.java
+++ src/main/java/com/supermartijn642/fusion/mixin/ModelBakeryMixin.java
@@ -1,14 +1,18 @@
 package com.supermartijn642.fusion.mixin;
 
+import com.supermartijn642.fusion.api.util.UserErrorException;
 import com.supermartijn642.fusion.model.FusionBlockModelData;
 import com.supermartijn642.fusion.model.modifiers.block.BlockModelModifierReloadListener;
 import com.supermartijn642.fusion.model.modifiers.item.ItemModelModifierReloadListener;
+import com.supermartijn642.fusion.util.LoggingHelper;
 import net.minecraft.client.resources.model.ModelBakery;
 import net.minecraft.client.resources.model.ModelResourceLocation;
 import net.minecraft.resources.ResourceLocation;
+import org.apache.logging.log4j.Logger;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
@@ -45,5 +49,22 @@ public class ModelBakeryMixin {
     )
     private void clearBlockModelName(ResourceLocation identifier, CallbackInfoReturnable<?> ci){
         FusionBlockModelData.CURRENT_MODEL.remove();
+    }
+
+    @Redirect(
+        method = "getModel(Lnet/minecraft/resources/ResourceLocation;)Lnet/minecraft/client/resources/model/UnbakedModel;",
+        at = @At(
+            value = "INVOKE",
+            target = "Lorg/apache/logging/log4j/Logger;warn(Ljava/lang/String;Ljava/lang/Object;Ljava/lang/Object;Ljava/lang/Object;)V"
+        )
+    )
+    private void interceptFusionErrors(Logger logger, String message, Object identifier, Object ignore, Object exception){
+        // Report Fusion model user errors in a more readable way
+        Exception e = (Exception)exception;
+        if(e instanceof UserErrorException){
+            LoggingHelper.logUserError(e.getCause(), "Failed to load model '%s':", identifier);
+            return;
+        }
+        logger.warn(message, identifier, ignore, exception);
     }
 }

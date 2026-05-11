2cc6acd37eaa9eea13e961c6133dd0326ee32b89
SuperMartijn624 <martijnvandenbrand@outlook.com>
2026-05-12 00:25:14 +0200

    Make logging for user errors more easily readable

--- src/main/java/com/supermartijn642/fusion/mixin/BlockModelDeserializerMixin.java
+++ src/main/java/com/supermartijn642/fusion/mixin/BlockModelDeserializerMixin.java
@@ -4,6 +4,7 @@ import com.google.gson.JsonDeserializationContext;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonParseException;
 import com.supermartijn642.fusion.api.model.ModelInstance;
+import com.supermartijn642.fusion.api.util.UserErrorException;
 import com.supermartijn642.fusion.model.FusionBlockModelData;
 import com.supermartijn642.fusion.model.ModelTypeRegistryImpl;
 import com.supermartijn642.fusion.util.IdentifierUtil;
@@ -27,11 +28,11 @@ public class BlockModelDeserializerMixin {
     private static final ThreadLocal<Boolean> SHOULD_IGNORE = ThreadLocal.withInitial(() -> false);
 
     @Inject(
-        method = "deserialize(Lcom/google/gson/JsonElement;Ljava/lang/reflect/Type;Lcom/google/gson/JsonDeserializationContext;)Lnet/minecraft/client/resources/model/cuboid/CuboidModel;",
+        method = "deserialize(Lcom/google/gson/JsonElement;Ljava/lang/reflect/Type;Lcom/google/gson/JsonDeserializationContext;)Lnet/minecraft/client/renderer/block/model/BlockModel;",
         at = @At("HEAD"),
         cancellable = true
     )
-    private void deserialize(JsonElement json, Type type, JsonDeserializationContext context, CallbackInfoReturnable<BlockModel> ci) throws JsonParseException{
+    private void deserialize(JsonElement json, Type type, JsonDeserializationContext context, CallbackInfoReturnable<BlockModel> ci) throws JsonParseException, UserErrorException{
         if(SHOULD_IGNORE.get())
             return;
 
@@ -44,6 +45,8 @@ public class BlockModelDeserializerMixin {
                 ModelInstance<?> model;
                 try{
                     model = ModelTypeRegistryImpl.deserializeModelData(json.getAsJsonObject());
+                }catch(JsonParseException e){
+                    throw new UserErrorException("Failed to deserialize Fusion model!", e);
                 }finally{
                     SHOULD_IGNORE.set(false);
                 }

f9cfce5826deab5a0e71571c5846bc400d8c6f58
SuperMartijn624 <martijnvandenbrand@outlook.com>
2026-05-12 00:25:14 +0200

    Make logging for user errors more easily readable

--- src/main/java/com/supermartijn642/fusion/mixin/ModelBakeryMixin.java
+++ src/main/java/com/supermartijn642/fusion/mixin/ModelBakeryMixin.java
@@ -1,14 +1,19 @@
 package com.supermartijn642.fusion.mixin;
 
+import com.supermartijn642.fusion.api.util.UserErrorException;
 import com.supermartijn642.fusion.model.FusionBlockModelData;
 import com.supermartijn642.fusion.model.modifiers.block.BlockModelModifierReloadListener;
 import com.supermartijn642.fusion.model.modifiers.item.ItemModelModifierReloadListener;
+import com.supermartijn642.fusion.util.LoggingHelper;
 import net.minecraft.client.resources.model.ModelBakery;
 import net.minecraft.client.resources.model.ModelResourceLocation;
 import net.minecraft.resources.ResourceLocation;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
@@ -17,6 +22,8 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
  */
 @Mixin(ModelBakery.class)
 public class ModelBakeryMixin {
+
+    private static final Logger log = LoggerFactory.getLogger(ModelBakeryMixin.class);
 
     @Inject(
         method = "loadTopLevel",
@@ -45,5 +52,22 @@ public class ModelBakeryMixin {
     )
     private void clearBlockModelName(ResourceLocation identifier, CallbackInfoReturnable<?> ci){
         FusionBlockModelData.CURRENT_MODEL.remove();
+    }
+
+    @Redirect(
+        method = "getModel(Lnet/minecraft/resources/ResourceLocation;)Lnet/minecraft/client/resources/model/UnbakedModel;",
+        at = @At(
+            value = "INVOKE",
+            target = "Lorg/slf4j/Logger;warn(Ljava/lang/String;[Ljava/lang/Object;)V"
+        )
+    )
+    private void interceptFusionErrors(Logger logger, String message, Object[] arguments){
+        // Report Fusion model user errors in a more readable way
+        Exception e = (Exception)arguments[2];
+        if(e instanceof UserErrorException){
+            LoggingHelper.logUserError(e.getCause(), "Failed to load model '%s':", arguments[0]);
+            return;
+        }
+        logger.warn(message, arguments);
     }
 }

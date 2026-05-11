ca7698eabe3631a8eadf23cd9554d7da69449f19
SuperMartijn624 <martijnvandenbrand@outlook.com>
2026-05-11 16:49:28 +0200

    Overhaul `ModelType`s and model baking behaviour

--- src/main/java/com/supermartijn642/fusion/api/model/ModelType.java
+++ src/main/java/com/supermartijn642/fusion/api/model/ModelType.java
@@ -1,48 +1,103 @@
 package com.supermartijn642.fusion.api.model;
 
+import com.supermartijn642.fusion.api.model.custom.*;
+import com.supermartijn642.fusion.api.model.custom.geometry.ModelGeometry;
+import com.supermartijn642.fusion.api.util.Either;
 import com.supermartijn642.fusion.api.util.Serializer;
 import net.minecraft.client.renderer.block.model.BlockStateModel;
+import net.minecraft.client.renderer.block.model.ItemTransform;
 import net.minecraft.client.renderer.item.ItemModel;
 import net.minecraft.client.resources.model.UnbakedModel;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.ItemDisplayContext;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.Collection;
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 
 /**
+ * An interface for custom model types.
+ * <p>
  * Created 27/04/2023 by SuperMartijn642
+ * @param <T> data used to bake the model
  */
 public interface ModelType<T> extends Serializer<T> {
 
     /**
-     * Gets all the dependencies on other model files.
-     * @param data custom model data
+     * Gets all the dependencies on other unbaked models.
      */
-    Collection<ResourceLocation> getModelDependencies(T data);
+    Collection<ResourceLocation> getDependencies(T data);
 
     /**
-     * Converts the model data into a baked model.
-     * @param context context for baking the model
-     * @param data    custom model data
-     * @return a baked model
-     * @see BlockModelBakingContext
+     * Gets any parent models which the model may inherit properties from.
      */
-    BlockStateModel bakeBlockModel(BlockModelBakingContext context, T data);
-
-    ItemModel bakeItemModel(ItemModelBakingContext context, T data);
+    List<Either<ResourceLocation,ModelInstance<?>>> getParents(T data);
 
     /**
-     * Represents the model as a vanilla {@link UnbakedModel} instance. May be used gather info from other models, such as with the vanilla 'parent' property.
-     * If the model cannot be represented as a {@link UnbakedModel} instance, this method should return {@code null}.
-     * @param data custom model data
-     * @return a representation of the model as a vanilla {@link UnbakedModel} instance, or {@code null} if such a representation is not available
+     * Gets whether the model should be rendered with ambient occlusion.
      */
     @Nullable
-    UnbakedModel getAsVanillaModel(T data);
+    Boolean getAmbientOcclusion(T data);
 
     /**
-     * Gets any 'parent' models which the model may inherit properties from.
+     * Gets the lighting to use when the model is rendered in a gui.
      */
-    List<ResourceLocation> getParentModels(T data);
+    @Nullable
+    UnbakedModel.GuiLight getGuiLight(T data);
+
+    /**
+     * Gets the transformations used to render the model as an item under the given context.
+     */
+    @Nullable
+    ItemTransform getItemTransform(ItemDisplayContext type, T data);
+
+    /**
+     * Gets the material references of the model.
+     */
+    Map<String,Either<String,ModelMaterial>> getMaterials(T data);
+
+    /**
+     * Gets the geometry of the model.
+     */
+    @Nullable
+    ModelGeometry getGeometry(T data);
+
+    /**
+     * Gets whether the model should be shaded.
+     */
+    @Nullable
+    Boolean getShade(T data);
+
+    /**
+     * Gets whether the model is emissive.
+     */
+    @Nullable
+    Boolean getEmissive(T data);
+
+    /**
+     * Gets the transformations that should be applied to the model's geometry.
+     */
+    default ModelTransform getTransform(T data){
+        return ModelTransform.identity();
+    }
+
+    /**
+     * Gets an arbitrary property of this model.
+     * @see ModelProperty
+     */
+    <X, C> Optional<X> getProperty(ModelProperty<X,C> property, C context, T data);
+
+    /**
+     * Creates a block state model from the model data.
+     * @see BlockStateModelBakingContext
+     */
+    BlockStateModel bakeBlockStateModel(BlockStateModelBakingContext context, T data);
+
+    /**
+     * Creates an item model from the model data.
+     * @see ItemModelBakingContext
+     */
+    ItemModel bakeItemModel(ItemModelBakingContext context, T data);
 }
--- src/main/java/com/supermartijn642/fusion/model/ItemModelBakingContextImpl.java
+++ src/main/java/com/supermartijn642/fusion/model/ItemModelBakingContextImpl.java
@@ -1,27 +1,25 @@
 package com.supermartijn642.fusion.model;
 
-import com.supermartijn642.fusion.api.model.ItemModelBakingContext;
+import com.supermartijn642.fusion.api.model.custom.ItemModelBakingContext;
+import com.supermartijn642.fusion.api.model.custom.ModelTransform;
 import net.minecraft.client.color.item.ItemTintSource;
 import net.minecraft.client.model.geom.EntityModelSet;
-import net.minecraft.client.renderer.block.model.ItemTransforms;
-import net.minecraft.client.renderer.texture.TextureAtlasSprite;
-import net.minecraft.client.resources.model.*;
+import net.minecraft.client.resources.model.ModelBaker;
 import net.minecraft.resources.ResourceLocation;
 
 import java.util.List;
-import java.util.Map;
-import java.util.function.Function;
+import java.util.function.Consumer;
 
 /**
  * Created 27/04/2023 by SuperMartijn642
  */
-public class ItemModelBakingContextImpl extends BlockModelBakingContextImpl implements ItemModelBakingContext {
+public class ItemModelBakingContextImpl extends BlockStateModelBakingContextImpl implements ItemModelBakingContext {
 
     private final List<ItemTintSource> tintSources;
     private final EntityModelSet entityModelSet;
 
-    public ItemModelBakingContextImpl(ModelBaker modelBaker, Function<Material,TextureAtlasSprite> spriteGetter, ResourceLocation modelIdentifier, Map<ResourceLocation,UnbakedModel> dependencies, Map<String,Material> topLevelTextureReferences, boolean topLevelAmbientOcclusion, boolean topLevelUseBlockLighting, ItemTransforms topLevelItemTransforms, UnbakedGeometry topLevelGeometry, List<ItemTintSource> tintSources, EntityModelSet entityModelSet){
-        super(modelBaker, spriteGetter, BlockModelRotation.X0_Y0, modelIdentifier, dependencies, topLevelTextureReferences, topLevelAmbientOcclusion, topLevelUseBlockLighting, topLevelItemTransforms, topLevelGeometry);
+    public ItemModelBakingContextImpl(Consumer<String> warnings, ResourceLocation identifier, ModelTransform transform, ModelBaker modelBaker, List<ItemTintSource> tintSources, EntityModelSet entityModelSet){
+        super(warnings, identifier, transform, modelBaker);
         this.tintSources = tintSources;
         this.entityModelSet = entityModelSet;
     }

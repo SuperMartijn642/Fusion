1f964adf0c5c248581f9d6ed349d782c7000bd72
SuperMartijn624 <martijnvandenbrand@outlook.com>
2026-05-11 16:49:28 +0200

    Overhaul `ModelType`s and model baking behaviour

--- src/main/java/com/supermartijn642/fusion/model/types/connecting/ConnectingItemModel.java
+++ src/main/java/com/supermartijn642/fusion/model/types/connecting/ConnectingItemModel.java
@@ -1,26 +1,36 @@
 package com.supermartijn642.fusion.model.types.connecting;
 
-import com.google.common.base.Suppliers;
-import com.supermartijn642.fusion.FusionClient;
-import com.supermartijn642.fusion.model.types.base.BaseModelQuad;
+import com.supermartijn642.fusion.api.model.custom.quad.MutableQuad;
+import com.supermartijn642.fusion.api.model.custom.quad.QuadAccess;
+import com.supermartijn642.fusion.api.texture.DefaultTextureTypes;
+import com.supermartijn642.fusion.api.texture.SpriteHelper;
+import com.supermartijn642.fusion.api.texture.custom.SpriteInstance;
 import com.supermartijn642.fusion.texture.types.connecting.StitchedConnectingTextureData;
 import com.supermartijn642.fusion.texture.types.connecting.layouts.ConnectingTextureLayoutHandler;
-import net.fabricmc.fabric.api.renderer.v1.Renderer;
-import net.fabricmc.fabric.api.renderer.v1.material.RenderMaterial;
-import net.fabricmc.fabric.api.renderer.v1.mesh.Mesh;
-import net.fabricmc.fabric.api.renderer.v1.mesh.MutableMesh;
-import net.fabricmc.fabric.api.renderer.v1.mesh.QuadEmitter;
 import net.minecraft.client.color.item.ItemTintSource;
 import net.minecraft.client.multiplayer.ClientLevel;
+import net.minecraft.client.renderer.ItemBlockRenderTypes;
+import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.Sheets;
-import net.minecraft.client.renderer.item.*;
+import net.minecraft.client.renderer.block.model.ItemTransforms;
+import net.minecraft.client.renderer.item.BlockModelWrapper;
+import net.minecraft.client.renderer.item.ItemModel;
+import net.minecraft.client.renderer.item.ItemModelResolver;
+import net.minecraft.client.renderer.item.ItemStackRenderState;
+import net.minecraft.client.renderer.texture.TextureAtlasSprite;
+import net.minecraft.client.resources.model.UnbakedModel;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.ItemDisplayContext;
 import net.minecraft.world.item.ItemStack;
 import org.jetbrains.annotations.Nullable;
 import org.joml.Vector3f;
 
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+import java.util.function.Function;
 import java.util.function.Supplier;
 
 /**
@@ -28,60 +38,119 @@ import java.util.function.Supplier;
  */
 public class ConnectingItemModel implements ItemModel {
 
+    private final List<Part> parts;
     private final List<ItemTintSource> tints;
-    private final ModelRenderProperties properties;
-    private final Mesh mesh;
-    private final Supplier<Vector3f[]> extents;
 
-    public ConnectingItemModel(List<ItemTintSource> tints, List<ConnectingModelQuad> quads, ModelRenderProperties properties){
+    public ConnectingItemModel(List<Part> parts, List<ItemTintSource> tints){
+        this.parts = parts;
         this.tints = tints;
-        this.properties = properties;
-        this.extents = Suppliers.memoize(() -> BlockModelWrapper.computeExtents(quads.stream().map(BaseModelQuad::bakedQuad).toList()));
-
-        // Create the item mesh
-        MutableMesh builder = Renderer.get().mutableMesh();
-        QuadEmitter emitter = builder.emitter();
-        OrientedMutableQuad mutableQuad = new OrientedMutableQuad();
-        for(ConnectingModelQuad quad : quads){
-            // Some layouts need auxiliary quads, hence simply repeat the quad that many times
-            int auxiliaryQuadCount = 0;
-            ConnectingTextureLayoutHandler layoutHandler = null;
-            if(quad.hasConnectingTexture()){
-                layoutHandler = ConnectingTextureLayoutHandler.get(quad.getLayout());
-                // Get the number of auxiliary quads needed
-                auxiliaryQuadCount = layoutHandler.getAuxiliaryQuadCount();
-            }
-            // Process and submit the quads
-            RenderMaterial material = FusionClient.getRenderTypeMaterial(null, quad.renderType(), quad.emissive());
-            for(int quadIndex = 0; quadIndex < auxiliaryQuadCount + 1; quadIndex++){
-                emitter.fromVanilla(quad.bakedQuad(), material, quad.cullDirection());
-                // Process the quad if it has a connecting texture
-                // As item mesh does not depend on state, we can run the connecting texture processing immediately
-                if(layoutHandler != null){
-                    mutableQuad.set(emitter);
-                    mutableQuad.set(ConnectingBakedModel.TextureOrientation.NORMAL_0.vertexIndexPermutation);
-                    boolean keepQuad = layoutHandler.processItemQuad(quadIndex, mutableQuad, quad.spriteInstance(), (StitchedConnectingTextureData)quad.spriteInstance().getTexture().getCustomData());
-                    if(!keepQuad)
-                        continue;
-                }
-                emitter.emit();
-            }
-        }
-        this.mesh = builder.immutableCopy();
     }
 
     @Override
-    public void update(ItemStackRenderState renderState, ItemStack stack, ItemModelResolver modelResolver, ItemDisplayContext displayContext, @Nullable ClientLevel level, @Nullable LivingEntity entity, int i){
-        ItemStackRenderState.LayerRenderState layer = renderState.newLayer();
-        if(stack.hasFoil())
-            layer.setFoilType(BlockModelWrapper.hasSpecialAnimatedTexture(stack) ? ItemStackRenderState.FoilType.SPECIAL : ItemStackRenderState.FoilType.STANDARD);
-        int tints = this.tints.size();
-        int[] tintValues = layer.prepareTintLayers(tints);
-        for(int j = 0; j < tints; j++)
-            tintValues[j] = this.tints.get(j).calculate(stack, level, entity);
-        layer.setExtents(this.extents);
-        this.properties.applyToLayer(layer, displayContext);
-        this.mesh.outputTo(layer.emitter());
-        layer.setRenderType(Sheets.translucentItemSheet());
+    public void update(ItemStackRenderState renderState, ItemStack stack, ItemModelResolver modelResolver, ItemDisplayContext displayContext, @Nullable ClientLevel level, @Nullable LivingEntity owner, int seed){
+        renderState.ensureCapacity(this.parts.size());
+        // Compute tint values
+        int[] tintValues;
+        if(!this.tints.isEmpty()){
+            tintValues = new int[this.tints.size()];
+            for(int i = 0; i < this.tints.size(); i++)
+                tintValues[i] = this.tints.get(i).calculate(stack, level, owner);
+        }else
+            tintValues = null;
+        // Get foil type
+        ItemStackRenderState.FoilType foilType = stack.hasFoil() ?
+            BlockModelWrapper.hasSpecialAnimatedTexture(stack) ? ItemStackRenderState.FoilType.SPECIAL : ItemStackRenderState.FoilType.STANDARD :
+            null;
+        // Get default render type to use for the item
+        RenderType defaultRenderType = Sheets.translucentItemSheet();
+        if(stack.getItem() instanceof BlockItem && ItemBlockRenderTypes.getChunkRenderType(((BlockItem)stack.getItem()).getBlock().defaultBlockState()) != RenderType.translucent())
+            defaultRenderType = Sheets.cutoutBlockSheet();
+        // Submit each part
+        for(Part part : this.parts){
+            // Create function to create layers
+            Function<RenderType,ItemStackRenderState.LayerRenderState> layerConfigurer = renderType -> {
+                ItemStackRenderState.LayerRenderState layer = renderState.newLayer();
+                if(tintValues != null)
+                    System.arraycopy(tintValues, 0, layer.prepareTintLayers(tintValues.length), 0, tintValues.length);
+                if(foilType != null)
+                    layer.setFoilType(foilType);
+                layer.setExtents(part.extents);
+                layer.setUsesBlockLight(part.guiLight.lightLikeBlock());
+                layer.setParticleIcon(part.particleSprite);
+                layer.setTransform(part.transforms.getTransform(displayContext));
+                layer.setRenderType(renderType);
+                return layer;
+            };
+            // Create a layer for each render type
+            List<RenderType> renderTypes = new ArrayList<>(4);
+            List<ItemStackRenderState.LayerRenderState> layers = new ArrayList<>(4);
+            for(QuadAccess quad : part.quads){
+                // Get render type
+                RenderType renderType = quad.itemRenderType();
+                if(renderType == null)
+                    renderType = defaultRenderType;
+                // Get or create layer
+                int i = renderTypes.indexOf(renderType);
+                ItemStackRenderState.LayerRenderState layer;
+                if(i == -1){
+                    renderTypes.add(renderType);
+                    layer = layerConfigurer.apply(renderType);
+                    layers.add(layer);
+                }else
+                    layer = layers.get(i);
+                // Add the quad to the layer
+                layer.prepareQuadList().add(quad.toBakedQuad());
+            }
+        }
+    }
+
+    public static class Part {
+        private final List<QuadAccess> quads;
+        private final UnbakedModel.GuiLight guiLight;
+        private final TextureAtlasSprite particleSprite;
+        private final ItemTransforms transforms;
+        private final Supplier<Vector3f[]> extents;
+
+        public Part(List<QuadAccess> quads, UnbakedModel.GuiLight guiLight, TextureAtlasSprite particleSprite, ItemTransforms transforms){
+            // Process quads with connecting textures
+            List<QuadAccess> processedQuads = new ArrayList<>(quads.size());
+            MutableQuad mutableQuad = null;
+            for(QuadAccess quad : quads){
+                // Get the sprite instance
+                SpriteInstance sprite = SpriteHelper.getSpriteInstance(quad.sprite());
+                if(sprite == null || sprite.getTexture().getTextureType() != DefaultTextureTypes.CONNECTING){
+                    processedQuads.add(quad);
+                    continue;
+                }
+
+                // Get the sprite data
+                StitchedConnectingTextureData data = (StitchedConnectingTextureData)sprite.getTexture().getCustomData();
+                ConnectingTextureLayoutHandler layoutHandler = ConnectingTextureLayoutHandler.get(data.getLayout());
+
+                // Create quads
+                if(mutableQuad == null)
+                    mutableQuad = MutableQuad.create();
+                for(int i = 0; i < layoutHandler.getAuxiliaryQuadCount() + 1; i++){
+                    mutableQuad.copyFrom(quad);
+                    boolean keepQuad = layoutHandler.processItemQuad(i, mutableQuad, sprite, data);
+                    if(keepQuad)
+                        processedQuads.add(mutableQuad.createCopy());
+                }
+            }
+            this.quads = List.copyOf(processedQuads);
+
+            this.guiLight = guiLight;
+            this.particleSprite = particleSprite;
+            this.transforms = transforms;
+            this.extents = () -> {
+                Set<Vector3f> positions = new HashSet<>();
+                for(QuadAccess quad : this.quads){
+                    for(int vertex = 0; vertex < 4; vertex++){
+                        positions.add(new Vector3f(quad.position(vertex)));
+                    }
+                }
+                return positions.toArray(Vector3f[]::new);
+            };
+        }
     }
 }

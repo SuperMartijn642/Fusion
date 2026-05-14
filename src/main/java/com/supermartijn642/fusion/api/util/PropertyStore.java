fa5c375ce41e8f95b249ee2b1072b9685c2eda1a
SuperMartijn624 <martijnvandenbrand@outlook.com>
2026-05-14 13:20:00 +0200

    Add `PropertyStore#getOrCompute`

--- src/main/java/com/supermartijn642/fusion/api/util/PropertyStore.java
+++ src/main/java/com/supermartijn642/fusion/api/util/PropertyStore.java
@@ -3,6 +3,9 @@ package com.supermartijn642.fusion.api.util;
 import com.supermartijn642.fusion.util.PropertyStoreImpl;
 import org.jetbrains.annotations.ApiStatus;
 
+import java.util.function.Function;
+import java.util.function.Supplier;
+
 /**
  * Created 12/05/2026 by SuperMartijn642
  */
@@ -26,5 +29,23 @@ public interface PropertyStore extends PropertyGetter {
     @ApiStatus.NonExtendable
     default <X> void setProperty(Property<X,Void> property, X value){
         this.setProperty(property, null, value);
+    }
+
+    @ApiStatus.NonExtendable
+    default <X, C> X getOrCompute(Property<X,C> property, C context, Function<C,X> compute){
+        return this.getProperty(property, context).orElseGet(() -> {
+            X value = compute.apply(context);
+            this.setProperty(property, context, value);
+            return value;
+        });
+    }
+
+    @ApiStatus.NonExtendable
+    default <X> X getOrCompute(Property<X,Void> property, Supplier<X> compute){
+        return this.getProperty(property).orElseGet(() -> {
+            X value = compute.get();
+            this.setProperty(property, value);
+            return value;
+        });
     }
 }

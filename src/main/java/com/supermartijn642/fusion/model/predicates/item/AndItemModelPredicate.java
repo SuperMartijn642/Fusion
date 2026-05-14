6c4286339a7e7ea09328f292a567b69d3a4f058f
SuperMartijn624 <martijnvandenbrand@outlook.com>
2026-05-14 14:07:18 +0200

    Add model conditions

--- src/main/java/com/supermartijn642/fusion/model/predicates/item/AndItemModelPredicate.java
+++ src/main/java/com/supermartijn642/fusion/model/predicates/item/AndItemModelPredicate.java
@@ -1,10 +1,10 @@
-package com.supermartijn642.fusion.model.modifiers.item.predicates;
+package com.supermartijn642.fusion.model.predicates.item;
 
 import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParseException;
-import com.supermartijn642.fusion.api.model.modifier.item.ItemPredicate;
+import com.supermartijn642.fusion.api.model.predicates.item.ItemModelPredicate;
 import com.supermartijn642.fusion.api.util.Serializer;
 import net.minecraft.world.item.ItemStack;
 
@@ -15,50 +15,50 @@ import java.util.List;
 /**
  * Created 20/09/2024 by SuperMartijn642
  */
-public class AndItemPredicate implements ItemPredicate {
+public class AndItemModelPredicate implements ItemModelPredicate {
 
-    public static final Serializer<AndItemPredicate> SERIALIZER = new Serializer<>() {
+    public static final Serializer<AndItemModelPredicate> SERIALIZER = new Serializer<>() {
         @Override
-        public AndItemPredicate deserialize(JsonObject json) throws JsonParseException{
+        public AndItemModelPredicate deserialize(JsonObject json) throws JsonParseException{
             if(!json.has("predicates") || !json.get("predicates").isJsonArray())
                 throw new JsonParseException("And-predicate must have array property 'predicates'!");
-            List<ItemPredicate> predicates = new ArrayList<>();
+            List<ItemModelPredicate> predicates = new ArrayList<>();
             // Deserialize all the predicates from the 'predicates' array
             JsonArray array = json.getAsJsonArray("predicates");
             for(JsonElement element : array){
                 if(!element.isJsonObject())
                     throw new JsonParseException("Property 'predicates' must only contain objects!");
-                ItemPredicate predicate = ItemPredicateRegistry.deserializeItemPredicate(element.getAsJsonObject());
+                ItemModelPredicate predicate = ItemPredicateRegistry.deserializeItemPredicate(element.getAsJsonObject());
                 predicates.add(predicate);
             }
-            return new AndItemPredicate(predicates);
+            return new AndItemModelPredicate(predicates);
         }
 
         @Override
-        public JsonObject serialize(AndItemPredicate value){
+        public JsonObject serialize(AndItemModelPredicate value){
             JsonObject json = new JsonObject();
             // Create an array with all the serialized predicates
             JsonArray predicatesJson = new JsonArray();
-            for(ItemPredicate predicate : value.predicates)
+            for(ItemModelPredicate predicate : value.predicates)
                 predicatesJson.add(ItemPredicateRegistry.serializeItemPredicate(predicate));
             json.add("predicates", predicatesJson);
             return json;
         }
     };
 
-    private final List<ItemPredicate> predicates;
+    private final List<ItemModelPredicate> predicates;
 
-    public AndItemPredicate(List<ItemPredicate> predicates){
+    public AndItemModelPredicate(List<ItemModelPredicate> predicates){
         this.predicates = predicates;
     }
 
-    public List<ItemPredicate> getPredicates(){
+    public List<ItemModelPredicate> getPredicates(){
         return Collections.unmodifiableList(this.predicates);
     }
 
     @Override
     public boolean test(ItemStack stack){
-        for(ItemPredicate predicate : this.predicates){
+        for(ItemModelPredicate predicate : this.predicates){
             if(!predicate.test(stack))
                 return false;
         }
@@ -66,7 +66,7 @@ public class AndItemPredicate implements ItemPredicate {
     }
 
     @Override
-    public Serializer<? extends ItemPredicate> getSerializer(){
+    public Serializer<? extends ItemModelPredicate> getSerializer(){
         return SERIALIZER;
     }
 }

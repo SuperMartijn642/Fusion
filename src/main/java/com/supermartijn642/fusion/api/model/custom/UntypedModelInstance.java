96977dc49592b4d3d47cd6f2421aa1f63678bd3c
SuperMartijn624 <martijnvandenbrand@outlook.com>
2026-05-14 13:54:58 +0200

    Extract `UntypedModelInstance` from `ModelInstance`, and allow `ModelType#getParents` to return untyped models

--- src/main/java/com/supermartijn642/fusion/api/model/custom/UntypedModelInstance.java
+++ src/main/java/com/supermartijn642/fusion/api/model/custom/UntypedModelInstance.java
@@ -1,13 +1,8 @@
-package com.supermartijn642.fusion.api.model;
+package com.supermartijn642.fusion.api.model.custom;
 
-import com.supermartijn642.fusion.api.model.custom.ModelBakingContext;
-import com.supermartijn642.fusion.api.model.custom.ModelMaterial;
-import com.supermartijn642.fusion.api.model.custom.ModelTransform;
-import com.supermartijn642.fusion.api.model.custom.UntypedModelInstance;
 import com.supermartijn642.fusion.api.model.custom.geometry.ModelGeometry;
 import com.supermartijn642.fusion.api.util.Either;
-import com.supermartijn642.fusion.api.util.Property;
-import com.supermartijn642.fusion.api.util.Serializer;
+import com.supermartijn642.fusion.api.util.PropertyGetter;
 import net.minecraft.client.renderer.block.model.BlockModel;
 import net.minecraft.client.renderer.block.model.ItemTransform;
 import net.minecraft.client.resources.model.BakedModel;
@@ -18,83 +13,78 @@ import org.jetbrains.annotations.Nullable;
 import java.util.Collection;
 import java.util.List;
 import java.util.Map;
-import java.util.Optional;
 
 /**
- * An interface for custom model types.
- * <p>
- * Created 27/04/2023 by SuperMartijn642
- * @param <T> data used to bake the model
+ * Created 14/05/2026 by SuperMartijn642
  */
-public interface ModelType<T> extends Serializer<T> {
+public interface UntypedModelInstance extends PropertyGetter {
 
     /**
      * Gets all the dependencies on other unbaked models.
      */
-    Collection<ResourceLocation> getDependencies(T data);
+    Collection<ResourceLocation> getDependencies();
 
     /**
      * Gets any parent models which the model may inherit properties from.
      */
-    List<Either<ResourceLocation,UntypedModelInstance>> getParents(T data);
+    List<Either<ResourceLocation,UntypedModelInstance>> getParents();
 
     /**
      * Gets whether the model should be rendered with ambient occlusion.
      */
     @Nullable
-    Boolean getAmbientOcclusion(T data);
+    Boolean getAmbientOcclusion();
 
     /**
      * Gets the lighting to use when the model is rendered in a gui.
      */
     @Nullable
-    BlockModel.GuiLight getGuiLight(T data);
+    BlockModel.GuiLight getGuiLight();
 
     /**
      * Gets the transformations used to render the model as an item under the given context.
      */
     @Nullable
-    ItemTransform getItemTransform(ItemDisplayContext type, T data);
+    ItemTransform getItemTransform(ItemDisplayContext type);
 
     /**
      * Gets the material references of the model.
      */
-    Map<String,Either<String,ModelMaterial>> getMaterials(T data);
+    Map<String,Either<String,ModelMaterial>> getMaterials();
+
+    /**
+     * Gets the material reference for the given key.
+     */
+    @Nullable
+    default Either<String,ModelMaterial> getMaterial(String key){
+        return this.getMaterials().get(key);
+    }
 
     /**
      * Gets the geometry of the model.
      */
     @Nullable
-    ModelGeometry getGeometry(T data);
+    ModelGeometry getGeometry();
 
     /**
      * Gets whether the model should be shaded.
      */
     @Nullable
-    Boolean getShade(T data);
+    Boolean getShade();
 
     /**
      * Gets whether the model is emissive.
      */
     @Nullable
-    Boolean getEmissive(T data);
+    Boolean getEmissive();
 
     /**
      * Gets the transformations that should be applied to the model's geometry.
      */
-    default ModelTransform getTransform(T data){
-        return ModelTransform.identity();
-    }
-
-    /**
-     * Gets an arbitrary property of this model.
-     * @see Property
-     */
-    <X, C> Optional<X> getProperty(Property<X,C> property, C context, T data);
+    ModelTransform getTransform();
 
     /**
      * Creates a baked model from the model data.
-     * @see ModelBakingContext
      */
-    BakedModel bakeModel(ModelBakingContext context, T data);
+    BakedModel bakeModel(ModelBakingContext context);
 }

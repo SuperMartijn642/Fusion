f411a41fc04344c16e52dbb04d7e90048c7be669
SuperMartijn624 <martijnvandenbrand@outlook.com>
2026-05-14 11:36:20 +0200

    Add generic quad processing for `TextureType`s
    - Move connecting texture handling from model to texture
    - Unify model implementations of connecting and base model types

--- src/main/java/com/supermartijn642/fusion/mixin/embeddium/BlockRendererMixinEmbeddium.java
+++ src/main/java/com/supermartijn642/fusion/mixin/embeddium/BlockRendererMixinEmbeddium.java
@@ -2,7 +2,7 @@ package com.supermartijn642.fusion.mixin.embeddium;
 
 import com.supermartijn642.fusion.api.texture.SpriteHelper;
 import com.supermartijn642.fusion.api.texture.custom.TextureInstance;
-import com.supermartijn642.fusion.api.texture.data.BaseTextureData;
+import com.supermartijn642.fusion.api.texture.types.base.BaseTextureData;
 import com.supermartijn642.fusion.texture.QuadTintingHelper;
 import me.jellysquid.mods.sodium.client.model.color.ColorProvider;
 import me.jellysquid.mods.sodium.client.model.quad.BakedQuadView;

48e2d7f3cf49ea0722a92c293d9f93068a6308ea
SuperMartijn624 <martijnvandenbrand@outlook.com>
2026-05-14 11:36:20 +0200

    Add generic quad processing for `TextureType`s
    - Move connecting texture handling from model to texture
    - Unify model implementations of connecting and base model types

--- src/main/java/com/supermartijn642/fusion/mixin/rubidium/ItemRendererMixinRubidium.java
+++ src/main/java/com/supermartijn642/fusion/mixin/rubidium/ItemRendererMixinRubidium.java
@@ -3,7 +3,7 @@ package com.supermartijn642.fusion.mixin.rubidium;
 import com.mojang.blaze3d.vertex.PoseStack;
 import com.supermartijn642.fusion.api.texture.SpriteHelper;
 import com.supermartijn642.fusion.api.texture.custom.TextureInstance;
-import com.supermartijn642.fusion.api.texture.data.BaseTextureData;
+import com.supermartijn642.fusion.api.texture.types.base.BaseTextureData;
 import com.supermartijn642.fusion.texture.QuadTintingHelper;
 import me.jellysquid.mods.sodium.client.model.quad.ModelQuadView;
 import me.jellysquid.mods.sodium.client.render.vertex.VertexBufferWriter;
---
+++ src/main/java/com/supermartijn642/fusion/texture/types/connecting/predicates/MatchBlockConnectionPredicate.java
@@ -0,0 +1,71 @@
+package com.supermartijn642.fusion.texture.types.connecting.predicates;
+
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParseException;
+import com.supermartijn642.fusion.api.texture.types.connecting.predicates.ConnectionDirection;
+import com.supermartijn642.fusion.api.texture.types.connecting.predicates.ConnectionPredicate;
+import com.supermartijn642.fusion.api.util.Serializer;
+import com.supermartijn642.fusion.util.IdentifierUtil;
+import net.minecraft.core.Direction;
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.state.BlockState;
+import org.jetbrains.annotations.Nullable;
+
+/**
+ * Created 28/04/2023 by SuperMartijn642
+ */
+public class MatchBlockConnectionPredicate implements ConnectionPredicate {
+
+    public static final Serializer<MatchBlockConnectionPredicate> SERIALIZER = new Serializer<>() {
+        @Override
+        public MatchBlockConnectionPredicate deserialize(JsonObject json) throws JsonParseException{
+            if(!json.has("block") || !json.get("block").isJsonPrimitive() || !json.getAsJsonPrimitive("block").isString())
+                throw new JsonParseException("Match block predicate must have string property 'block'!");
+            if(!IdentifierUtil.isValidIdentifier(json.get("block").getAsString()))
+                throw new JsonParseException("Property 'block' must be a valid identifier!");
+            ResourceLocation identifier = new ResourceLocation(json.get("block").getAsString());
+            if(!BuiltInRegistries.BLOCK.containsKey(identifier))
+                throw new JsonParseException("Unknown block '" + identifier + "'!");
+            Block block = BuiltInRegistries.BLOCK.get(identifier);
+            return new MatchBlockConnectionPredicate(block);
+        }
+
+        @Override
+        public JsonObject serialize(MatchBlockConnectionPredicate value){
+            JsonObject json = new JsonObject();
+            json.addProperty("block", BuiltInRegistries.BLOCK.getKey(value.block).toString());
+            return json;
+        }
+    };
+
+    private final Block block;
+
+    public MatchBlockConnectionPredicate(Block block){
+        this.block = block;
+    }
+
+    @Override
+    public boolean shouldConnect(Direction side, @Nullable BlockState ownState, BlockState otherState, BlockState blockInFront, ConnectionDirection direction){
+        return otherState.getBlock() == this.block;
+    }
+
+    @Override
+    public Serializer<? extends ConnectionPredicate> getSerializer(){
+        return SERIALIZER;
+    }
+
+    @Override
+    public final boolean equals(Object o){
+        if(this == o) return true;
+        if(!(o instanceof MatchBlockConnectionPredicate that)) return false;
+
+        return this.block.equals(that.block);
+    }
+
+    @Override
+    public int hashCode(){
+        return this.block.hashCode();
+    }
+}

721810b990002d3b77aeb2fe12ef605e2247fac5
SuperMartijn624 <martijnvandenbrand@outlook.com>
2026-05-14 11:36:20 +0200

    Add generic quad processing for `TextureType`s
    - Move connecting texture handling from model to texture
    - Unify model implementations of connecting and base model types

--- src/main/java/com/supermartijn642/fusion/model/types/cuboid/CuboidModelDataBuilderImpl.java
+++ src/main/java/com/supermartijn642/fusion/model/types/cuboid/CuboidModelDataBuilderImpl.java
@@ -2,7 +2,7 @@ package com.supermartijn642.fusion.model.types.cuboid;
 
 import com.google.common.collect.ImmutableMap;
 import com.mojang.datafixers.util.Either;
-import com.supermartijn642.fusion.api.model.custom.ModelProperty;
+import com.supermartijn642.fusion.api.model.custom.DefaultModelProperties;
 import com.supermartijn642.fusion.api.model.custom.geometry.CuboidModelGeometry;
 import com.supermartijn642.fusion.api.model.types.CuboidModelDataBuilder;
 import net.minecraft.client.renderer.block.model.*;
@@ -58,10 +58,10 @@ public class CuboidModelDataBuilderImpl extends AbstractCuboidModelDataBuilder<C
                         face.rotation() == null ? 0 : face.rotation().angle()
                     ),
                     new ForgeFaceData(
-                        face.getProperty(ModelProperty.FORGE_GEOMETRY_COLOR).orElse(-1),
-                        face.getProperty(ModelProperty.FORGE_GEOMETRY_BLOCK_LIGHT).orElse(0),
-                        face.getProperty(ModelProperty.FORGE_GEOMETRY_SKY_LIGHT).orElse(0),
-                        face.getProperty(ModelProperty.FORGE_GEOMETRY_AMBIENT_OCCLUSION).orElse(true)
+                        face.getProperty(DefaultModelProperties.FORGE_GEOMETRY_COLOR).orElse(-1),
+                        face.getProperty(DefaultModelProperties.FORGE_GEOMETRY_BLOCK_LIGHT).orElse(0),
+                        face.getProperty(DefaultModelProperties.FORGE_GEOMETRY_SKY_LIGHT).orElse(0),
+                        face.getProperty(DefaultModelProperties.FORGE_GEOMETRY_AMBIENT_OCCLUSION).orElse(true)
                     )
                 ));
             }

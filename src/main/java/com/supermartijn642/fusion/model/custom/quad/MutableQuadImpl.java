7780e6cd101a436554a5f9aa4fd4362b68882474
SuperMartijn624 <martijnvandenbrand@outlook.com>
2026-05-14 11:37:13 +0200

    Improve `MutableQuadImpl#copyFrom`

--- src/main/java/com/supermartijn642/fusion/model/custom/quad/MutableQuadImpl.java
+++ src/main/java/com/supermartijn642/fusion/model/custom/quad/MutableQuadImpl.java
@@ -55,22 +55,23 @@ public class MutableQuadImpl implements MutableQuad {
 
     @Override
     public MutableQuad copyFrom(QuadAccess quad){
+        MutableQuadImpl impl = (MutableQuadImpl)quad;
         // Quad data
-        this.bakedQuadCache = null;
+        this.bakedQuadCache = impl.bakedQuadCache;
         for(int i = 0; i < 4; i++){
-            this.positions[i].set(quad.position(i));
-            this.uvs[i].set(quad.u(i), quad.v(i));
+            this.positions[i].set(impl.positions[i]);
+            this.uvs[i].set(impl.uvs[i]);
         }
-        this.facing = quad.facing();
-        this.sprite = quad.sprite();
-        this.tintIndex = quad.tintIndex();
-        this.shade = quad.shade();
-        this.lightEmission = quad.lightEmission();
+        this.facing = impl.facing;
+        this.sprite = impl.sprite;
+        this.tintIndex = impl.tintIndex;
+        this.shade = impl.shade;
+        this.lightEmission = impl.lightEmission;
         // Our properties
-        this.ambientOcclusion = quad.ambientOcclusion();
-        this.emissive = quad.emissive();
-        this.chunkRenderType = quad.chunkRenderType();
-        this.itemRenderType = quad.itemRenderType();
+        this.ambientOcclusion = impl.ambientOcclusion;
+        this.emissive = impl.emissive;
+        this.chunkRenderType = impl.chunkRenderType;
+        this.itemRenderType = impl.itemRenderType;
         return this;
     }
 

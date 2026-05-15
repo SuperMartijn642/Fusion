caafc6fe23a4896f752ac6c98b4dd15b47000c79
SuperMartijn624 <martijnvandenbrand@outlook.com>
2026-05-15 13:45:13 +0200

    Improve all predicate types and make them consistent

---
+++ src/main/java/com/supermartijn642/fusion/model/predicates/blockstate/FalseBlockStateModelPredicate.java
@@ -0,0 +1,42 @@
+package com.supermartijn642.fusion.model.predicates.blockstate;
+
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParseException;
+import com.supermartijn642.fusion.api.model.predicates.blockstate.BlockStateModelPredicate;
+import com.supermartijn642.fusion.api.util.Serializer;
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.BlockAndTintGetter;
+import net.minecraft.world.level.block.state.BlockState;
+import org.jetbrains.annotations.Nullable;
+
+/**
+ * Created 14/05/2026 by SuperMartijn642
+ */
+public class FalseBlockStateModelPredicate implements BlockStateModelPredicate {
+
+    public static final FalseBlockStateModelPredicate INSTANCE = new FalseBlockStateModelPredicate();
+    public static final Serializer<FalseBlockStateModelPredicate> SERIALIZER = new Serializer<>() {
+        @Override
+        public FalseBlockStateModelPredicate deserialize(JsonObject json) throws JsonParseException{
+            return INSTANCE;
+        }
+
+        @Override
+        public JsonObject serialize(FalseBlockStateModelPredicate value){
+            return null;
+        }
+    };
+
+    private FalseBlockStateModelPredicate(){
+    }
+
+    @Override
+    public boolean test(@Nullable BlockAndTintGetter level, @Nullable BlockPos pos, @Nullable BlockState state){
+        return false;
+    }
+
+    @Override
+    public Serializer<? extends BlockStateModelPredicate> getSerializer(){
+        return SERIALIZER;
+    }
+}

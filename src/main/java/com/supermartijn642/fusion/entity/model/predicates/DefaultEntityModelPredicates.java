b23c51dc81a85749dbea97f01af8f807f9a24660
SuperMartijn624 <martijnvandenbrand@outlook.com>
2026-05-15 16:32:48 +0200

    Add actual block state model predicates

--- src/main/java/com/supermartijn642/fusion/entity/model/predicates/DefaultEntityModelPredicates.java
+++ src/main/java/com/supermartijn642/fusion/entity/model/predicates/DefaultEntityModelPredicates.java
@@ -69,14 +69,14 @@ public final class DefaultEntityModelPredicates {
     /**
      * Creates a predicate that evaluates whether an entity is in the given biomes.
      */
-    public static EntityModelPredicate biomes(ResourceKey<Biome>... biomes){
+    public static EntityModelPredicate biome(ResourceKey<Biome>... biomes){
         return BiomeEntityModelPredicate.create(biomes);
     }
 
     /**
      * Creates a predicate that evaluates whether an entity is in the given biomes.
      */
-    public static EntityModelPredicate biomes(ResourceLocation... biomes){
+    public static EntityModelPredicate biome(ResourceLocation... biomes){
         return BiomeEntityModelPredicate.create(biomes);
     }
 

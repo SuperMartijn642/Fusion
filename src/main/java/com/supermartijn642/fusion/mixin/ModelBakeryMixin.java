c58d965ff75adacf9fb2d55da379f0bc8ebd7050
SuperMartijn624 <martijnvandenbrand@outlook.com>
2026-05-19 19:18:28 +0200

    Overhaul block model modifiers
    Allow conditions for models.
    Order modifiers by a `priority` property.
    Add `default_model_overrides`.
    Add `show_breaking_overlay` option for each model individually.

--- src/main/java/com/supermartijn642/fusion/mixin/ModelBakeryMixin.java
+++ src/main/java/com/supermartijn642/fusion/mixin/ModelBakeryMixin.java
@@ -53,7 +53,7 @@ public class ModelBakeryMixin {
 
             // Apply Fusion model modifier
             ModelBakery.ModelBakerImpl resolver = ((ModelBakery)(Object)this).new ModelBakerImpl(textureGetter);
-            BlockModelModifierReloadListener.INSTANCE.applyOverlays(results, resolver);
+            BlockModelModifierReloadListener.INSTANCE.applyModelModifiers(results, resolver);
             ItemModelModifierReloadListener.INSTANCE.applyPredicateModels(results, new ItemModel.BakingContext(
                 resolver,
                 this.entityModelSet,

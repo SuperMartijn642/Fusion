f27649e402649bae1ab0dfcaca1427f8e480339b
SuperMartijn624 <martijnvandenbrand@outlook.com>
2026-05-19 21:40:18 +0200

    Overhaul item model modifiers
    Order modifiers by a `priority` property.
    Add `default_model_overrides`.

--- src/main/java/com/supermartijn642/fusion/model/modifiers/item/ItemModelModifierBakedModel.java
+++ src/main/java/com/supermartijn642/fusion/model/modifiers/item/ItemModelModifierBakedModel.java
@@ -1,113 +1,120 @@
 package com.supermartijn642.fusion.model.modifiers.item;
 
 import com.mojang.blaze3d.matrix.MatrixStack;
+import com.supermartijn642.fusion.FusionClient;
 import com.supermartijn642.fusion.api.model.predicates.item.ItemModelPredicate;
-import com.supermartijn642.fusion.api.util.Pair;
+import com.supermartijn642.fusion.model.ModelRenderTypeHelper;
+import com.supermartijn642.fusion.model.WrappedBakedModel;
 import net.minecraft.block.BlockState;
+import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.model.BakedQuad;
 import net.minecraft.client.renderer.model.IBakedModel;
 import net.minecraft.client.renderer.model.ItemCameraTransforms;
-import net.minecraft.client.renderer.model.ItemOverrideList;
-import net.minecraft.client.renderer.texture.TextureAtlasSprite;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.Direction;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.ILightReader;
 import net.minecraftforge.client.model.data.IModelData;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
-import javax.annotation.Nonnull;
-import javax.annotation.Nullable;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
 /**
  * Created 20/09/2024 by SuperMartijn642
  */
-public class ItemModelModifierBakedModel implements IBakedModel {
+public class ItemModelModifierBakedModel extends WrappedBakedModel {
 
-    private final IBakedModel defaultModel;
-    private final List<Pair<ItemModelPredicate,IBakedModel>> models;
+    private final IBakedModel original;
+    private final List<ConditionalModel> defaultModelOverrides;
+    private final List<List<ConditionalModel>> appendModels;
 
-    public ItemModelModifierBakedModel(IBakedModel defaultModel, List<Pair<ItemModelPredicate,IBakedModel>> models){
-        this.defaultModel = defaultModel;
-        this.models = models;
+    ItemModelModifierBakedModel(IBakedModel original, List<ConditionalModel> defaultModelOverrides, List<List<ConditionalModel>> appendModels){
+        super(original);
+        this.original = original;
+        this.defaultModelOverrides = defaultModelOverrides;
+        this.appendModels = appendModels;
     }
 
-    public IBakedModel forStack(ItemStack stack){
-        for(Pair<ItemModelPredicate,IBakedModel> entry : this.models){
-            if(entry.left().test(stack))
-                return entry.right();
+    @Override
+    public @NotNull List<BakedQuad> getQuads(@Nullable BlockState state, @Nullable Direction cullDirection, @NotNull Random random){
+        // Get item stack context
+        ItemStack stack = FusionClient.ITEM_STACK_RENDER_CONTEXT.get();
+
+        // Get seed to reset random instance
+        long seed = random.nextLong();
+        random.setSeed(seed);
+
+        // Collect all quads
+        List<BakedQuad> quads = new ArrayList<>();
+
+        // Default model
+        overrides:
+        {
+            for(ConditionalModel override : this.defaultModelOverrides){
+                if(override.conditions == null || override.conditions.test(stack)){
+                    quads.addAll(override.model.getQuads(state, cullDirection, random));
+                    break overrides;
+                }
+            }
+            quads.addAll(this.original.getQuads(state, cullDirection, random));
         }
-        return this.defaultModel;
+
+        // Append models
+        for(List<ConditionalModel> appendEntry : this.appendModels){
+            // First model whose conditions are met is submitted
+            for(ConditionalModel conditional : appendEntry){
+                if(conditional.conditions == null || conditional.conditions.test(stack)){
+                    random.setSeed(seed);
+                    quads.addAll(conditional.model.getQuads(state, cullDirection, random));
+                    break;
+                }
+            }
+        }
+        return quads;
     }
 
     @Override
-    public List<BakedQuad> getQuads(@Nullable BlockState state, @Nullable Direction cullDirection, Random random, IModelData data){
-        return this.defaultModel.getQuads(state, cullDirection, random, data);
+    public List<BakedQuad> getQuads(@Nullable BlockState state, @Nullable Direction cullDirection, Random random, IModelData modelData){
+        return this.getQuads(state, cullDirection, random);
     }
 
     @Override
-    public List<BakedQuad> getQuads(@Nullable BlockState state, @Nullable Direction cullDirection, Random random){
-        return this.defaultModel.getQuads(state, cullDirection, random);
-    }
+    public boolean canRenderInLayer(BlockState state, RenderType renderType){
+        // Check whether the render type is a default one for the state
+        boolean isDefaultRenderType = ModelRenderTypeHelper.couldBlockRenderInLayerOriginally(state, renderType);
 
-    @Override
-    public @Nonnull IModelData getModelData(@Nonnull ILightReader level, @Nonnull BlockPos pos, @Nonnull BlockState state, @Nonnull IModelData data){
-        return this.defaultModel.getModelData(level, pos, state, data);
-    }
+        // Default model
+        for(ConditionalModel override : this.defaultModelOverrides){
+            if(ModelRenderTypeHelper.canRenderInLayer(override.model, state, renderType, isDefaultRenderType))
+                return true;
+        }
+        if(ModelRenderTypeHelper.canRenderInLayer(this.original, state, renderType, isDefaultRenderType))
+            return true;
 
-    @Override
-    public ItemCameraTransforms getTransforms(){
-        return this.defaultModel.getTransforms();
-    }
-
-    @Override
-    public boolean useAmbientOcclusion(){
-        return this.defaultModel.useAmbientOcclusion();
-    }
-
-    @Override
-    public boolean isGui3d(){
-        return this.defaultModel.isGui3d();
-    }
-
-    @Override
-    public boolean usesBlockLight(){
-        return this.defaultModel.usesBlockLight();
-    }
-
-    @Override
-    public boolean isCustomRenderer(){
-        return this.defaultModel.isCustomRenderer();
-    }
-
-    @Override
-    public TextureAtlasSprite getParticleIcon(){
-        return this.defaultModel.getParticleIcon();
-    }
-
-    @Override
-    public TextureAtlasSprite getParticleTexture(@Nonnull IModelData data){
-        return this.defaultModel.getParticleTexture(data);
-    }
-
-    @Override
-    public ItemOverrideList getOverrides(){
-        return this.defaultModel.getOverrides();
-    }
-
-    @Override
-    public boolean isAmbientOcclusion(BlockState state){
-        return this.defaultModel.isAmbientOcclusion(state);
-    }
-
-    @Override
-    public boolean doesHandlePerspectives(){
-        return this.defaultModel.doesHandlePerspectives();
+        // Append models
+        for(List<ConditionalModel> appendEntry : this.appendModels){
+            for(ConditionalModel conditional : appendEntry){
+                if(ModelRenderTypeHelper.canRenderInLayer(conditional.model, state, renderType, isDefaultRenderType))
+                    return true;
+            }
+        }
+        return false;
     }
 
     @Override
     public IBakedModel handlePerspective(ItemCameraTransforms.TransformType transformType, MatrixStack poseStack){
-        return this.defaultModel.handlePerspective(transformType, poseStack);
+        super.handlePerspective(transformType, poseStack);
+        return this;
+    }
+
+    static final class ConditionalModel {
+        private final IBakedModel model;
+        private final @Nullable ItemModelPredicate conditions;
+
+        ConditionalModel(IBakedModel model, @Nullable ItemModelPredicate conditions){
+            this.model = model;
+            this.conditions = conditions;
+        }
     }
 }

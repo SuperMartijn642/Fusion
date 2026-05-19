c507b51b151828a8cc41885ff5196f191cc5c498
SuperMartijn624 <martijnvandenbrand@outlook.com>
2026-05-19 19:18:28 +0200

    Overhaul block model modifiers
    Allow conditions for models.
    Order modifiers by a `priority` property.
    Add `default_model_overrides`.
    Add `show_breaking_overlay` option for each model individually.

--- src/main/java/com/supermartijn642/fusion/model/modifiers/block/BlockModelModifierBakedModel.java
+++ src/main/java/com/supermartijn642/fusion/model/modifiers/block/BlockModelModifierBakedModel.java
@@ -1,11 +1,10 @@
 package com.supermartijn642.fusion.model.modifiers.block;
 
 import com.supermartijn642.fusion.FusionClient;
-import net.minecraft.client.renderer.ItemBlockRenderTypes;
+import com.supermartijn642.fusion.api.model.predicates.blockstate.BlockStateModelPredicate;
 import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.block.model.BlockModelPart;
 import net.minecraft.client.renderer.block.model.BlockStateModel;
-import net.minecraft.client.renderer.block.model.SingleVariant;
 import net.minecraft.client.renderer.texture.TextureAtlasSprite;
 import net.minecraft.core.BlockPos;
 import net.minecraft.util.RandomSource;
@@ -17,7 +16,6 @@ import net.minecraftforge.client.model.data.ModelProperty;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -25,149 +23,180 @@ import java.util.List;
  */
 public class BlockModelModifierBakedModel implements BlockStateModel {
 
-    private static final ModelProperty<Long> SEED_PROPERTY = new ModelProperty<>();
-    private static final ModelProperty<ModelData[]> DATA_PROPERTY = new ModelProperty<>();
-    private static final ModelProperty<BlockState> STATE_PROPERTY = new ModelProperty<>();
+    private static final ModelProperty<RenderData> RENDER_DATA = new ModelProperty<>();
 
     private final BlockStateModel original;
-    private final List<BlockStateModel> models;
-    private final boolean showBreakingOverlay;
-    private final boolean isOriginalSimpleModel;
-    private final boolean hasSimpleModels, hasNonSimpleModels;
-    private final List<BlockStateModel> nonSimpleModels;
+    private final List<ConditionalModel> defaultModelOverrides;
+    private final List<List<ConditionalModel>> appendModels;
+    private final TextureAtlasSprite particleSprite;
 
-    public BlockModelModifierBakedModel(BlockStateModel original, List<BlockStateModel> models, boolean showBreakingOverlay){
+    BlockModelModifierBakedModel(BlockStateModel original, List<ConditionalModel> defaultModelOverrides, List<List<ConditionalModel>> appendModels){
         this.original = original;
-        this.models = new ArrayList<>(models.size() + 1);
-        this.models.add(original);
-        this.models.addAll(models);
-        this.showBreakingOverlay = showBreakingOverlay;
-        List<BlockStateModel> nonSimpleModels = new ArrayList<>();
-        for(BlockStateModel model : this.models){
-            if(!model.getClass().equals(SingleVariant.class))
-                nonSimpleModels.add(model);
+        this.defaultModelOverrides = defaultModelOverrides;
+        this.appendModels = appendModels;
+
+        // Resolve particle material
+        TextureAtlasSprite particleSprite = null;
+        for(ConditionalModel override : this.defaultModelOverrides){
+            if(override.conditions == null || override.conditions.test(null, null, null)){
+                particleSprite = override.model.particleIcon();
+                break;
+            }
         }
-        this.isOriginalSimpleModel = original.getClass().equals(SingleVariant.class);
-        this.hasSimpleModels = nonSimpleModels.size() < this.models.size();
-        this.hasNonSimpleModels = !nonSimpleModels.isEmpty();
-        this.nonSimpleModels = nonSimpleModels.isEmpty() ? null : List.copyOf(nonSimpleModels);
+        if(particleSprite == null)
+            particleSprite = this.original.particleIcon();
+        this.particleSprite = particleSprite;
+    }
+
+    private RenderData getRenderData(@Nullable BlockAndTintGetter level, @Nullable BlockPos pos, @Nullable BlockState state, ModelData modelData){
+        boolean hasAllArguments = level != null && pos != null && state != null;
+
+        // Default model
+        int defaultModel = -1;
+        ModelData defaultModelData = null;
+        for(int i = 0; i < this.defaultModelOverrides.size(); i++){
+            ConditionalModel override = this.defaultModelOverrides.get(i);
+            if(override.conditions == null || override.conditions.test(level, pos, state)){
+                defaultModel = i;
+                defaultModelData = hasAllArguments ? override.model.getModelData(level, pos, state, modelData) : modelData;
+            }
+        }
+        if(defaultModel == -1)
+            defaultModelData = hasAllArguments ? this.original.getModelData(level, pos, state, modelData) : modelData;
+
+        // Append models
+        int[] appendModels = new int[this.appendModels.size()];
+        ModelData[] appendModelsData = new ModelData[this.appendModels.size()];
+        for(int i = 0; i < this.appendModels.size(); i++){
+            List<ConditionalModel> appendEntry = this.appendModels.get(i);
+            appendModels[i] = -1;
+            // First model whose conditions are met is submitted
+            for(int j = 0; j < appendEntry.size(); j++){
+                ConditionalModel conditional = appendEntry.get(j);
+                if(conditional.conditions == null || conditional.conditions.test(level, pos, state)){
+                    appendModels[i] = j;
+                    appendModelsData[i] = hasAllArguments ? conditional.model.getModelData(level, pos, state, modelData) : modelData;
+                    break;
+                }
+            }
+        }
+
+        return new RenderData(state, defaultModel, defaultModelData, appendModels, appendModelsData);
     }
 
     @Override
-    public void collectParts(RandomSource random, List<BlockModelPart> parts, ModelData data, @Nullable RenderType renderType){
-        // Get model data properties
-        Long seed = data.get(SEED_PROPERTY);
-        ModelData[] arr = data.get(DATA_PROPERTY);
-        BlockState state = data.get(STATE_PROPERTY);
-        // Keep track of whether the given render type is part of the block's default render types
-        Boolean isDefaultRenderType = null;
-        // When rendering breaking overlay, only submit the original model
-        if(!this.showBreakingOverlay && FusionClient.IS_RENDERING_BREAKING_OVERLAY.get() != null){
-            if(this.isOriginalSimpleModel){
-                this.original.collectParts(random, parts, ModelData.EMPTY, renderType);
-                return;
-            }
-            ModelData subData = arr == null || arr[0] == null ? ModelData.EMPTY : arr[0];
-            if(renderType == null || state == null || this.original.getRenderTypes(state, random, subData).contains(renderType)){
-                if(seed != null)
-                    random.setSeed(seed);
-                this.original.collectParts(random, parts, subData, renderType);
-            }
-            return;
-        }
-        // If there's only simple models, use vanilla method
-        if(!this.hasNonSimpleModels){
-            this.collectParts(random, parts);
-            return;
-        }
-        // Submit all models
-        int i = 0;
-        for(BlockStateModel model : this.models){
-            if(model.getClass().equals(SingleVariant.class)){ // For simple models, don't increase the model data array index
-                if(isDefaultRenderType == null)
-                    //noinspection removal
-                    isDefaultRenderType = renderType == null || state == null || ItemBlockRenderTypes.getRenderLayers(state).contains(renderType);
-                if(isDefaultRenderType)
-                    //noinspection deprecation
-                    model.collectParts(random, parts);
-            }else{ // For complex models, get the correct model data
-                ModelData subData = arr == null || arr[i] == null ? ModelData.EMPTY : arr[i];
-                if(renderType == null || state == null || model.getRenderTypes(state, random, subData).contains(renderType)){
-                    if(seed != null)
-                        random.setSeed(seed);
-                    model.collectParts(random, parts, subData, renderType);
-                }
-                i++;
+    public @NotNull ModelData getModelData(@NotNull BlockAndTintGetter level, @NotNull BlockPos pos, @NotNull BlockState state, @NotNull ModelData modelData){
+        return ModelData.builder()
+            .with(RENDER_DATA, this.getRenderData(level, pos, state, modelData))
+            .build();
+    }
+
+    @Override
+    public void collectParts(RandomSource random, List<BlockModelPart> parts, ModelData modelData, RenderType renderType){
+        // Get render data
+        RenderData renderData = modelData.get(RENDER_DATA);
+        if(renderData == null)
+            renderData = this.getRenderData(null, null, null, modelData);
+
+        // Get seed to reset random instance
+        long seed = random.nextLong();
+        random.setSeed(seed);
+
+        // Check whether the breaking overlay is being rendered
+        boolean isBreakingOverlay = FusionClient.isRenderingBreakingOverlay();
+
+        // Check whether we need to check the models' render types against the given one
+        boolean doRenderTypeCheck = renderType != null && renderData.state != null;
+
+        // Default model
+        if(renderData.defaultModel != -1){
+            ConditionalModel override = this.defaultModelOverrides.get(renderData.defaultModel);
+            if((!isBreakingOverlay || override.showBreakingOverlay)
+                && (!doRenderTypeCheck || override.model.getRenderTypes(renderData.state, random, renderData.defaultModelData).contains(renderType)))
+                override.model.collectParts(random, parts, renderData.defaultModelData, renderType);
+        }else if(!doRenderTypeCheck || this.original.getRenderTypes(renderData.state, random, renderData.defaultModelData).contains(renderType))
+            this.original.collectParts(random, parts, renderData.defaultModelData, renderType);
+
+        // Append models
+        for(int i = 0; i < this.appendModels.size(); i++){
+            if(renderData.appendModels[i] == -1)
+                continue;
+            ConditionalModel conditional = this.appendModels.get(i).get(renderData.appendModels[i]);
+            if(!isBreakingOverlay || conditional.showBreakingOverlay){
+                random.setSeed(seed);
+                ModelData conditionalData = renderData.appendModelsData[i];
+                if(!doRenderTypeCheck || conditional.model.getRenderTypes(renderData.state, random, conditionalData).contains(renderType))
+                    conditional.model.collectParts(random, parts, conditionalData, renderType);
             }
         }
     }
 
     @Override
     public void collectParts(RandomSource random, List<BlockModelPart> parts){
-        if(!this.showBreakingOverlay && FusionClient.IS_RENDERING_BREAKING_OVERLAY.get() != null){
-            this.original.collectParts(random, parts, ModelData.EMPTY, null);
-            return;
-        }
-        this.models.forEach(model -> model.collectParts(random, parts, ModelData.EMPTY, null));
+        this.collectParts(random, parts, ModelData.EMPTY, null);
     }
 
     @Override
-    public ChunkRenderTypeSet getRenderTypes(@NotNull BlockState state, @NotNull RandomSource rand, @NotNull ModelData data){
-        // Start with block's default render types
-        ChunkRenderTypeSet renderTypes = this.hasSimpleModels ? BlockStateModel.super.getRenderTypes(state, rand, ModelData.EMPTY) : ChunkRenderTypeSet.none();
-        // Get model data properties
-        ModelData[] arr = data.get(DATA_PROPERTY);
-        Long seed = data.get(SEED_PROPERTY);
-        // When rendering breaking overlay, only submit the original model's render types
-        if(!this.showBreakingOverlay && FusionClient.IS_RENDERING_BREAKING_OVERLAY.get() != null){
-            if(this.isOriginalSimpleModel)
-                return renderTypes;
-            ModelData subData = arr == null || arr[0] == null ? ModelData.EMPTY : arr[0];
-            if(seed != null)
-                rand.setSeed(seed);
-            return this.original.getRenderTypes(state, rand, subData);
+    public ChunkRenderTypeSet getRenderTypes(@NotNull BlockState state, @NotNull RandomSource random, @NotNull ModelData modelData){
+        // Get render data
+        RenderData renderData = modelData.get(RENDER_DATA);
+        if(renderData == null)
+            renderData = this.getRenderData(null, null, null, modelData);
+
+        // Get seed to reset random instance
+        long seed = random.nextLong();
+        random.setSeed(seed);
+
+        // Check whether the breaking overlay is being rendered
+        boolean isBreakingOverlay = FusionClient.isRenderingBreakingOverlay();
+
+        // Collect all render types
+        ChunkRenderTypeSet renderTypes = ChunkRenderTypeSet.none();
+
+        // Default model
+        if(renderData.defaultModel != -1){
+            ConditionalModel override = this.defaultModelOverrides.get(renderData.defaultModel);
+            if(!isBreakingOverlay || override.showBreakingOverlay)
+                renderTypes = ChunkRenderTypeSet.union(renderTypes, override.model.getRenderTypes(state, random, renderData.defaultModelData));
+        }else
+            renderTypes = ChunkRenderTypeSet.union(renderTypes, this.original.getRenderTypes(state, random, renderData.defaultModelData));
+
+        // Append models
+        for(int i = 0; i < this.appendModels.size(); i++){
+            if(renderData.appendModels[i] == -1)
+                continue;
+            ConditionalModel conditional = this.appendModels.get(i).get(renderData.appendModels[i]);
+            if(!isBreakingOverlay || conditional.showBreakingOverlay){
+                random.setSeed(seed);
+                renderTypes = ChunkRenderTypeSet.union(renderTypes, conditional.model.getRenderTypes(state, random, renderData.appendModelsData[i]));
+            }
         }
-        // If there's only simple models, just return the block's default render types
-        if(!this.hasNonSimpleModels)
-            return BlockStateModel.super.getRenderTypes(state, rand, ModelData.EMPTY);
-        // Gather render types from complex models
-        for(int i = 0; i < this.nonSimpleModels.size(); i++){
-            ModelData subData = arr == null || arr[i] == null ? ModelData.EMPTY : arr[i];
-            if(seed != null)
-                rand.setSeed(seed);
-            renderTypes = ChunkRenderTypeSet.union(this.nonSimpleModels.get(i).getRenderTypes(state, rand, subData), renderTypes);
-        }
+
         return renderTypes;
     }
 
     @Override
-    public ModelData getModelData(BlockAndTintGetter level, BlockPos pos, BlockState state, ModelData data){
-        // If there's only simple models, no need for model data
-        if(!this.hasNonSimpleModels)
-            return ModelData.EMPTY;
-        // Add seed and block state
-        ModelData.Builder builder = ModelData.builder()
-            .with(SEED_PROPERTY, state.getSeed(pos))
-            .with(STATE_PROPERTY, state);
-        // Gather model data for complex models
-        ModelData[] arr = new ModelData[this.nonSimpleModels.size()];
-        for(int i = 0; i < this.nonSimpleModels.size(); i++)
-            arr[i] = this.nonSimpleModels.get(i).getModelData(level, pos, state, data);
-        return builder.with(DATA_PROPERTY, arr).build();
-    }
-
-    @Override
-    public TextureAtlasSprite particleIcon(ModelData data){
-        if(this.isOriginalSimpleModel)
-            return this.original.particleIcon(ModelData.EMPTY);
-        // Get appropriate model data
-        ModelData[] arr = data.get(DATA_PROPERTY);
-        ModelData subData = arr == null || arr[0] == null ? ModelData.EMPTY : arr[0];
-        return this.original.particleIcon(subData);
-    }
-
-    @Override
     public TextureAtlasSprite particleIcon(){
-        return this.original.particleIcon(ModelData.EMPTY);
+        return this.particleSprite;
+    }
+
+    @Override
+    public TextureAtlasSprite particleIcon(@NotNull ModelData data){
+        // Get render data
+        RenderData renderData = data.get(RENDER_DATA);
+        if(renderData == null)
+            return this.particleIcon();
+
+        if(renderData.defaultModel != -1){
+            ConditionalModel override = this.defaultModelOverrides.get(renderData.defaultModel);
+            return override.model.particleIcon(renderData.defaultModelData);
+        }
+        return this.original.particleIcon(renderData.defaultModelData);
+    }
+
+    record ConditionalModel(BlockStateModel model, @Nullable BlockStateModelPredicate conditions, boolean showBreakingOverlay) {
+    }
+
+    private record RenderData(BlockState state, int defaultModel, ModelData defaultModelData, int[] appendModels, ModelData[] appendModelsData) {
     }
 }

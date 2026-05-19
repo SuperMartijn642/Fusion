97142fff5e152d574683982e9fad81a37203d1bc
SuperMartijn624 <martijnvandenbrand@outlook.com>
2026-05-19 19:18:28 +0200

    Overhaul block model modifiers
    Allow conditions for models.
    Order modifiers by a `priority` property.
    Add `default_model_overrides`.
    Add `show_breaking_overlay` option for each model individually.

--- src/main/java/com/supermartijn642/fusion/model/modifiers/block/BlockModelModifierBakedModel.java
+++ src/main/java/com/supermartijn642/fusion/model/modifiers/block/BlockModelModifierBakedModel.java
@@ -1,10 +1,10 @@
 package com.supermartijn642.fusion.model.modifiers.block;
 
 import com.supermartijn642.fusion.FusionClient;
+import com.supermartijn642.fusion.api.model.predicates.blockstate.BlockStateModelPredicate;
 import net.minecraft.client.renderer.block.BlockAndTintGetter;
 import net.minecraft.client.renderer.block.dispatch.BlockStateModel;
 import net.minecraft.client.renderer.block.dispatch.BlockStateModelPart;
-import net.minecraft.client.renderer.block.dispatch.SingleVariant;
 import net.minecraft.client.resources.model.geometry.BakedQuad;
 import net.minecraft.client.resources.model.sprite.Material;
 import net.minecraft.core.BlockPos;
@@ -13,8 +13,8 @@ import net.minecraft.world.level.block.state.BlockState;
 import net.minecraftforge.client.model.data.ModelData;
 import net.minecraftforge.client.model.data.ModelProperty;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
-import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -22,119 +22,152 @@ import java.util.List;
  */
 public class BlockModelModifierBakedModel implements BlockStateModel {
 
-    private static final ModelProperty<Long> SEED_PROPERTY = new ModelProperty<>();
-    private static final ModelProperty<ModelData[]> DATA_PROPERTY = new ModelProperty<>();
-    private static final ModelProperty<BlockState> STATE_PROPERTY = new ModelProperty<>();
+    private static final ModelProperty<RenderData> RENDER_DATA = new ModelProperty<>();
 
     private final BlockStateModel original;
-    private final List<BlockStateModel> models;
-    private final boolean showBreakingOverlay;
-    private final boolean isOriginalSimpleModel;
-    private final boolean hasSimpleModels, hasNonSimpleModels;
-    private final List<BlockStateModel> nonSimpleModels;
+    private final List<ConditionalModel> defaultModelOverrides;
+    private final List<List<ConditionalModel>> appendModels;
+    private final Material.Baked particleMaterial;
     private final int materialFlags;
 
-    public BlockModelModifierBakedModel(BlockStateModel original, List<BlockStateModel> models, boolean showBreakingOverlay){
+    BlockModelModifierBakedModel(BlockStateModel original, List<ConditionalModel> defaultModelOverrides, List<List<ConditionalModel>> appendModels){
         this.original = original;
-        this.models = new ArrayList<>(models.size() + 1);
-        this.models.add(original);
-        this.models.addAll(models);
-        this.showBreakingOverlay = showBreakingOverlay;
-        List<BlockStateModel> nonSimpleModels = new ArrayList<>();
-        for(BlockStateModel model : this.models){
-            if(!model.getClass().equals(SingleVariant.class))
-                nonSimpleModels.add(model);
-        }
-        this.isOriginalSimpleModel = original.getClass().equals(SingleVariant.class);
-        this.hasSimpleModels = nonSimpleModels.size() < this.models.size();
-        this.hasNonSimpleModels = !nonSimpleModels.isEmpty();
-        this.nonSimpleModels = nonSimpleModels.isEmpty() ? null : List.copyOf(nonSimpleModels);
+        this.defaultModelOverrides = defaultModelOverrides;
+        this.appendModels = appendModels;
 
-        int materialFlags = 0;
-        for(BlockStateModel model : this.models)
-            materialFlags |= model.materialFlags();
+        // Resolve particle material
+        Material.Baked particleMaterial = null;
+        for(ConditionalModel override : this.defaultModelOverrides){
+            if(override.conditions == null || override.conditions.test(null, null, null)){
+                particleMaterial = override.model.particleMaterial();
+                break;
+            }
+        }
+        if(particleMaterial == null)
+            particleMaterial = this.original.particleMaterial();
+        this.particleMaterial = particleMaterial;
+
+        // Resolve material flags
+        int materialFlags = original.materialFlags();
+        for(ConditionalModel model : defaultModelOverrides)
+            materialFlags |= model.model.materialFlags();
+        for(List<ConditionalModel> conditionals : appendModels){
+            for(ConditionalModel model : conditionals){
+                materialFlags |= model.model.materialFlags();
+            }
+        }
         this.materialFlags = materialFlags;
     }
 
-    @Override
-    public void collectParts(RandomSource random, List<BlockStateModelPart> parts, ModelData data){
-        // Get model data properties
-        Long seed = data.get(SEED_PROPERTY);
-        ModelData[] arr = data.get(DATA_PROPERTY);
-        BlockState state = data.get(STATE_PROPERTY);
-        // When rendering breaking overlay, only submit the original model
-        if(!this.showBreakingOverlay && FusionClient.IS_RENDERING_BREAKING_OVERLAY.get() != null){
-            if(this.isOriginalSimpleModel){
-                this.original.collectParts(random, parts, ModelData.EMPTY);
-                return;
+    private RenderData getRenderData(@Nullable BlockAndTintGetter level, @Nullable BlockPos pos, @Nullable BlockState state, ModelData modelData){
+        boolean hasAllArguments = level != null && pos != null && state != null;
+
+        // Default model
+        int defaultModel = -1;
+        ModelData defaultModelData = null;
+        for(int i = 0; i < this.defaultModelOverrides.size(); i++){
+            ConditionalModel override = this.defaultModelOverrides.get(i);
+            if(override.conditions == null || override.conditions.test(level, pos, state)){
+                defaultModel = i;
+                defaultModelData = hasAllArguments ? override.model.getModelData(level, pos, state, modelData) : modelData;
             }
-            ModelData subData = arr == null || arr[0] == null ? ModelData.EMPTY : arr[0];
-            if(seed != null)
+        }
+        if(defaultModel == -1)
+            defaultModelData = hasAllArguments ? this.original.getModelData(level, pos, state, modelData) : modelData;
+
+        // Append models
+        int[] appendModels = new int[this.appendModels.size()];
+        ModelData[] appendModelsData = new ModelData[this.appendModels.size()];
+        appendLoop:
+        for(int i = 0; i < this.appendModels.size(); i++){
+            List<ConditionalModel> appendEntry = this.appendModels.get(i);
+            appendModels[i] = -1;
+            // First model whose conditions are met is submitted
+            for(int j = 0; j < appendEntry.size(); j++){
+                ConditionalModel conditional = appendEntry.get(j);
+                if(conditional.conditions == null || conditional.conditions.test(level, pos, state)){
+                    appendModels[i] = j;
+                    appendModelsData[i] = hasAllArguments ? conditional.model.getModelData(level, pos, state, modelData) : modelData;
+                    break;
+                }
+            }
+        }
+
+        return new RenderData(defaultModel, defaultModelData, appendModels, appendModelsData);
+    }
+
+    @Override
+    public @NotNull ModelData getModelData(@NotNull BlockAndTintGetter level, @NotNull BlockPos pos, @NotNull BlockState state, @NotNull ModelData modelData){
+        return ModelData.builder()
+            .with(RENDER_DATA, this.getRenderData(level, pos, state, modelData))
+            .build();
+    }
+
+    @Override
+    public void collectParts(RandomSource random, List<BlockStateModelPart> parts, ModelData modelData){
+        // Get render data
+        RenderData renderData = modelData.get(RENDER_DATA);
+        if(renderData == null)
+            renderData = this.getRenderData(null, null, null, modelData);
+
+        // Get seed to reset random instance
+        long seed = random.nextLong();
+        random.setSeed(seed);
+
+        // Check whether the breaking overlay is being rendered
+        boolean isBreakingOverlay = FusionClient.isRenderingBreakingOverlay();
+
+        // Default model
+        if(renderData.defaultModel != -1){
+            ConditionalModel override = this.defaultModelOverrides.get(renderData.defaultModel);
+            if(!isBreakingOverlay || override.showBreakingOverlay)
+                override.model.collectParts(random, parts, renderData.defaultModelData);
+        }else
+            this.original.collectParts(random, parts, renderData.defaultModelData);
+
+        // Append models
+        for(int i = 0; i < this.appendModels.size(); i++){
+            if(renderData.appendModels[i] == -1)
+                continue;
+            ConditionalModel conditional = this.appendModels.get(i).get(renderData.appendModels[i]);
+            if(!isBreakingOverlay || conditional.showBreakingOverlay){
                 random.setSeed(seed);
-            this.original.collectParts(random, parts, subData);
-            return;
-        }
-        // If there's only simple models, use vanilla method
-        if(!this.hasNonSimpleModels){
-            this.collectParts(random, parts);
-            return;
-        }
-        // Submit all models
-        int i = 0;
-        for(BlockStateModel model : this.models){
-            if(model.getClass().equals(SingleVariant.class)) // For simple models, don't increase the model data array index
-                model.collectParts(random, parts, ModelData.EMPTY);
-            else{ // For complex models, get the correct model data
-                ModelData subData = arr == null || arr[i] == null ? ModelData.EMPTY : arr[i];
-                if(seed != null)
-                    random.setSeed(seed);
-                model.collectParts(random, parts, subData);
-                i++;
+                conditional.model.collectParts(random, parts, renderData.appendModelsData[i]);
             }
         }
     }
 
     @Override
     public void collectParts(RandomSource random, List<BlockStateModelPart> parts){
-        if(!this.showBreakingOverlay && FusionClient.IS_RENDERING_BREAKING_OVERLAY.get() != null){
-            this.original.collectParts(random, parts, ModelData.EMPTY);
-            return;
-        }
-        this.models.forEach(model -> model.collectParts(random, parts, ModelData.EMPTY));
-    }
-
-    @Override
-    public ModelData getModelData(BlockAndTintGetter level, BlockPos pos, BlockState state, ModelData data){
-        // If there's only simple models, no need for model data
-        if(!this.hasNonSimpleModels)
-            return ModelData.EMPTY;
-        // Add seed and block state
-        ModelData.Builder builder = ModelData.builder()
-            .with(SEED_PROPERTY, state.getSeed(pos))
-            .with(STATE_PROPERTY, state);
-        // Gather model data for complex models
-        ModelData[] arr = new ModelData[this.nonSimpleModels.size()];
-        for(int i = 0; i < this.nonSimpleModels.size(); i++)
-            arr[i] = this.nonSimpleModels.get(i).getModelData(level, pos, state, data);
-        return builder.with(DATA_PROPERTY, arr).build();
-    }
-
-    @Override
-    public Material.Baked particleMaterial(@NotNull ModelData data){
-        if(this.isOriginalSimpleModel)
-            return this.original.particleMaterial(ModelData.EMPTY);
-        // Get appropriate model data
-        ModelData[] arr = data.get(DATA_PROPERTY);
-        ModelData subData = arr == null || arr[0] == null ? ModelData.EMPTY : arr[0];
-        return this.original.particleMaterial(subData);
+        this.collectParts(random, parts, ModelData.EMPTY);
     }
 
     @Override
     public Material.Baked particleMaterial(){
-        return this.original.particleMaterial(ModelData.EMPTY);
+        return this.particleMaterial;
+    }
+
+    @Override
+    public Material.Baked particleMaterial(@NotNull ModelData data){
+        // Get render data
+        RenderData renderData = data.get(RENDER_DATA);
+        if(renderData == null)
+            return this.particleMaterial();
+
+        if(renderData.defaultModel != -1){
+            ConditionalModel override = this.defaultModelOverrides.get(renderData.defaultModel);
+            return override.model.particleMaterial(renderData.defaultModelData);
+        }
+        return this.original.particleMaterial(renderData.defaultModelData);
     }
 
     public @BakedQuad.MaterialFlags int materialFlags(){
         return this.materialFlags;
+    }
+
+    record ConditionalModel(BlockStateModel model, @Nullable BlockStateModelPredicate conditions, boolean showBreakingOverlay) {
+    }
+
+    private record RenderData(int defaultModel, ModelData defaultModelData, int[] appendModels, ModelData[] appendModelsData) {
     }
 }

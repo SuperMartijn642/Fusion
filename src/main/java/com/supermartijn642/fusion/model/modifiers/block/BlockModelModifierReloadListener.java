78e231e6b040b0ecf93ef3eb11239ec6b7c021e4
SuperMartijn624 <martijnvandenbrand@outlook.com>
2026-05-19 21:44:33 +0200

    Use resource location as secondary ordering for block and item model modifiers

--- src/main/java/com/supermartijn642/fusion/model/modifiers/block/BlockModelModifierReloadListener.java
+++ src/main/java/com/supermartijn642/fusion/model/modifiers/block/BlockModelModifierReloadListener.java
@@ -76,7 +76,7 @@ public class BlockModelModifierReloadListener {
 
             // Sort modifier properties by their priority
             List<Properties> modifiers = entry.getValue();
-            modifiers.sort(Comparator.comparingInt(p -> p.priority));
+            modifiers.sort(Comparator.<Properties>comparingInt(p -> p.priority).thenComparing(p -> p.location));
 
             // Resolve default model overwrites
             List<BlockModelModifierBakedModel.ConditionalModel> defaultModelOverrides = createConditionModels(
@@ -145,14 +145,14 @@ public class BlockModelModifierReloadListener {
             }
             JsonObject json = entry.getValue().getAsJsonObject();
             try{
-                this.parseResource(json);
+                this.parseResource(json, location);
             }catch(JsonParseException e){
                 LoggingHelper.logUserError(e, "Failed to parse block model modifier '%s':", location);
             }
         }
     }
 
-    private void parseResource(JsonObject json){
+    private void parseResource(JsonObject json, ResourceLocation location){
         // Get the targets
         if(!json.has("targets") || !json.get("targets").isJsonArray())
             throw new JsonParseException("Model modifier must have array property 'targets'!");
@@ -248,7 +248,7 @@ public class BlockModelModifierReloadListener {
         }
 
         // Put the properties into the map
-        Properties properties = new Properties(priority, defaultModel, appendModels, paneCullingFix, showBreakingOverlay);
+        Properties properties = new Properties(priority, location, defaultModel, appendModels, paneCullingFix, showBreakingOverlay);
         for(ModelResourceLocation target : targets)
             this.modifiers.computeIfAbsent(target, t -> new ArrayList<>(8)).add(properties);
     }
@@ -386,7 +386,7 @@ public class BlockModelModifierReloadListener {
         return List.of(new ModelEntry(model, conditions, showBreakingOverlay));
     }
 
-    private record Properties(int priority, List<ModelEntry> defaultModelOverrides, List<List<ModelEntry>> appendModels, boolean paneCullingFix, Boolean showBreakingOverlay) {
+    private record Properties(int priority, ResourceLocation location, List<ModelEntry> defaultModelOverrides, List<List<ModelEntry>> appendModels, boolean paneCullingFix, Boolean showBreakingOverlay) {
     }
 
     private record ModelEntry(ResourceLocation model, @Nullable BlockStateModelPredicate conditions, Boolean showBreakingOverlay) {

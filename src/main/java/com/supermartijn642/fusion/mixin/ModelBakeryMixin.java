ca0628b2f57be3c6f751c04b1642a414962c5541
SuperMartijn624 <martijnvandenbrand@outlook.com>
2026-05-19 21:40:18 +0200

    Overhaul item model modifiers
    Order modifiers by a `priority` property.
    Add `default_model_overrides`.

--- src/main/java/com/supermartijn642/fusion/mixin/ModelBakeryMixin.java
+++ src/main/java/com/supermartijn642/fusion/mixin/ModelBakeryMixin.java
@@ -53,7 +53,7 @@ public class ModelBakeryMixin {
             // Apply Fusion model modifiers
             ModelBakery.ModelBakerImpl resolver = ((ModelBakery)(Object)this).new ModelBakerImpl(textureGetter);
             BlockModelModifierReloadListener.INSTANCE.applyModelModifiers(results, resolver);
-            ItemModelModifierReloadListener.INSTANCE.applyPredicateModels(results, new ItemModel.BakingContext(
+            ItemModelModifierReloadListener.INSTANCE.applyModelModifiers(results, new ItemModel.BakingContext(
                 resolver,
                 this.entityModelSet,
                 results.missingModels().item(),

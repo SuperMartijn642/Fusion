77f6d957d8a8c45692d739563d3570f9591b2320
SuperMartijn624 <martijnvandenbrand@outlook.com>
2026-06-06 01:45:27 +0200

    Fix emissive dependent properties in `MutableQuadImpl#toBakedQuad`

--- src/main/java/com/supermartijn642/fusion/model/custom/quad/MutableQuadImpl.java
+++ src/main/java/com/supermartijn642/fusion/model/custom/quad/MutableQuadImpl.java
@@ -292,7 +292,7 @@ public class MutableQuadImpl implements MutableQuad {
                 this.tintIndex,
                 this.facing,
                 this.sprite,
-                this.emissive || this.shade,
+                !this.emissive && this.shade,
                 !this.emissive && this.ambientOcclusion
             );
         }

49cd0384ca47d55d8600cb0d72e4395e9f650f58
SuperMartijn624 <martijnvandenbrand@outlook.com>
2026-06-06 01:14:00 +0200

    Serialize cuboid models using base model type

--- src/main/java/com/supermartijn642/fusion/model/types/cuboid/CuboidModelType.java
+++ src/main/java/com/supermartijn642/fusion/model/types/cuboid/CuboidModelType.java
@@ -2,17 +2,21 @@ package com.supermartijn642.fusion.model.types.cuboid;
 
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParseException;
+import com.supermartijn642.fusion.api.model.DefaultModelTypes;
 import com.supermartijn642.fusion.api.model.custom.UntypedModelInstance;
 import com.supermartijn642.fusion.api.model.custom.geometry.CuboidModelGeometry;
 import com.supermartijn642.fusion.api.model.custom.geometry.ModelGeometry;
+import com.supermartijn642.fusion.api.model.types.base.BaseModelData;
 import com.supermartijn642.fusion.api.util.Either;
 import com.supermartijn642.fusion.model.types.UnknownModelType;
 import net.minecraft.client.renderer.block.model.BlockElement;
 import net.minecraft.client.renderer.block.model.BlockModel;
+import net.minecraft.client.renderer.block.model.TextureSlots;
 import net.minecraft.resources.ResourceLocation;
 
 import java.util.Collection;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created 29/04/2023 by SuperMartijn642
@@ -43,7 +47,29 @@ public class CuboidModelType extends UnknownModelType<BlockModel> {
     }
 
     @Override
-    public JsonObject serialize(BlockModel value){
-        return (JsonObject)CuboidModelSerializer.GSON.toJsonTree(value);
+    public JsonObject serialize(BlockModel model){
+        // Use base model type to serialize vanilla cuboid model
+        BaseModelData.Builder<?,BaseModelData> builder = BaseModelData.builder();
+        // Copy properties
+        builder.parent(model.getParentLocation())
+            .guiLight(model.getGuiLight())
+            .ambientOcclusion(model.getAmbientOcclusion())
+            .itemTransforms(model.getTransforms());
+        // Copy materials
+        for(Map.Entry<String,TextureSlots.SlotContents> entry : model.getTextureSlots().values().entrySet()){
+            String key = entry.getKey();
+            switch(entry.getValue()){
+                case TextureSlots.Reference reference -> builder.material(key, reference.target());
+                case TextureSlots.Value value -> builder.material(key, value.material().texture());
+            }
+        }
+        // Copy elements
+        List<BlockElement> elements = model.getElements();
+        if(elements != null){
+            for(BlockElement element : elements)
+                builder.elements(CuboidModelGeometry.Element.of(element));
+        }
+        // Serialize data
+        return DefaultModelTypes.BASE.serialize(builder.build());
     }
 }

c61853c5ddc3f224bc35fb21ce424ed97942a50e
SuperMartijn624 <martijnvandenbrand@outlook.com>
2026-06-06 00:59:09 +0200

    Fix scaling for origin of element rotation when serializing base model

--- src/main/java/com/supermartijn642/fusion/model/types/base/BaseModelType.java
+++ src/main/java/com/supermartijn642/fusion/model/types/base/BaseModelType.java
@@ -481,7 +481,9 @@ public abstract class BaseModelType<T extends BaseModelData, BUILDER extends Bas
 
     protected JsonObject serializeRotation(BlockPartRotation rotation){
         JsonObject rotationJson = new JsonObject();
-        rotationJson.add("origin", this.serializeVector3f(rotation.origin));
+        Vector3f origin = rotation.origin.copy();
+        origin.mul(16);
+        rotationJson.add("origin", this.serializeVector3f(origin));
         rotationJson.addProperty("axis", rotation.axis.name().toLowerCase(Locale.ROOT));
         rotationJson.addProperty("angle", rotation.angle);
         if(rotation.rescale)

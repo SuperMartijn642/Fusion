9ebe6de8508162494e27bddab06b4ccd2ad536c9
SuperMartijn624 <martijnvandenbrand@outlook.com>
2026-06-06 21:49:37 +0200

    Unify model baking behaviour and provide texture initialization with model and geometry properties

--- src/main/java/com/supermartijn642/fusion/model/custom/geometry/ModelGeometryImpl.java
+++ src/main/java/com/supermartijn642/fusion/model/custom/geometry/ModelGeometryImpl.java
@@ -4,13 +4,13 @@ import com.mojang.blaze3d.vertex.PoseStack;
 import com.mojang.blaze3d.vertex.VertexConsumer;
 import com.mojang.blaze3d.vertex.VertexMultiConsumer;
 import com.supermartijn642.fusion.Fusion;
-import com.supermartijn642.fusion.api.model.custom.CullableQuads;
 import com.supermartijn642.fusion.api.model.custom.ModelMaterial;
 import com.supermartijn642.fusion.api.model.custom.ModelTransform;
 import com.supermartijn642.fusion.api.model.custom.geometry.CuboidModelGeometry;
 import com.supermartijn642.fusion.api.model.custom.geometry.ModelGeometry;
 import com.supermartijn642.fusion.api.model.custom.quad.MutableQuad;
 import com.supermartijn642.fusion.api.util.Either;
+import com.supermartijn642.fusion.api.util.PropertyGetter;
 import com.supermartijn642.fusion.model.FusionBlockModelData;
 import com.supermartijn642.fusion.model.WrappedBakedModel;
 import com.supermartijn642.fusion.util.EmptyBlockAndTintGetter;
@@ -106,7 +106,7 @@ public class ModelGeometryImpl implements ModelGeometry {
     }
 
     @Override
-    public CullableQuads bake(ModelTransform transformation, MaterialResolver materialResolver){
+    public void bake(QuadConsumer consumer, ModelTransform transformation, MaterialResolver materialResolver){
         // Bake the model
         Function<Material,TextureAtlasSprite> spriteGetter = material -> materialResolver.get(material.texture().toString());
         ModelBakery modelBakery = FusionBlockModelData.modelBakery.get();
@@ -120,17 +120,17 @@ public class ModelGeometryImpl implements ModelGeometry {
             throw new RuntimeException("Encountered an exception baking model of class '" + this.model.getClass().getName() + "'!", e);
         }
         if(baked == null)
-            return CullableQuads.empty();
+            return;
 
         // Collect all quads from the model
-        CullableQuads.Builder quads = CullableQuads.builder();
         Minecraft.getInstance().getBlockRenderer().getModelRenderer().tesselateBlock(
             EmptyBlockAndTintGetter.INSTANCE,
             new WrappedBakedModel(baked) {
                 @Override
                 public void emitBlockQuads(BlockAndTintGetter blockView, BlockState state, BlockPos pos, Supplier<RandomSource> randomSupplier, RenderContext context){
                     context.pushTransform(quad -> {
-                        quads.add(quad.cullFace(), MutableQuad.create().copyFrapiQuad(quad));
+                        MutableQuad mutableQuad = MutableQuad.create().copyFrapiQuad(quad);
+                        consumer.consume(mutableQuad, quad.cullFace(), PropertyGetter.empty());
                         return false;
                     });
                     ((FabricBakedModel)baked).emitBlockQuads(blockView, state, pos, randomSupplier, context);
@@ -151,6 +151,5 @@ public class ModelGeometryImpl implements ModelGeometry {
             42,
             OverlayTexture.NO_OVERLAY
         );
-        return quads.build();
     }
 }

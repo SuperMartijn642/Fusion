fb8d53d96f1fe0eb6f2570a67374d7c87a41c126
SuperMartijn624 <martijnvandenbrand@outlook.com>
2026-06-07 16:01:05 +0200

    Handle Forge/NeoForge 'render_type' property

---
+++ src/main/java/com/supermartijn642/fusion/util/ForgeNamedRenderTypeGroupHelper.java
@@ -0,0 +1,36 @@
+package com.supermartijn642.fusion.util;
+
+import com.google.common.collect.ImmutableMap;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraftforge.client.RenderTypeGroup;
+
+import java.util.*;
+
+/**
+ * Helps with going from render type group -> identifier as Forge has no way to do this.
+ * <p>
+ * Created 07/06/2026 by SuperMartijn642
+ */
+public class ForgeNamedRenderTypeGroupHelper {
+
+    private static Map<RenderTypeGroup,ResourceLocation> RENDER_TYPE_GROUPS = Map.of();
+
+    public static void updateMappings(ImmutableMap<ResourceLocation,RenderTypeGroup> renderTypes){
+        // Sort identifiers, so behavior is consistent
+        List<ResourceLocation> identifiers = new ArrayList<>(renderTypes.keySet());
+        identifiers.sort(Comparator.comparing(ResourceLocation::toString));
+
+        // Build render type group -> identifier map
+        Map<RenderTypeGroup,ResourceLocation> reverseMapping = new HashMap<>();
+        for(ResourceLocation identifier : identifiers){
+            RenderTypeGroup renderTypeGroup = renderTypes.get(identifier);
+            if(!reverseMapping.containsKey(renderTypeGroup))
+                reverseMapping.put(renderTypeGroup, identifier);
+        }
+        RENDER_TYPE_GROUPS = Map.copyOf(reverseMapping);
+    }
+
+    public static ResourceLocation getIdentifier(RenderTypeGroup renderTypeGroup){
+        return RENDER_TYPE_GROUPS.get(renderTypeGroup);
+    }
+}

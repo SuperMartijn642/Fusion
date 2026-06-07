b3097c8943a84087f5dc19b39906ba1fd673bb73
SuperMartijn624 <martijnvandenbrand@outlook.com>
2026-06-07 14:48:29 +0200

    Rename `MaterialResolver` to `MaterialKeyResolver`

--- src/main/java/com/supermartijn642/fusion/model/custom/geometry/ModelGeometryImpl.java
+++ src/main/java/com/supermartijn642/fusion/model/custom/geometry/ModelGeometryImpl.java
@@ -33,7 +33,7 @@ public class ModelGeometryImpl implements ModelGeometry {
         return new ModelGeometryImpl(geometry);
     }
 
-    public static TextureSlots createTextureSlots(MaterialResolver materialResolver){
+    public static TextureSlots createTextureSlots(MaterialKeyResolver materialResolver){
         return new TextureSlots(Map.of()) {
             @Override
             public @Nullable Material getMaterial(String reference){
@@ -45,10 +45,10 @@ public class ModelGeometryImpl implements ModelGeometry {
         };
     }
 
-    public static MaterialResolver fromKeyLookup(Function<String,Either<String,ModelMaterial>> lookup,
-                                                 Function<ModelMaterial,TextureAtlasSprite> materialResolver,
-                                                 Consumer<String> reportMissing,
-                                                 Consumer<List<String>> reportCircular){
+    public static MaterialKeyResolver fromKeyLookup(Function<String,Either<String,ModelMaterial>> lookup,
+                                                    Function<ModelMaterial,TextureAtlasSprite> materialResolver,
+                                                    Consumer<String> reportMissing,
+                                                    Consumer<List<String>> reportCircular){
         Map<String,TextureAtlasSprite> resolvedMaterials = new HashMap<>();
         return (key, required) -> {
             // Check if the key has already been resolved
@@ -98,7 +98,7 @@ public class ModelGeometryImpl implements ModelGeometry {
     }
 
     @Override
-    public void bake(QuadConsumer consumer, ModelTransform transformation, MaterialResolver materialResolver){
+    public void bake(QuadConsumer consumer, ModelTransform transformation, MaterialKeyResolver materialResolver){
         // Create dummy texture slots instance
         TextureSlots textureSlots = createTextureSlots(materialResolver);
         // Create dummy sprite getter

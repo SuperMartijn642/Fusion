c34f6430acdb261d22b11544771215337ee9c0e1
SuperMartijn624 <martijnvandenbrand@outlook.com>
2026-06-07 14:48:29 +0200

    Rename `MaterialResolver` to `MaterialKeyResolver`

--- src/main/java/com/supermartijn642/fusion/model/SimpleModelType.java
+++ src/main/java/com/supermartijn642/fusion/model/SimpleModelType.java
@@ -43,7 +43,7 @@ public abstract class SimpleModelType<T> implements ModelType<T> {
             PropertyStore propertyStore = PropertyStore.create();
             // Resolve materials
             Set<String> missingKeys = new HashSet<>();
-            ModelGeometry.MaterialResolver materialResolver = ModelGeometry.MaterialResolver.fromKeyLookup(
+            ModelGeometry.MaterialKeyResolver materialResolver = ModelGeometry.MaterialKeyResolver.fromKeyLookup(
                 key -> modelStack.findMaterialIncludingParents(key, context),
                 context::getMaterial,
                 missingKeys::add,
@@ -138,7 +138,7 @@ public abstract class SimpleModelType<T> implements ModelType<T> {
             PropertyStore propertyStore = PropertyStore.create();
             // Resolve materials
             Set<String> missingKeys = new HashSet<>();
-            ModelGeometry.MaterialResolver materialResolver = ModelGeometry.MaterialResolver.fromKeyLookup(
+            ModelGeometry.MaterialKeyResolver materialResolver = ModelGeometry.MaterialKeyResolver.fromKeyLookup(
                 key -> modelStack.findMaterialIncludingParents(key, context),
                 context::getMaterial,
                 missingKeys::add,
@@ -244,7 +244,7 @@ public abstract class SimpleModelType<T> implements ModelType<T> {
     protected abstract ResourceLocation getParent(T data);
 
     protected void bakeGeometry(BlockStateModelBakingContext context, ModelStack modelStack, T data,
-                                ModelTransform transform, ModelGeometry.MaterialResolver materialResolver,
+                                ModelTransform transform, ModelGeometry.MaterialKeyResolver materialResolver,
                                 ModelGeometry.QuadConsumer quadConsumer){
         this.getGeometry(data).bake(quadConsumer, transform, materialResolver);
     }

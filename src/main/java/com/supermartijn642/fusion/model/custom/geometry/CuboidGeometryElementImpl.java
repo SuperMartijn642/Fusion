33f38e13db25b31d40114f603bab9aa0923a8cb0
SuperMartijn624 <martijnvandenbrand@outlook.com>
2026-06-10 15:43:23 +0200

    Fix `CuboidGeometryElementImpl#of`

--- src/main/java/com/supermartijn642/fusion/model/custom/geometry/CuboidGeometryElementImpl.java
+++ src/main/java/com/supermartijn642/fusion/model/custom/geometry/CuboidGeometryElementImpl.java
@@ -25,22 +25,29 @@ public class CuboidGeometryElementImpl implements CuboidModelGeometry.Element {
     }
 
     public static CuboidModelGeometry.Element of(BlockElement element){
-        return builder()
+        CuboidModelGeometry.Element.Builder builder = builder()
             .fromTo(element.from, element.to)
             .rotation(element.rotation)
-            .face(Direction.UP, CuboidModelGeometry.Face.of(element.faces.get(Direction.UP)))
-            .face(Direction.DOWN, CuboidModelGeometry.Face.of(element.faces.get(Direction.DOWN)))
-            .face(Direction.NORTH, CuboidModelGeometry.Face.of(element.faces.get(Direction.NORTH)))
-            .face(Direction.EAST, CuboidModelGeometry.Face.of(element.faces.get(Direction.EAST)))
-            .face(Direction.SOUTH, CuboidModelGeometry.Face.of(element.faces.get(Direction.SOUTH)))
-            .face(Direction.WEST, CuboidModelGeometry.Face.of(element.faces.get(Direction.WEST)))
             .shade(element.shade ? null : false)
             .ambientOcclusion(element.getFaceData().ambientOcclusion() ? null : false)
             .property(DefaultModelProperties.NEO_GEOMETRY_COLOR, element.getFaceData().color())
             .property(DefaultModelProperties.NEO_GEOMETRY_BLOCK_LIGHT, element.getFaceData().blockLight())
             .property(DefaultModelProperties.NEO_GEOMETRY_SKY_LIGHT, element.getFaceData().skyLight())
-            .property(DefaultModelProperties.NEO_GEOMETRY_AMBIENT_OCCLUSION, element.getFaceData().ambientOcclusion())
-            .build();
+            .property(DefaultModelProperties.NEO_GEOMETRY_AMBIENT_OCCLUSION, element.getFaceData().ambientOcclusion());
+        // Faces
+        if(element.faces.get(Direction.UP) != null)
+            builder.face(Direction.UP, CuboidModelGeometry.Face.of(element.faces.get(Direction.UP)));
+        if(element.faces.get(Direction.DOWN) != null)
+            builder.face(Direction.DOWN, CuboidModelGeometry.Face.of(element.faces.get(Direction.DOWN)));
+        if(element.faces.get(Direction.NORTH) != null)
+            builder.face(Direction.NORTH, CuboidModelGeometry.Face.of(element.faces.get(Direction.NORTH)));
+        if(element.faces.get(Direction.EAST) != null)
+            builder.face(Direction.EAST, CuboidModelGeometry.Face.of(element.faces.get(Direction.EAST)));
+        if(element.faces.get(Direction.SOUTH) != null)
+            builder.face(Direction.SOUTH, CuboidModelGeometry.Face.of(element.faces.get(Direction.SOUTH)));
+        if(element.faces.get(Direction.WEST) != null)
+            builder.face(Direction.WEST, CuboidModelGeometry.Face.of(element.faces.get(Direction.WEST)));
+        return builder.build();
     }
 
     private final Vector3fc from, to;

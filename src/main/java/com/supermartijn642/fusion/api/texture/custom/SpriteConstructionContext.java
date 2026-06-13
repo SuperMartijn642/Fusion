7b771f8a4f6771a9377587ffe78eed1577513a6d
SuperMartijn624 <martijnvandenbrand@outlook.com>
2026-06-14 00:30:39 +0200

    Add missing `NonExtendable` in API

--- src/main/java/com/supermartijn642/fusion/api/texture/custom/SpriteConstructionContext.java
+++ src/main/java/com/supermartijn642/fusion/api/texture/custom/SpriteConstructionContext.java
@@ -1,11 +1,12 @@
 package com.supermartijn642.fusion.api.texture.custom;
 
-
 import net.minecraft.client.renderer.texture.TextureAtlas;
+import org.jetbrains.annotations.ApiStatus;
 
 /**
  * Created 29/04/2023 by SuperMartijn642
  */
+@ApiStatus.NonExtendable
 public interface SpriteConstructionContext {
 
     /**

7080d2d28d9cf9a98597457d3a40bc121bc823c4
SuperMartijn624 <martijnvandenbrand@outlook.com>
2026-06-15 14:35:33 +0200

    Add `ignore_missing` option for registry entries

--- src/main/java/com/supermartijn642/fusion/model/predicates/item/PotionItemModelPredicate.java
+++ src/main/java/com/supermartijn642/fusion/model/predicates/item/PotionItemModelPredicate.java
@@ -2,6 +2,7 @@ package com.supermartijn642.fusion.model.predicates.item;
 
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParseException;
+import com.supermartijn642.fusion.api.model.predicates.item.DefaultItemModelPredicates;
 import com.supermartijn642.fusion.api.model.predicates.item.ItemModelPredicate;
 import com.supermartijn642.fusion.api.util.Serializer;
 import com.supermartijn642.fusion.util.IdentifierUtil;
@@ -33,14 +34,24 @@ public class PotionItemModelPredicate implements ItemModelPredicate {
     public static final Serializer<PotionItemModelPredicate> SERIALIZER = new Serializer<>() {
         @Override
         public PotionItemModelPredicate deserialize(JsonObject json) throws JsonParseException{
+            boolean ignoreMissing = false;
+            if(json.has("ignore_missing")){
+                if(!json.get("ignore_missing").isJsonPrimitive() || !json.getAsJsonPrimitive("ignore_missing").isBoolean())
+                    throw new JsonParseException("Property 'ignore_missing' must be a boolean!");
+                ignoreMissing = json.get("ignore_missing").getAsBoolean();
+            }
+
             if(!json.has("potion") || !json.get("potion").isJsonPrimitive() || !json.getAsJsonPrimitive("potion").isString())
                 throw new JsonParseException("Potion-predicate must have string property 'enchantment'!");
             if(!IdentifierUtil.isValidIdentifier(json.get("potion").getAsString()))
-                throw new JsonParseException("Property 'enchantment' must be a valid identifier, not '" + json.get("potion").getAsString() + "'!");
+                throw new JsonParseException("Property 'potion' must be a valid identifier, not '" + json.get("potion").getAsString() + "'!");
             ResourceLocation potionIdentifier = ResourceLocation.parse(json.get("potion").getAsString());
             Optional<Holder.Reference<Potion>> potion = BuiltInRegistries.POTION.getHolder(potionIdentifier);
-            if(potion.isEmpty())
+            if(potion.isEmpty()){
+                if(ignoreMissing)
+                    return new PotionItemModelPredicate(null);
                 throw new JsonParseException("Unknown potion '" + potionIdentifier + "'!");
+            }
             return new PotionItemModelPredicate(potion.get());
         }
 
@@ -60,8 +71,15 @@ public class PotionItemModelPredicate implements ItemModelPredicate {
 
     @Override
     public boolean test(ItemStack stack){
+        if(this.potion == null)
+            return false;
         PotionContents potion = stack.get(DataComponents.POTION_CONTENTS);
         return potion != null && potion.is(this.potion);
+    }
+
+    @Override
+    public ItemModelPredicate simplify(){
+        return this.potion == null ? DefaultItemModelPredicates.never() : this;
     }
 
     @Override

5230a990c4d4cd8d1d006d90df52de795e6fb1b2
SuperMartijn624 <martijnvandenbrand@outlook.com>
2026-06-15 14:35:33 +0200

    Add `ignore_missing` option for registry entries

--- src/main/java/com/supermartijn642/fusion/texture/types/connecting/predicates/MatchBlockInFrontConnectionPredicate.java
+++ src/main/java/com/supermartijn642/fusion/texture/types/connecting/predicates/MatchBlockInFrontConnectionPredicate.java
@@ -4,6 +4,7 @@ import com.google.gson.JsonObject;
 import com.google.gson.JsonParseException;
 import com.supermartijn642.fusion.api.texture.types.connecting.predicates.ConnectionDirection;
 import com.supermartijn642.fusion.api.texture.types.connecting.predicates.ConnectionPredicate;
+import com.supermartijn642.fusion.api.texture.types.connecting.predicates.DefaultConnectionPredicates;
 import com.supermartijn642.fusion.api.util.Serializer;
 import com.supermartijn642.fusion.util.IdentifierUtil;
 import net.minecraft.core.Direction;
@@ -28,13 +29,23 @@ public class MatchBlockInFrontConnectionPredicate implements ConnectionPredicate
     public static final Serializer<MatchBlockInFrontConnectionPredicate> SERIALIZER = new Serializer<>() {
         @Override
         public MatchBlockInFrontConnectionPredicate deserialize(JsonObject json) throws JsonParseException{
+            boolean ignoreMissing = false;
+            if(json.has("ignore_missing")){
+                if(!json.get("ignore_missing").isJsonPrimitive() || !json.getAsJsonPrimitive("ignore_missing").isBoolean())
+                    throw new JsonParseException("Property 'ignore_missing' must be a boolean!");
+                ignoreMissing = json.get("ignore_missing").getAsBoolean();
+            }
+
             if(!json.has("block") || !json.get("block").isJsonPrimitive() || !json.getAsJsonPrimitive("block").isString())
                 throw new JsonParseException("Match block predicate must have string property 'block'!");
             if(!IdentifierUtil.isValidIdentifier(json.get("block").getAsString()))
                 throw new JsonParseException("Property 'block' must be a valid identifier!");
             ResourceLocation identifier = new ResourceLocation(json.get("block").getAsString());
-            if(!Registry.BLOCK.containsKey(identifier))
+            if(!Registry.BLOCK.containsKey(identifier)){
+                if(ignoreMissing)
+                    return new MatchBlockInFrontConnectionPredicate(null);
                 throw new JsonParseException("Unknown block '" + identifier + "'!");
+            }
             Block block = Registry.BLOCK.get(identifier);
             return new MatchBlockInFrontConnectionPredicate(block);
         }
@@ -55,7 +66,12 @@ public class MatchBlockInFrontConnectionPredicate implements ConnectionPredicate
 
     @Override
     public boolean shouldConnect(Direction side, @Nullable BlockState ownState, BlockState otherState, BlockState blockInFront, ConnectionDirection direction){
-        return blockInFront.getBlock() == this.block;
+        return this.block != null && blockInFront.getBlock() == this.block;
+    }
+
+    @Override
+    public ConnectionPredicate simplify(){
+        return this.block == null ? DefaultConnectionPredicates.never() : this;
     }
 
     @Override
@@ -65,14 +81,13 @@ public class MatchBlockInFrontConnectionPredicate implements ConnectionPredicate
 
     @Override
     public final boolean equals(Object o){
-        if(this == o) return true;
         if(!(o instanceof MatchBlockInFrontConnectionPredicate that)) return false;
 
-        return this.block.equals(that.block);
+        return Objects.equals(this.block, that.block);
     }
 
     @Override
     public int hashCode(){
-        return this.block.hashCode();
+        return Objects.hashCode(this.block);
     }
 }

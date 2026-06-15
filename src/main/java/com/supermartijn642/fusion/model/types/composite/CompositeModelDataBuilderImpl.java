8e2f51324aa6711cb5ce262067c6acc60d1fe2f3
SuperMartijn624 <martijnvandenbrand@outlook.com>
2026-06-15 20:29:47 +0200

    Add `composite` model type

---
+++ src/main/java/com/supermartijn642/fusion/model/types/composite/CompositeModelDataBuilderImpl.java
@@ -0,0 +1,108 @@
+package com.supermartijn642.fusion.model.types.composite;
+
+import com.google.common.collect.ImmutableList;
+import com.supermartijn642.fusion.api.model.custom.ModelMaterial;
+import com.supermartijn642.fusion.api.model.types.base.BaseModelData;
+import com.supermartijn642.fusion.api.model.types.composite.CompositeModelData;
+import com.supermartijn642.fusion.api.util.Either;
+import net.minecraft.client.renderer.model.ItemCameraTransforms;
+import net.minecraft.client.renderer.model.ItemOverride;
+import net.minecraft.client.renderer.model.ItemTransformVec3f;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created 15/06/2026 by SuperMartijn642
+ */
+public class CompositeModelDataBuilderImpl implements CompositeModelData.Builder {
+
+    public static CompositeModelData.Builder builder(){
+        return new CompositeModelDataBuilderImpl();
+    }
+
+    private final List<List<CompositeModelData.ModelEntry>> modelSeries = new ArrayList<>();
+    private final BaseModelData.Builder<?,BaseModelData> baseModelData = BaseModelData.builder();
+
+    private CompositeModelDataBuilderImpl(){
+    }
+
+    @Override
+    public CompositeModelData.Builder model(CompositeModelData.ModelEntry entry){
+        this.modelSeries.add(ImmutableList.of(entry));
+        return this;
+    }
+
+    @Override
+    public CompositeModelData.Builder modelSeries(CompositeModelData.ModelEntry... entries){
+        if(entries.length != 0)
+            this.modelSeries.add(ImmutableList.copyOf(entries));
+        return this;
+    }
+
+    @Override
+    public CompositeModelData.Builder modelSeries(List<CompositeModelData.ModelEntry> entries){
+        if(!entries.isEmpty())
+            this.modelSeries.add(ImmutableList.copyOf(entries));
+        return this;
+    }
+
+    @Override
+    public CompositeModelData.Builder material(String key, Either<String,ModelMaterial> material){
+        this.baseModelData.material(key, material);
+        return this;
+    }
+
+    @Override
+    public CompositeModelData.Builder isGui3d(@Nullable Boolean isGui3d){
+        this.baseModelData.isGui3d(isGui3d);
+        return this;
+    }
+
+    @Override
+    public CompositeModelData.Builder ambientOcclusion(@Nullable Boolean ambientOcclusion){
+        this.baseModelData.ambientOcclusion(ambientOcclusion);
+        return this;
+    }
+
+    @Override
+    public CompositeModelData.Builder itemTransform(ItemCameraTransforms.TransformType type, @Nullable ItemTransformVec3f transform){
+        this.baseModelData.itemTransform(type, transform);
+        return this;
+    }
+
+    @Override
+    public CompositeModelData.Builder itemTransforms(ItemCameraTransforms itemTransforms){
+        this.baseModelData.itemTransforms(itemTransforms);
+        return this;
+    }
+
+    @Override
+    public CompositeModelData.Builder itemOverrides(ItemOverride... overrides){
+        this.baseModelData.itemOverrides(overrides);
+        return this;
+    }
+
+    @Override
+    public CompositeModelData.Builder shade(@Nullable Boolean shade){
+        this.baseModelData.shade(shade);
+        return this;
+    }
+
+    @Override
+    public CompositeModelData.Builder emissive(@Nullable Boolean emissive){
+        this.baseModelData.emissive(emissive);
+        return this;
+    }
+
+    @Override
+    public CompositeModelData build(){
+        if(this.modelSeries.isEmpty())
+            throw new IllegalStateException("Composite model data must have at least one model entry!");
+        return new CompositeModelDataImpl(
+            ImmutableList.copyOf(this.modelSeries),
+            this.baseModelData.build()
+        );
+    }
+}
---
+++ src/main/java/com/supermartijn642/fusion/model/types/composite/CompositeModelDataImpl.java
@@ -0,0 +1,107 @@
+package com.supermartijn642.fusion.model.types.composite;
+
+import com.supermartijn642.fusion.api.model.ModelInstance;
+import com.supermartijn642.fusion.api.model.custom.ModelMaterial;
+import com.supermartijn642.fusion.api.model.custom.ModelTransform;
+import com.supermartijn642.fusion.api.model.predicates.ModelPredicate;
+import com.supermartijn642.fusion.api.model.types.base.BaseModelData;
+import com.supermartijn642.fusion.api.model.types.composite.CompositeModelData;
+import com.supermartijn642.fusion.api.util.Either;
+import net.minecraft.client.renderer.model.ItemCameraTransforms;
+import net.minecraft.client.renderer.model.ItemOverride;
+import net.minecraft.client.renderer.model.ItemTransformVec3f;
+import net.minecraft.util.ResourceLocation;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created 15/06/2026 by SuperMartijn642
+ */
+public class CompositeModelDataImpl implements CompositeModelData {
+
+    public static CompositeModelData.ModelEntry entry(ResourceLocation model, ModelTransform transform, @Nullable ModelPredicate condition){
+        return new ModelEntryImpl(Either.left(model), transform, condition);
+    }
+
+    public static CompositeModelData.ModelEntry entry(ModelInstance<?> model, ModelTransform transform, @Nullable ModelPredicate condition){
+        return new ModelEntryImpl(Either.right(model), transform, condition);
+    }
+
+    private final List<List<ModelEntry>> models;
+    private final BaseModelData baseModelData;
+
+    CompositeModelDataImpl(List<List<ModelEntry>> models, BaseModelData baseModelData){
+        this.models = models;
+        this.baseModelData = baseModelData;
+    }
+
+    @Override
+    public List<List<ModelEntry>> getModels(){
+        return this.models;
+    }
+
+    @Override
+    public Map<String,Either<String,ModelMaterial>> getMaterials(){
+        return this.baseModelData.getMaterials();
+    }
+
+    @Override
+    public @Nullable Boolean getAmbientOcclusion(){
+        return this.baseModelData.getAmbientOcclusion();
+    }
+
+    @Override
+    public @Nullable Boolean getIsGui3d(){
+        return this.baseModelData.getIsGui3d();
+    }
+
+    @Override
+    public @Nullable ItemTransformVec3f getItemTransform(ItemCameraTransforms.TransformType type){
+        return this.baseModelData.getItemTransform(type);
+    }
+
+    @Override
+    public List<ItemOverride> getItemOverrides(){
+        return this.baseModelData.getItemOverrides();
+    }
+
+    @Override
+    public @Nullable Boolean getShade(){
+        return this.baseModelData.getShade();
+    }
+
+    @Override
+    public @Nullable Boolean getEmissive(){
+        return this.baseModelData.getEmissive();
+    }
+
+    private static class ModelEntryImpl implements CompositeModelData.ModelEntry {
+
+        private final Either<ResourceLocation,ModelInstance<?>> model;
+        private final ModelTransform transform;
+        private final ModelPredicate condition;
+
+        private ModelEntryImpl(Either<ResourceLocation,ModelInstance<?>> model, ModelTransform transform, ModelPredicate condition){
+            this.model = model;
+            this.transform = transform;
+            this.condition = condition;
+        }
+
+        @Override
+        public Either<ResourceLocation,ModelInstance<?>> getModel(){
+            return this.model;
+        }
+
+        @Override
+        public ModelTransform getTransform(){
+            return this.transform;
+        }
+
+        @Override
+        public @Nullable ModelPredicate getCondition(){
+            return this.condition;
+        }
+    }
+}

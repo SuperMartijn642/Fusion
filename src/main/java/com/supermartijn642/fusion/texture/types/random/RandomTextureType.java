1e46776ca88bbe2709ac85dfc7178cf6eee62c2f
SuperMartijn624 <martijnvandenbrand@outlook.com>
2026-06-15 13:37:00 +0200

    Increase max random texture grid size

--- src/main/java/com/supermartijn642/fusion/texture/types/random/RandomTextureType.java
+++ src/main/java/com/supermartijn642/fusion/texture/types/random/RandomTextureType.java
@@ -275,7 +275,7 @@ public class RandomTextureType implements TextureType<RandomTextureData,Stitched
         };
     }
 
-    public static final int MAX_SIZE = 10;
+    public static final int MAX_SIZE = 100;
 
     @Override
     public RandomTextureData deserialize(JsonObject json) throws JsonParseException{

a2b51d4395737b6bc3ee0197b613858e2791c6bb
SuperMartijn624 <martijnvandenbrand@outlook.com>
2026-06-17 22:45:44 +0200

    Fix mutable block pos setting for match block and match state block state predicates

--- src/main/java/com/supermartijn642/fusion/model/predicates/blockstate/MatchBlockBlockStatePredicate.java
+++ src/main/java/com/supermartijn642/fusion/model/predicates/blockstate/MatchBlockBlockStatePredicate.java
@@ -226,7 +226,7 @@ public class MatchBlockBlockStatePredicate implements BlockStateModelPredicate {
         if(level == null || pos == null)
             return this.containsAir;
         for(BlockPos offset : this.offsets){
-            this.dummyBlockPos.set(pos).offset(offset);
+            this.dummyBlockPos.set(pos).move(offset);
             state = level.getBlockState(this.dummyBlockPos);
             if(state.isAir()){
                 if(this.containsAir)

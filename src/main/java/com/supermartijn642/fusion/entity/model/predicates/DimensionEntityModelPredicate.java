0e5d0f003674ef3bfdb61a3158018fd73d54d07d
SuperMartijn624 <martijnvandenbrand@outlook.com>
2026-06-17 21:39:57 +0200

    Allow checking multiple blocks/biomes/dimensions at once in most predicate types

--- src/main/java/com/supermartijn642/fusion/entity/model/predicates/DimensionEntityModelPredicate.java
+++ src/main/java/com/supermartijn642/fusion/entity/model/predicates/DimensionEntityModelPredicate.java
@@ -1,5 +1,7 @@
 package com.supermartijn642.fusion.entity.model.predicates;
 
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParseException;
 import com.supermartijn642.fusion.api.util.Serializer;
@@ -9,20 +11,23 @@ import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.level.Level;
 
-import java.util.Objects;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 /**
  * Created 20/09/2024 by SuperMartijn642
  */
 public class DimensionEntityModelPredicate implements EntityModelPredicate {
 
-    public static EntityModelPredicate create(ResourceKey<Level> dimension){
-        return new DimensionEntityModelPredicate(dimension.location());
+    public static EntityModelPredicate create(ResourceKey<Level>... dimensions){
+        return new DimensionEntityModelPredicate(Arrays.stream(dimensions).map(ResourceKey::location).collect(Collectors.toSet()));
     }
 
-    public static EntityModelPredicate create(ResourceLocation dimension){
-        Objects.requireNonNull(dimension);
-        return new DimensionEntityModelPredicate(dimension);
+    public static EntityModelPredicate create(ResourceLocation... dimensions){
+        return new DimensionEntityModelPredicate(Arrays.asList(dimensions));
     }
 
     public static final Serializer<DimensionEntityModelPredicate> SERIALIZER = new Serializer<>() {
@@ -34,35 +39,81 @@ public class DimensionEntityModelPredicate implements EntityModelPredicate {
                 // Ignore on this version
             }
 
-            if(!json.has("dimension") || !json.get("dimension").isJsonPrimitive() || !json.getAsJsonPrimitive("dimension").isString())
-                throw new JsonParseException("Dimension-predicate must have string property 'dimension'!");
-            if(!IdentifierUtil.isValidIdentifier(json.get("dimension").getAsString()))
-                throw new JsonParseException("Dimension must be a valid identifier, not '" + json.get("dimension").getAsString() + "'!");
-            return new DimensionEntityModelPredicate(new ResourceLocation(json.get("dimension").getAsString()));
+            if(!json.has("dimension") && !json.has("dimensions"))
+                throw new JsonParseException("Dimension predicate must have either property 'dimension' or 'dimensions'!");
+            if(json.has("dimension") && json.has("dimensions"))
+                throw new JsonParseException("Dimension predicate must have either property 'dimension' or 'dimensions', not both!");
+            Set<ResourceLocation> dimensions;
+            if(json.has("dimensions")){
+                if(!json.get("dimension").isJsonPrimitive() || !json.getAsJsonPrimitive("dimension").isString())
+                    throw new JsonParseException("Property 'dimension' must be a string!");
+                if(!IdentifierUtil.isValidIdentifier(json.get("dimension").getAsString()))
+                    throw new JsonParseException("Property 'dimension' must be a valid identifier, not '" + json.get("dimension").getAsString() + "'!");
+                dimensions = Set.of(new ResourceLocation(json.get("dimension").getAsString()));
+            }else{
+                if(!json.get("dimensions").isJsonArray())
+                    throw new JsonParseException("Property 'dimensions' must be an array!");
+                JsonArray array = json.getAsJsonArray("dimensions");
+                dimensions = new HashSet<>(array.size());
+                for(JsonElement element : array){
+                    if(!element.isJsonPrimitive() || !element.getAsJsonPrimitive().isString())
+                        throw new JsonParseException("Array property 'dimensions' must only contain strings!");
+                    if(!IdentifierUtil.isValidIdentifier(element.getAsString()))
+                        throw new JsonParseException("Dimension entries must be a valid identifier, not '" + element.getAsString() + "'!");
+                    dimensions.add(new ResourceLocation(element.getAsString()));
+                }
+            }
+            return new DimensionEntityModelPredicate(dimensions);
         }
 
         @Override
         public JsonObject serialize(DimensionEntityModelPredicate value){
             JsonObject json = new JsonObject();
-            json.addProperty("dimension", value.dimension.toString());
+            if(value.dimensions.size() == 1)
+                json.addProperty("dimension", value.dimensions.iterator().next().toString());
+            else{
+                JsonArray biomes = new JsonArray(value.dimensions.size());
+                value.dimensions.stream()
+                    .map(ResourceLocation::toString)
+                    .sorted()
+                    .forEach(biomes::add);
+                json.add("dimensions", biomes);
+            }
             return json;
         }
     };
 
-    private final ResourceLocation dimension;
+    private final Set<ResourceLocation> dimensions;
 
-    private DimensionEntityModelPredicate(ResourceLocation dimension){
-        this.dimension = dimension;
+    private DimensionEntityModelPredicate(Collection<ResourceLocation> dimensions){
+        this.dimensions = Set.copyOf(dimensions);
     }
 
     @Override
     public boolean test(Entity entity){
         Level level = entity.level();
-        return level != null && level.dimension().location().equals(this.dimension);
+        return level != null && this.dimensions.contains(level.dimension().location());
+    }
+
+    @Override
+    public EntityModelPredicate simplify(){
+        return this.dimensions.isEmpty() ? DefaultEntityModelPredicates.never() : this;
     }
 
     @Override
     public Serializer<? extends EntityModelPredicate> getSerializer(){
         return SERIALIZER;
+    }
+
+    @Override
+    public final boolean equals(Object o){
+        if(!(o instanceof DimensionEntityModelPredicate that)) return false;
+
+        return this.dimensions.equals(that.dimensions);
+    }
+
+    @Override
+    public int hashCode(){
+        return this.dimensions.hashCode();
     }
 }

1af935e84ee05a35d933ac213e5e7237e6a6c34f
SuperMartijn624 <martijnvandenbrand@outlook.com>
2026-06-18 23:00:28 +0200

    Fix model warning string formatting

--- src/main/java/com/supermartijn642/fusion/model/FusionBlockModelData.java
+++ src/main/java/com/supermartijn642/fusion/model/FusionBlockModelData.java
@@ -155,7 +155,7 @@ public class FusionBlockModelData extends BlockModel {
             bakedModel = context.getMissingBakedModel();
         // Log warnings
         if(!warnings.isEmpty())
-            LoggingHelper.logUserWarnings(warnings, "Warnings for block model '{}':", this.identifier);
+            LoggingHelper.logUserWarnings(warnings, "Warnings for block model '%s':", this.identifier);
         return bakedModel;
     }
 

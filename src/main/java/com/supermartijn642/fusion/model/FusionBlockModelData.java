b15466f684a53627889676c035415a25a330b8b0
SuperMartijn624 <martijnvandenbrand@outlook.com>
2026-06-18 23:00:28 +0200

    Fix model warning string formatting

--- src/main/java/com/supermartijn642/fusion/model/FusionBlockModelData.java
+++ src/main/java/com/supermartijn642/fusion/model/FusionBlockModelData.java
@@ -95,7 +95,7 @@ public class FusionBlockModelData {
             model = context.getMissingBlockStateModel();
         // Log warnings
         if(!warnings.isEmpty())
-            LoggingHelper.logUserWarnings(warnings, "Warnings for block model '{}':", this.identifier);
+            LoggingHelper.logUserWarnings(warnings, "Warnings for block model '%s':", this.identifier);
 
         // Clear missing models
         MISSING_MODEL.remove();
@@ -131,7 +131,7 @@ public class FusionBlockModelData {
             model = context.getMissingItemModel();
         // Log warnings
         if(!warnings.isEmpty())
-            LoggingHelper.logUserWarnings(warnings, "Warnings for item model '{}':", this.identifier);
+            LoggingHelper.logUserWarnings(warnings, "Warnings for item model '%s':", this.identifier);
 
         // Clear missing models
         MISSING_MODEL.remove();

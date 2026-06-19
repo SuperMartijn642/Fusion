fabe39a720aca41fae49f7e169d96c890f32fac1
Drex <nicknamedrex@gmail.com>
2026-06-19 23:37:33 +0200

    Improve fusion override folder logic (#224)
    Instead of manually rewriting the pack loading logic to support asset
    overrides we simply inject the fusion override folder as the first
    overlay in the pack.
    
    This improves mod compatibility, see https://github.com/DrexHD/quick-pack/issues/3
    And reduces the risk of outdated code staying when vanilla makes changes.
    
    This approach should be applicable to all version after 1.20.2 (where
    overlays where introduced in vanilla)

--- src/main/java/com/supermartijn642/fusion/extensions/PackResourcesExtension.java
+++
@@ -1,9 +0,0 @@
-package com.supermartijn642.fusion.extensions;
-
-/**
- * Created 19/10/2023 by SuperMartijn642
- */
-public interface PackResourcesExtension {
-
-    void setFusionOverridesFolder(String folder);
-}

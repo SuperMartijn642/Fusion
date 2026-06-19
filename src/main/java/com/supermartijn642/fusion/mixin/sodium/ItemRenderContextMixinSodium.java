1597449ba9760cd4d22b34cb4d07621113b46e55
SuperMartijn624 <martijnvandenbrand@outlook.com>
2026-06-19 13:22:10 +0200

    Fix Sodium integration

--- src/main/java/com/supermartijn642/fusion/mixin/sodium/ItemRenderContextMixinSodium.java
+++ src/main/java/com/supermartijn642/fusion/mixin/sodium/ItemRenderContextMixinSodium.java
@@ -4,12 +4,11 @@ import com.supermartijn642.fusion.api.texture.SpriteHelper;
 import com.supermartijn642.fusion.api.texture.custom.TextureInstance;
 import com.supermartijn642.fusion.api.texture.types.base.BaseTextureData;
 import com.supermartijn642.fusion.texture.QuadTintingHelper;
-import net.caffeinemc.mods.sodium.api.util.ColorMixer;
 import net.caffeinemc.mods.sodium.client.render.frapi.render.ItemRenderContext;
-import net.caffeinemc.mods.sodium.client.render.model.MutableQuadViewImpl;
-import net.caffeinemc.mods.sodium.client.render.model.SodiumQuadAtlas;
+import net.caffeinemc.mods.sodium.client.render.frapi.wrapper.MutableQuadViewWrapper;
 import net.caffeinemc.mods.sodium.client.render.texture.SodiumSpriteFinder;
 import net.caffeinemc.mods.sodium.client.render.texture.SpriteFinderCache;
+import net.fabricmc.fabric.api.client.renderer.v1.mesh.QuadAtlas;
 import net.minecraft.client.renderer.texture.TextureAtlasSprite;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -28,19 +27,19 @@ public class ItemRenderContextMixinSodium {
         cancellable = true,
         remap = false
     )
-    private void tintQuad(MutableQuadViewImpl quad, CallbackInfo ci){
+    private void tintQuad(MutableQuadViewWrapper quad, CallbackInfo ci){
         // In case texture has a custom tinting set, replace the original tinting
-        if(quad.getTintIndex() == 39216){
-            SodiumSpriteFinder spriteFinder = quad.getQuadAtlas() == SodiumQuadAtlas.ITEM ?
+        if(quad.tintIndex() == 39216){
+            SodiumSpriteFinder spriteFinder = quad.atlas() == QuadAtlas.ITEM ?
                 SpriteFinderCache.forItemAtlas() : SpriteFinderCache.forBlockAtlas();
-            TextureAtlasSprite sprite = quad.sprite(spriteFinder);
+            TextureAtlasSprite sprite = quad.getOriginal().sprite(spriteFinder);
             TextureInstance<?> textureInstance = SpriteHelper.getTextureInstance(sprite);
             if(textureInstance != null && textureInstance.getCustomData() instanceof BaseTextureData data){
                 BaseTextureData.QuadTinting tinting = data.getTinting();
                 if(tinting != null){
                     int color = 0xFF000000 | QuadTintingHelper.getInWorldColor(tinting, null, null, null);
                     for(int i = 0; i < 4; i++)
-                        quad.setColor(i, ColorMixer.mulComponentWise(color, quad.getColor(i)));
+                        quad.multiplyColor(color);
                     ci.cancel();
                 }
             }

98c5d6869aeeb1981622da454754bcc96ffb6576
SuperMartijn624 <martijnvandenbrand@outlook.com>
2026-06-19 14:38:46 +0200

    Don't clear atlas sprites map when ModernFix is installed

--- src/main/java/com/supermartijn642/fusion/mixin/FusionMixinPlugin.java
+++ src/main/java/com/supermartijn642/fusion/mixin/FusionMixinPlugin.java
@@ -15,10 +15,12 @@ import java.util.Set;
 public class FusionMixinPlugin implements IMixinConfigPlugin {
 
     private boolean isEmbeddiumLoaded;
+    private boolean isModernFixLoaded;
 
     @Override
     public void onLoad(String mixinPackage){
         this.isEmbeddiumLoaded = isClassAvailable("org.embeddedt.embeddium.api.eventbus.EmbeddiumEvent");
+        this.isModernFixLoaded = isClassAvailable("org.embeddedt.modernfix.ModernFix");
     }
 
     private static boolean isClassAvailable(String className){
@@ -37,6 +39,8 @@ public class FusionMixinPlugin implements IMixinConfigPlugin {
 
     @Override
     public boolean shouldApplyMixin(String targetClassName, String mixinClassName){
+        if(this.isModernFixLoaded && mixinClassName.endsWith("ModelManagerMixin"))
+            return false;
         return true;
     }
 
@@ -49,6 +53,8 @@ public class FusionMixinPlugin implements IMixinConfigPlugin {
         List<String> mixins = new ArrayList<>();
         if(this.isEmbeddiumLoaded)
             mixins.add("embeddium.BlockRendererMixinEmbeddium");
+        if(this.isModernFixLoaded)
+            mixins.add("modernfix.ModelManagerMixinModernFix");
         return mixins;
     }
 

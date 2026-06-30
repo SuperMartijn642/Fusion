e1354233410263e4d561f660d8e07125e4904d4c
SuperMartijn624 <martijnvandenbrand@outlook.com>
2026-06-30 21:14:31 +0200

    Add dumb workaround for Sodium `InterpolationData#uploadInterpolatedFrame` overwrite #295

--- src/main/java/com/supermartijn642/fusion/mixin/FusionMixinPlugin.java
+++ src/main/java/com/supermartijn642/fusion/mixin/FusionMixinPlugin.java
@@ -54,6 +54,7 @@ public class FusionMixinPlugin implements IMixinConfigPlugin {
         if(this.isSodiumLoaded){
             mixins.add("sodium.BlockRendererMixinSodium");
             mixins.add("sodium.WorldSliceMixinSodium");
+            mixins.add("sodium.SpriteContentsInterpolationDataMixinSodium");
         }
         if(this.isIndiumLoaded){
             mixins.add("indium.AbstractBlockRenderContextMixinIndium");

b438a1e926d8cb20fb10967ed482a71d6977649d
SuperMartijn624 <martijnvandenbrand@outlook.com>
2026-06-30 23:44:02 +0200

    Fix parsing of 'blocks' for many predicate types

--- src/main/java/com/supermartijn642/fusion/texture/types/connecting/predicates/MatchBlockInFrontConnectionPredicate.java
+++ src/main/java/com/supermartijn642/fusion/texture/types/connecting/predicates/MatchBlockInFrontConnectionPredicate.java
@@ -72,9 +72,8 @@ public class MatchBlockInFrontConnectionPredicate implements ConnectionPredicate
                         if(block.isEmpty()){
                             if(!ignoreMissing)
                                 throw new JsonParseException("Unknown block '" + identifier + "'!");
-                            blocks = List.of();
                         }else
-                            blocks = List.of(block.get());
+                            blocks.add(block.get());
                     }catch(JsonParseException e){
                         throw new JsonParseException("Failed to parse 'blocks' entry", e);
                     }

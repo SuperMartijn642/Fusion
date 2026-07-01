733023eda9b21f008253d8416df32b6030864eb0
SuperMartijn624 <martijnvandenbrand@outlook.com>
2026-07-01 16:43:53 +0200

    Fix model transform composition order for `SimpleModelType#bakeBlockStateModel`

--- src/main/java/com/supermartijn642/fusion/model/SimpleModelType.java
+++ src/main/java/com/supermartijn642/fusion/model/SimpleModelType.java
@@ -48,7 +48,7 @@ public abstract class SimpleModelType<T> implements ModelType<T> {
             );
             // Compose transformations
             ModelTransform transforms = modelStack.composeTransforms();
-            transforms = ModelTransform.compose(transforms, context.getTransformation());
+            transforms = ModelTransform.compose(context.getTransformation(), transforms);
             // Combine conditions
             ModelPredicate conditions = modelStack.combineConditions();
             if(conditions != null)

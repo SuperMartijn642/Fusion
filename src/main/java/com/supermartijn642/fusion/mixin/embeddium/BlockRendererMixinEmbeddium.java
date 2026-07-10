12a84d026c116abe0242d8f1707604b07d359427
SuperMartijn624 <martijnvandenbrand@outlook.com>
2026-07-10 14:02:58 +0200

    Allow block model modifiers to overwrite block random offset

--- src/main/java/com/supermartijn642/fusion/mixin/embeddium/BlockRendererMixinEmbeddium.java
+++ src/main/java/com/supermartijn642/fusion/mixin/embeddium/BlockRendererMixinEmbeddium.java
@@ -3,20 +3,33 @@ package com.supermartijn642.fusion.mixin.embeddium;
 import com.supermartijn642.fusion.api.texture.SpriteHelper;
 import com.supermartijn642.fusion.api.texture.custom.TextureInstance;
 import com.supermartijn642.fusion.api.texture.types.base.BaseTextureData;
+import com.supermartijn642.fusion.model.modifiers.block.BlockModelModifierBakedModel;
+import com.supermartijn642.fusion.model.modifiers.block.ModelsByRandomOffset;
 import com.supermartijn642.fusion.texture.QuadTintingHelper;
 import me.jellysquid.mods.sodium.client.model.color.ColorProvider;
 import me.jellysquid.mods.sodium.client.model.quad.BakedQuadView;
+import me.jellysquid.mods.sodium.client.render.chunk.compile.ChunkBuildBuffers;
 import me.jellysquid.mods.sodium.client.render.chunk.compile.pipeline.BlockRenderContext;
 import me.jellysquid.mods.sodium.client.render.chunk.compile.pipeline.BlockRenderer;
+import me.jellysquid.mods.sodium.client.world.WorldSlice;
 import net.caffeinemc.mods.sodium.api.util.ColorARGB;
+import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.block.model.BakedQuad;
 import net.minecraft.client.renderer.texture.TextureAtlasSprite;
+import net.minecraft.client.resources.model.BakedModel;
+import net.minecraft.core.BlockPos;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.Vec3;
+import net.minecraftforge.client.model.data.ModelData;
+import org.joml.Vector3fc;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import java.util.Arrays;
@@ -30,6 +43,69 @@ public class BlockRendererMixinEmbeddium {
     @Final
     @Shadow(remap = false)
     private int[] quadColors;
+
+    @Unique
+    private final ModelsByRandomOffset modelsByRandomOffset = new ModelsByRandomOffset();
+    @Unique
+    private BlockRenderContext dummyRenderContext;
+    @Unique
+    private final BlockPos.MutableBlockPos dummyOrigin = new BlockPos.MutableBlockPos();
+
+    @Shadow
+    private void renderModel(BlockRenderContext context, ChunkBuildBuffers buffers){
+        throw new AssertionError();
+    }
+
+    @Inject(
+        method = "renderModel",
+        at = @At("HEAD"),
+        cancellable = true,
+        remap = false
+    )
+    private void renderModel(BlockRenderContext context, ChunkBuildBuffers buffers, CallbackInfo ci){
+        BakedModel model = context.model();
+        if(!(model instanceof BlockModelModifierBakedModel))
+            return;
+        WorldSlice level = context.world();
+        if(this.dummyRenderContext == null || this.dummyRenderContext.world() != level)
+            this.dummyRenderContext = new BlockRenderContext(level);
+        Vector3fc origin = context.origin();
+        this.dummyOrigin.set(origin.x(), origin.y(), origin.z());
+        BlockPos pos = context.pos();
+        BlockState state = context.state();
+        long seed = context.seed();
+        ModelData modelData = context.modelData();
+        RenderType renderType = context.renderLayer();
+        this.modelsByRandomOffset.setContext(pos, state.getOffset(level, pos));
+        try{
+            ((BlockModelModifierBakedModel)model).collectByOffset(this.modelsByRandomOffset, level, pos, state);
+            this.modelsByRandomOffset.foreach(
+                entry -> {
+                    this.dummyRenderContext.update(pos, this.dummyOrigin, state, entry, seed, modelData, renderType);
+                    this.renderModel(this.dummyRenderContext, buffers);
+                }
+            );
+        }finally{
+            this.modelsByRandomOffset.reset();
+        }
+        ci.cancel();
+    }
+
+    @ModifyVariable(
+        method = "renderModel",
+        at = @At(
+            value = "INVOKE",
+            target = "Lorg/embeddedt/embeddium/api/BlockRendererRegistry;fillCustomRenderers(Ljava/util/List;Lme/jellysquid/mods/sodium/client/render/chunk/compile/pipeline/BlockRenderContext;)V",
+            shift = At.Shift.BEFORE
+        ),
+        remap = false
+    )
+    private Vec3 modifyRandomOffset(Vec3 original, BlockRenderContext context, ChunkBuildBuffers buffers){
+        if(!(context.model() instanceof ModelsByRandomOffset.Entry entry))
+            return original;
+        Vector3fc offset = entry.getOffset();
+        return new Vec3(offset.x(), offset.y(), offset.z());
+    }
 
     @Inject(
         method = "getVertexColors",
--- src/main/java/com/supermartijn642/fusion/model/modifiers/block/BlockModelModifierBakedModel.java
+++ src/main/java/com/supermartijn642/fusion/model/modifiers/block/BlockModelModifierBakedModel.java
@@ -55,6 +55,36 @@ public class BlockModelModifierBakedModel extends WrappedBakedModel {
         this.ambientOcclusion = ambientOcclusion;
     }
 
+    public void collectByOffset(ModelsByRandomOffset output, @Nullable BlockAndTintGetter level, @Nullable BlockPos pos, @Nullable BlockState state){
+        // Check whether the breaking overlay is being rendered
+        boolean isBreakingOverlay = FusionClient.isRenderingBreakingOverlay();
+
+        // Default model
+        overrides:
+        {
+            for(ConditionalModel override : this.defaultModelOverrides){
+                if(override.conditions == null || override.conditions.test(level, pos, state)){
+                    if(!isBreakingOverlay || override.showBreakingOverlay)
+                        output.add(override.randomOffset, override.model);
+                    break overrides;
+                }
+            }
+            output.add(RandomOffsetFunction.MATCH_BLOCK, this.original);
+        }
+
+        // Append models
+        for(List<ConditionalModel> appendEntry : this.appendModels){
+            // First model whose conditions are met is submitted
+            for(ConditionalModel conditional : appendEntry){
+                if(conditional.conditions == null || conditional.conditions.test(level, pos, state)){
+                    if(!isBreakingOverlay || conditional.showBreakingOverlay)
+                        output.add(conditional.randomOffset, conditional.model);
+                    break;
+                }
+            }
+        }
+    }
+
     private RenderData getRenderData(@Nullable BlockAndTintGetter level, @Nullable BlockPos pos, @Nullable BlockState state, ModelData modelData){
         boolean hasAllArguments = level != null && pos != null && state != null;
 
@@ -223,7 +253,7 @@ public class BlockModelModifierBakedModel extends WrappedBakedModel {
         return keys;
     }
 
-    record ConditionalModel(BakedModel model, @Nullable BlockStateModelPredicate conditions, boolean showBreakingOverlay) {
+    record ConditionalModel(BakedModel model, @Nullable BlockStateModelPredicate conditions, boolean showBreakingOverlay, RandomOffsetFunction randomOffset) {
     }
 
     private record RenderData(int defaultModel, ModelData defaultModelData, int[] appendModels, ModelData[] appendModelsData) {

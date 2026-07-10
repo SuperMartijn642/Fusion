b3af760cb8190d603188a16a3fe9f2b04195fe1c
SuperMartijn624 <martijnvandenbrand@outlook.com>
2026-07-10 14:02:58 +0200

    Allow block model modifiers to overwrite block random offset

---
+++ src/main/java/com/supermartijn642/fusion/model/CombinedBakedModel.java
@@ -0,0 +1,138 @@
+package com.supermartijn642.fusion.model;
+
+import com.mojang.blaze3d.vertex.PoseStack;
+import net.minecraft.client.renderer.RenderType;
+import net.minecraft.client.renderer.block.model.BakedQuad;
+import net.minecraft.client.renderer.block.model.ItemOverrides;
+import net.minecraft.client.renderer.block.model.ItemTransforms;
+import net.minecraft.client.renderer.texture.TextureAtlasSprite;
+import net.minecraft.client.resources.model.BakedModel;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.util.RandomSource;
+import net.minecraft.world.item.ItemDisplayContext;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.BlockAndTintGetter;
+import net.minecraft.world.level.block.state.BlockState;
+import net.neoforged.neoforge.client.ChunkRenderTypeSet;
+import net.neoforged.neoforge.client.model.data.ModelData;
+import net.neoforged.neoforge.client.model.data.ModelProperty;
+import net.neoforged.neoforge.common.util.TriState;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created 08/07/2026 by SuperMartijn642
+ */
+public abstract class CombinedBakedModel implements BakedModel {
+
+    public static BakedModel of(List<BakedModel> models){
+        return new CombinedBakedModel() {
+            @Override
+            protected List<BakedModel> getModels(){
+                return models;
+            }
+        };
+    }
+
+    private static final ModelProperty<ModelData[]> SUB_MODEL_DATA = new ModelProperty<>();
+
+    protected abstract List<BakedModel> getModels();
+
+    @Override
+    public ModelData getModelData(BlockAndTintGetter level, BlockPos pos, BlockState state, ModelData modelData){
+        List<BakedModel> models = this.getModels();
+        ModelData[] subModelData = new ModelData[models.size()];
+        for(int i = 0; i < models.size(); i++)
+            subModelData[i] = models.get(i).getModelData(level, pos, state, modelData);
+        return ModelData.builder().with(SUB_MODEL_DATA, subModelData).build();
+    }
+
+    @Override
+    public List<BakedQuad> getQuads(@Nullable BlockState state, @Nullable Direction side, RandomSource randomSource, ModelData modelData, @Nullable RenderType renderType){
+        ModelData[] subModelData = modelData.get(SUB_MODEL_DATA);
+        List<BakedQuad> quads = new ArrayList<>();
+        List<BakedModel> models = this.getModels();
+        for(int i = 0; i < models.size(); i++)
+            quads.addAll(models.get(i).getQuads(state, side, randomSource, subModelData == null ? ModelData.EMPTY : subModelData[i], renderType));
+        return quads;
+    }
+
+    @Override
+    public List<BakedQuad> getQuads(@Nullable BlockState blockState, @Nullable Direction direction, RandomSource randomSource){
+        List<BakedQuad> quads = new ArrayList<>();
+        for(BakedModel model : this.getModels())
+            quads.addAll(model.getQuads(blockState, direction, randomSource));
+        return quads;
+    }
+
+    @Override
+    public boolean useAmbientOcclusion(){
+        return this.getModels().get(0).useAmbientOcclusion();
+    }
+
+    @Override
+    public boolean isGui3d(){
+        return this.getModels().get(0).isGui3d();
+    }
+
+    @Override
+    public boolean usesBlockLight(){
+        return this.getModels().get(0).usesBlockLight();
+    }
+
+    @Override
+    public TextureAtlasSprite getParticleIcon(){
+        return this.getModels().get(0).getParticleIcon();
+    }
+
+    @Override
+    public ItemTransforms getTransforms(){
+        return this.getModels().get(0).getTransforms();
+    }
+
+    @Override
+    public ItemOverrides getOverrides(){
+        return this.getModels().get(0).getOverrides();
+    }
+
+    @Override
+    public boolean isCustomRenderer(){
+        return this.getModels().get(0).isCustomRenderer();
+    }
+
+    @Override
+    public TriState useAmbientOcclusion(BlockState state, ModelData modelData, RenderType renderType){
+        ModelData[] subModelData = modelData.get(SUB_MODEL_DATA);
+        return this.getModels().get(0).useAmbientOcclusion(state, subModelData == null ? ModelData.EMPTY : subModelData[0], renderType);
+    }
+
+    @Override
+    public BakedModel applyTransform(ItemDisplayContext transformType, PoseStack poseStack, boolean applyLeftHandTransform){
+        return this.getModels().get(0).applyTransform(transformType, poseStack, applyLeftHandTransform);
+    }
+
+    @Override
+    public TextureAtlasSprite getParticleIcon(ModelData modelData){
+        ModelData[] subModelData = modelData.get(SUB_MODEL_DATA);
+        return this.getModels().get(0).getParticleIcon(subModelData == null ? ModelData.EMPTY : subModelData[0]);
+    }
+
+    @Override
+    public ChunkRenderTypeSet getRenderTypes(BlockState state, RandomSource rand, ModelData modelData){
+        ModelData[] subModelData = modelData.get(SUB_MODEL_DATA);
+        return this.getModels().get(0).getRenderTypes(state, rand, subModelData == null ? ModelData.EMPTY : subModelData[0]);
+    }
+
+    @Override
+    public List<RenderType> getRenderTypes(ItemStack stack, boolean fabulous){
+        return this.getModels().get(0).getRenderTypes(stack, fabulous);
+    }
+
+    @Override
+    public List<BakedModel> getRenderPasses(ItemStack stack, boolean fabulous){
+        return this.getModels().get(0).getRenderPasses(stack, fabulous);
+    }
+}

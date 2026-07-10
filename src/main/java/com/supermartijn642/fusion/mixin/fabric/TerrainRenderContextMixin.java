11310515603eab49af6b3c6f964e66e66ff35d8a
SuperMartijn624 <martijnvandenbrand@outlook.com>
2026-07-10 21:57:24 +0200

    Allow block model modifiers to overwrite block random offset

---
+++ src/main/java/com/supermartijn642/fusion/mixin/fabric/TerrainRenderContextMixin.java
@@ -0,0 +1,68 @@
+package com.supermartijn642.fusion.mixin.fabric;
+
+import com.mojang.blaze3d.vertex.PoseStack;
+import com.mojang.math.Vector3f;
+import com.supermartijn642.fusion.model.modifiers.block.BlockModelModifierBakedModel;
+import com.supermartijn642.fusion.model.modifiers.block.ModelsByRandomOffset;
+import net.fabricmc.fabric.impl.client.indigo.renderer.render.BlockRenderInfo;
+import net.fabricmc.fabric.impl.client.indigo.renderer.render.TerrainRenderContext;
+import net.minecraft.client.resources.model.BakedModel;
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.Vec3;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+/**
+ * Created 10/07/2026 by SuperMartijn642
+ */
+@Mixin(TerrainRenderContext.class)
+public abstract class TerrainRenderContextMixin {
+
+    @Final
+    @Shadow
+    private BlockRenderInfo blockInfo;
+
+    @Unique
+    private final ModelsByRandomOffset modelsByRandomOffset = new ModelsByRandomOffset();
+
+    @Shadow
+    private boolean tessellateBlock(BlockState state, BlockPos pos, BakedModel model, PoseStack poseStack){
+        throw new AssertionError();
+    }
+
+    @Inject(
+        method = "tessellateBlock",
+        at = @At("HEAD"),
+        cancellable = true
+    )
+    private void collectModelsByRandomOffset(BlockState state, BlockPos pos, BakedModel model, PoseStack poseStack, CallbackInfoReturnable<Boolean> ci){
+        if(!(model instanceof BlockModelModifierBakedModel))
+            return;
+        // Undo normal offset
+        Vec3 defaultOffset = state.getOffset(this.blockInfo.blockView, pos);
+        poseStack.translate(-defaultOffset.x, -defaultOffset.y, -defaultOffset.z);
+        // Render models with new offset
+        this.modelsByRandomOffset.setContext(pos, state.getOffset(this.blockInfo.blockView, pos));
+        try{
+            ((BlockModelModifierBakedModel)model).collectByOffset(this.modelsByRandomOffset, this.blockInfo.blockView, pos, state);
+            this.modelsByRandomOffset.foreach(
+                entry -> {
+                    poseStack.pushPose();
+                    Vector3f offset = entry.getOffset();
+                    poseStack.translate(offset.x(), offset.y(), offset.z());
+                    this.tessellateBlock(state, pos, entry, poseStack);
+                    poseStack.popPose();
+                }
+            );
+        }finally{
+            this.modelsByRandomOffset.reset();
+        }
+        ci.setReturnValue(false);
+    }
+}
---
+++ src/main/java/com/supermartijn642/fusion/mixin/indium/TerrainRenderContextMixinIndium.java
@@ -0,0 +1,62 @@
+package com.supermartijn642.fusion.mixin.indium;
+
+import com.mojang.math.Vector3f;
+import com.supermartijn642.fusion.model.modifiers.block.BlockModelModifierBakedModel;
+import com.supermartijn642.fusion.model.modifiers.block.ModelsByRandomOffset;
+import link.infra.indium.renderer.render.TerrainBlockRenderInfo;
+import link.infra.indium.renderer.render.TerrainRenderContext;
+import net.minecraft.client.resources.model.BakedModel;
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.Vec3;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+/**
+ * Created 10/07/2026 by SuperMartijn642
+ */
+@Mixin(TerrainRenderContext.class)
+public abstract class TerrainRenderContextMixinIndium {
+
+    @Final
+    @Shadow
+    private TerrainBlockRenderInfo blockInfo;
+    @Shadow
+    private Vec3 modelOffset;
+
+    @Unique
+    private final ModelsByRandomOffset modelsByRandomOffset = new ModelsByRandomOffset();
+
+    @Shadow
+    private boolean tessellateBlock(BlockState state, BlockPos pos, BlockPos origin, BakedModel model, Vec3 modelOffset){
+        throw new AssertionError();
+    }
+
+    @Inject(
+        method = "tessellateBlock",
+        at = @At("HEAD"),
+        cancellable = true
+    )
+    private void collectModelsByRandomOffset(BlockState state, BlockPos pos, BlockPos origin, BakedModel model, Vec3 modelOffset, CallbackInfoReturnable<Boolean> ci){
+        if(!(model instanceof BlockModelModifierBakedModel))
+            return;
+        this.modelsByRandomOffset.setContext(pos, state.getOffset(this.blockInfo.blockView, pos));
+        try{
+            ((BlockModelModifierBakedModel)model).collectByOffset(this.modelsByRandomOffset, this.blockInfo.blockView, pos, state);
+            this.modelsByRandomOffset.foreach(
+                entry -> {
+                    Vector3f offset = entry.getOffset();
+                    this.tessellateBlock(state, pos, origin, entry, new Vec3(offset.x(), offset.y(), offset.z()));
+                }
+            );
+        }finally{
+            this.modelsByRandomOffset.reset();
+        }
+        ci.setReturnValue(true);
+    }
+}

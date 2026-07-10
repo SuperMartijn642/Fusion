ae0ef6ed6ba02173d1570e056dee67219104e157
SuperMartijn624 <martijnvandenbrand@outlook.com>
2026-07-10 14:02:58 +0200

    Allow block model modifiers to overwrite block random offset

--- src/main/java/com/supermartijn642/fusion/mixin/ModelBlockRendererMixin.java
+++ src/main/java/com/supermartijn642/fusion/mixin/ModelBlockRendererMixin.java
@@ -1,26 +1,88 @@
 package com.supermartijn642.fusion.mixin;
 
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
+import com.llamalad7.mixinextras.sugar.Local;
 import com.mojang.blaze3d.vertex.PoseStack;
 import com.mojang.blaze3d.vertex.VertexConsumer;
 import com.supermartijn642.fusion.api.texture.SpriteHelper;
 import com.supermartijn642.fusion.api.texture.custom.TextureInstance;
 import com.supermartijn642.fusion.api.texture.types.base.BaseTextureData;
+import com.supermartijn642.fusion.model.modifiers.block.BlockModelModifierBakedModel;
+import com.supermartijn642.fusion.model.modifiers.block.ModelsByRandomOffset;
 import com.supermartijn642.fusion.texture.QuadTintingHelper;
+import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.block.ModelBlockRenderer;
 import net.minecraft.client.renderer.block.model.BakedQuad;
 import net.minecraft.client.renderer.texture.TextureAtlasSprite;
+import net.minecraft.client.resources.model.BakedModel;
 import net.minecraft.core.BlockPos;
+import net.minecraft.util.RandomSource;
 import net.minecraft.world.level.BlockAndTintGetter;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.Vec3;
+import net.neoforged.neoforge.client.model.data.ModelData;
+import org.joml.Vector3fc;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 /**
  * Created 07/09/2024 by SuperMartijn642
  */
 @Mixin(ModelBlockRenderer.class)
 public class ModelBlockRendererMixin {
+
+    @Unique
+    private final ThreadLocal<ModelsByRandomOffset> modelsByRandomOffset = ThreadLocal.withInitial(ModelsByRandomOffset::new);
+
+    @Shadow
+    private void tesselateBlock(BlockAndTintGetter level, BakedModel model, BlockState state, BlockPos pos, PoseStack poseStack, VertexConsumer buffer, boolean cull, RandomSource random, long seed, int overlay, ModelData entityData, RenderType renderType){
+        throw new AssertionError();
+    }
+
+    @Inject(
+        method = "tesselateBlock(Lnet/minecraft/world/level/BlockAndTintGetter;Lnet/minecraft/client/resources/model/BakedModel;Lnet/minecraft/world/level/block/state/BlockState;Lnet/minecraft/core/BlockPos;Lcom/mojang/blaze3d/vertex/PoseStack;Lcom/mojang/blaze3d/vertex/VertexConsumer;ZLnet/minecraft/util/RandomSource;JILnet/neoforged/neoforge/client/model/data/ModelData;Lnet/minecraft/client/renderer/RenderType;)V",
+        at = @At("HEAD"),
+        cancellable = true
+    )
+    private void collectModelsByRandomOffset(BlockAndTintGetter level, BakedModel model, BlockState state, BlockPos pos, PoseStack poseStack, VertexConsumer buffer, boolean cull, RandomSource random, long seed, int overlay, ModelData modelData, RenderType renderType, CallbackInfo ci){
+        if(!(model instanceof BlockModelModifierBakedModel))
+            return;
+        ModelsByRandomOffset modelsByRandomOffset = this.modelsByRandomOffset.get();
+        modelsByRandomOffset.setContext(pos, state.getOffset(pos));
+        try{
+            ((BlockModelModifierBakedModel)model).collectByOffset(modelsByRandomOffset, level, pos, state);
+            modelsByRandomOffset.foreach(
+                entry -> {
+                    poseStack.pushPose();
+                    this.tesselateBlock(level, entry, state, pos, poseStack, buffer, cull, random, seed, overlay, modelData, renderType);
+                    poseStack.popPose();
+                }
+            );
+        }finally{
+            modelsByRandomOffset.reset();
+        }
+        ci.cancel();
+    }
+
+    @ModifyExpressionValue(
+        method = "tesselateBlock(Lnet/minecraft/world/level/BlockAndTintGetter;Lnet/minecraft/client/resources/model/BakedModel;Lnet/minecraft/world/level/block/state/BlockState;Lnet/minecraft/core/BlockPos;Lcom/mojang/blaze3d/vertex/PoseStack;Lcom/mojang/blaze3d/vertex/VertexConsumer;ZLnet/minecraft/util/RandomSource;JILnet/neoforged/neoforge/client/model/data/ModelData;Lnet/minecraft/client/renderer/RenderType;)V",
+        at = @At(
+            value = "INVOKE",
+            target = "Lnet/minecraft/world/level/block/state/BlockState;getOffset(Lnet/minecraft/core/BlockPos;)Lnet/minecraft/world/phys/Vec3;"
+        )
+    )
+    private Vec3 modifyRandomOffset(Vec3 original, @Local BakedModel model){
+        if(model instanceof ModelsByRandomOffset.Entry entry){
+            Vector3fc offset = entry.getOffset();
+            return new Vec3(offset.x(), offset.y(), offset.z());
+        }
+        return original;
+    }
 
     @ModifyVariable(
         method = "putQuadData",

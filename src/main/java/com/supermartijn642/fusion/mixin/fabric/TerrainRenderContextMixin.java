1b1458014447e37898751523652d6523d95d2deb
SuperMartijn624 <martijnvandenbrand@outlook.com>
2026-07-10 14:02:58 +0200

    Allow block model modifiers to overwrite block random offset

---
+++ src/main/java/com/supermartijn642/fusion/mixin/fabric/TerrainRenderContextMixin.java
@@ -0,0 +1,74 @@
+package com.supermartijn642.fusion.mixin.fabric;
+
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
+import com.llamalad7.mixinextras.sugar.Local;
+import com.mojang.blaze3d.vertex.PoseStack;
+import com.supermartijn642.fusion.model.modifiers.block.BlockModelModifierBakedModel;
+import com.supermartijn642.fusion.model.modifiers.block.ModelsByRandomOffset;
+import net.fabricmc.fabric.impl.client.indigo.renderer.render.AbstractBlockRenderContext;
+import net.fabricmc.fabric.impl.client.indigo.renderer.render.TerrainRenderContext;
+import net.minecraft.client.resources.model.BakedModel;
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.Vec3;
+import org.joml.Vector3fc;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+/**
+ * Created 10/07/2026 by SuperMartijn642
+ */
+@Mixin(TerrainRenderContext.class)
+public abstract class TerrainRenderContextMixin extends AbstractBlockRenderContext {
+
+    @Unique
+    private final ModelsByRandomOffset modelsByRandomOffset = new ModelsByRandomOffset();
+
+    @Shadow
+    private void tessellateBlock(BlockState state, BlockPos pos, BakedModel model, PoseStack poseStack){
+        throw new AssertionError();
+    }
+
+    @Inject(
+        method = "tessellateBlock",
+        at = @At("HEAD"),
+        cancellable = true
+    )
+    private void collectModelsByRandomOffset(BlockState state, BlockPos pos, BakedModel model, PoseStack poseStack, CallbackInfo ci){
+        if(!(model instanceof BlockModelModifierBakedModel))
+            return;
+        this.modelsByRandomOffset.setContext(pos, state.getOffset(this.blockInfo.blockView, pos));
+        try{
+            ((BlockModelModifierBakedModel)model).collectByOffset(this.modelsByRandomOffset, this.blockInfo.blockView, pos, state);
+            this.modelsByRandomOffset.foreach(
+                entry -> {
+                    poseStack.pushPose();
+                    this.tessellateBlock(state, pos, entry, poseStack);
+                    poseStack.popPose();
+                }
+            );
+        }finally{
+            this.modelsByRandomOffset.reset();
+        }
+        ci.cancel();
+    }
+
+    @ModifyExpressionValue(
+        method = "tessellateBlock",
+        at = @At(
+            value = "INVOKE",
+            target = "Lnet/minecraft/world/level/block/state/BlockState;getOffset(Lnet/minecraft/world/level/BlockGetter;Lnet/minecraft/core/BlockPos;)Lnet/minecraft/world/phys/Vec3;"
+        )
+    )
+    private Vec3 modifyRandomOffset(Vec3 original, @Local BakedModel model){
+        if(model instanceof ModelsByRandomOffset.Entry entry){
+            Vector3fc offset = entry.getOffset();
+            return new Vec3(offset.x(), offset.y(), offset.z());
+        }
+        return original;
+    }
+}

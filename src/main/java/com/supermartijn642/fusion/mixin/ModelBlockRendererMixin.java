0fe12eaa07462b60cc8ce62069400de1402ea076
SuperMartijn624 <martijnvandenbrand@outlook.com>
2026-07-15 20:12:08 +0200

    Allow block model modifiers to overwrite block random offset

--- src/main/java/com/supermartijn642/fusion/mixin/ModelBlockRendererMixin.java
+++ src/main/java/com/supermartijn642/fusion/mixin/ModelBlockRendererMixin.java
@@ -2,25 +2,91 @@ package com.supermartijn642.fusion.mixin;
 
 import com.mojang.blaze3d.vertex.PoseStack;
 import com.mojang.blaze3d.vertex.VertexConsumer;
+import com.mojang.math.Vector3f;
 import com.supermartijn642.fusion.api.texture.SpriteHelper;
 import com.supermartijn642.fusion.api.texture.custom.TextureInstance;
 import com.supermartijn642.fusion.api.texture.types.base.BaseTextureData;
+import com.supermartijn642.fusion.model.modifiers.block.BlockModelModifierBakedModel;
+import com.supermartijn642.fusion.model.modifiers.block.ModelsByRandomOffset;
 import com.supermartijn642.fusion.texture.QuadTintingHelper;
 import net.minecraft.client.renderer.block.ModelBlockRenderer;
 import net.minecraft.client.renderer.block.model.BakedQuad;
 import net.minecraft.client.renderer.texture.TextureAtlasSprite;
+import net.minecraft.client.resources.model.BakedModel;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.level.BlockAndTintGetter;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.Vec3;
+import net.minecraftforge.client.model.data.IModelData;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
+
+import java.util.Random;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 /**
  * Created 07/09/2024 by SuperMartijn642
  */
 @Mixin(ModelBlockRenderer.class)
 public class ModelBlockRendererMixin {
+
+    @Unique
+    private final ThreadLocal<ModelsByRandomOffset> modelsByRandomOffset = ThreadLocal.withInitial(ModelsByRandomOffset::new);
+
+    @Shadow
+    private boolean tesselateBlock(BlockAndTintGetter level, BakedModel model, BlockState state, BlockPos pos, PoseStack poseStack, VertexConsumer buffer, boolean cull, Random random, long seed, int overlay, IModelData entityData){
+        throw new AssertionError();
+    }
+
+    @Inject(
+        method = "tesselateBlock(Lnet/minecraft/world/level/BlockAndTintGetter;Lnet/minecraft/client/resources/model/BakedModel;Lnet/minecraft/world/level/block/state/BlockState;Lnet/minecraft/core/BlockPos;Lcom/mojang/blaze3d/vertex/PoseStack;Lcom/mojang/blaze3d/vertex/VertexConsumer;ZLjava/util/Random;JILnet/minecraftforge/client/model/data/IModelData;)Z",
+        at = @At("HEAD"),
+        cancellable = true,
+        remap = false
+    )
+    private void collectModelsByRandomOffset(BlockAndTintGetter level, BakedModel model, BlockState state, BlockPos pos, PoseStack poseStack, VertexConsumer buffer, boolean cull, Random random, long seed, int overlay, IModelData modelData, CallbackInfoReturnable<Boolean> ci){
+        if(!(model instanceof BlockModelModifierBakedModel))
+            return;
+        ModelsByRandomOffset modelsByRandomOffset = this.modelsByRandomOffset.get();
+        AtomicBoolean rendered = new AtomicBoolean(false);
+        modelsByRandomOffset.setContext(pos, state.getOffset(level, pos));
+        try{
+            ((BlockModelModifierBakedModel)model).collectByOffset(modelsByRandomOffset, level, pos, state);
+            modelsByRandomOffset.foreach(
+                entry -> {
+                    poseStack.pushPose();
+                    if(this.tesselateBlock(level, entry, state, pos, poseStack, buffer, cull, random, seed, overlay, modelData))
+                        rendered.set(true);
+                    poseStack.popPose();
+                }
+            );
+        }finally{
+            modelsByRandomOffset.reset();
+        }
+        ci.setReturnValue(rendered.get());
+    }
+
+    @Inject(
+        method = "tesselateBlock(Lnet/minecraft/world/level/BlockAndTintGetter;Lnet/minecraft/client/resources/model/BakedModel;Lnet/minecraft/world/level/block/state/BlockState;Lnet/minecraft/core/BlockPos;Lcom/mojang/blaze3d/vertex/PoseStack;Lcom/mojang/blaze3d/vertex/VertexConsumer;ZLjava/util/Random;JILnet/minecraftforge/client/model/data/IModelData;)Z",
+        at = @At(
+            value = "INVOKE_ASSIGN",
+            target = "Lnet/minecraft/world/level/block/state/BlockState;getOffset(Lnet/minecraft/world/level/BlockGetter;Lnet/minecraft/core/BlockPos;)Lnet/minecraft/world/phys/Vec3;"
+        ),
+        locals = LocalCapture.CAPTURE_FAILHARD,
+        remap = false
+    )
+    private void modifyRandomOffset(BlockAndTintGetter level, BakedModel model, BlockState state, BlockPos pos, PoseStack poseStack, VertexConsumer buffer, boolean cull, Random random, long seed, int overlay, IModelData modelData, CallbackInfoReturnable<Boolean> ci, boolean ambientOcclusion, Vec3 defaultOffset){
+        if(!(model instanceof ModelsByRandomOffset.Entry entry))
+            return;
+        Vector3f offset = entry.getOffset();
+        poseStack.translate(offset.x() - defaultOffset.x(), offset.y() - defaultOffset.y(), offset.z() - defaultOffset.z());
+    }
 
     @ModifyVariable(
         method = "putQuadData",
---
+++ src/main/java/com/supermartijn642/fusion/model/CombinedBakedModel.java
@@ -0,0 +1,149 @@
+package com.supermartijn642.fusion.model;
+
+import com.mojang.blaze3d.vertex.PoseStack;
+import com.mojang.datafixers.util.Pair;
+import net.minecraft.client.renderer.RenderType;
+import net.minecraft.client.renderer.block.model.BakedQuad;
+import net.minecraft.client.renderer.block.model.ItemOverrides;
+import net.minecraft.client.renderer.block.model.ItemTransforms;
+import net.minecraft.client.renderer.texture.TextureAtlasSprite;
+import net.minecraft.client.resources.model.BakedModel;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.BlockAndTintGetter;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraftforge.client.model.data.EmptyModelData;
+import net.minecraftforge.client.model.data.IModelData;
+import net.minecraftforge.client.model.data.ModelDataMap;
+import net.minecraftforge.client.model.data.ModelProperty;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+/**
+ * Created 08/07/2026 by SuperMartijn642
+ */
+public abstract class CombinedBakedModel implements BakedModel, CustomRenderTypeBakedModel {
+
+    public static BakedModel of(List<BakedModel> models){
+        return new CombinedBakedModel() {
+            @Override
+            protected List<BakedModel> getModels(){
+                return models;
+            }
+        };
+    }
+
+    private static final ModelProperty<IModelData[]> SUB_MODEL_DATA = new ModelProperty<>();
+
+    protected abstract List<BakedModel> getModels();
+
+    @Override
+    public IModelData getModelData(BlockAndTintGetter level, BlockPos pos, BlockState state, IModelData modelData){
+        List<BakedModel> models = this.getModels();
+        IModelData[] subModelData = new IModelData[models.size()];
+        for(int i = 0; i < models.size(); i++)
+            subModelData[i] = models.get(i).getModelData(level, pos, state, modelData);
+        return new ModelDataMap.Builder().withInitial(SUB_MODEL_DATA, subModelData).build();
+    }
+
+    @Override
+    public @NotNull List<BakedQuad> getQuads(@Nullable BlockState state, @Nullable Direction side, Random random, IModelData modelData){
+        IModelData[] subModelData = modelData.getData(SUB_MODEL_DATA);
+        List<BakedQuad> quads = new ArrayList<>();
+        List<BakedModel> models = this.getModels();
+        for(int i = 0; i < models.size(); i++)
+            quads.addAll(models.get(i).getQuads(state, side, random, subModelData == null ? EmptyModelData.INSTANCE : subModelData[i]));
+        return quads;
+    }
+
+    @Override
+    public List<BakedQuad> getQuads(@Nullable BlockState blockState, @Nullable Direction direction, Random random){
+        List<BakedQuad> quads = new ArrayList<>();
+        for(BakedModel model : this.getModels())
+            quads.addAll(model.getQuads(blockState, direction, random));
+        return quads;
+    }
+
+    @Override
+    public boolean canRenderInLayer(BlockState state, RenderType renderType){
+        // Check whether the render type is a default one for the state
+        boolean isDefaultRenderType = ModelRenderTypeHelper.couldBlockRenderInLayerOriginally(state, renderType);
+        // Check models
+        for(BakedModel model : this.getModels()){
+            if(ModelRenderTypeHelper.canRenderInLayer(model, state, renderType, isDefaultRenderType))
+                return true;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean useAmbientOcclusion(){
+        return this.getModels().get(0).useAmbientOcclusion();
+    }
+
+    @Override
+    public boolean isGui3d(){
+        return this.getModels().get(0).isGui3d();
+    }
+
+    @Override
+    public boolean usesBlockLight(){
+        return this.getModels().get(0).usesBlockLight();
+    }
+
+    @Override
+    public TextureAtlasSprite getParticleIcon(){
+        return this.getModels().get(0).getParticleIcon();
+    }
+
+    @Override
+    public ItemTransforms getTransforms(){
+        return this.getModels().get(0).getTransforms();
+    }
+
+    @Override
+    public ItemOverrides getOverrides(){
+        return this.getModels().get(0).getOverrides();
+    }
+
+    @Override
+    public boolean isCustomRenderer(){
+        return this.getModels().get(0).isCustomRenderer();
+    }
+
+    @Override
+    public boolean isAmbientOcclusion(BlockState state){
+        return this.getModels().get(0).isAmbientOcclusion(state);
+    }
+
+    @Override
+    public boolean doesHandlePerspectives(){
+        return this.getModels().get(0).doesHandlePerspectives();
+    }
+
+    @Override
+    public BakedModel handlePerspective(ItemTransforms.TransformType displayContext, PoseStack poseStack){
+        return this.getModels().get(0).handlePerspective(displayContext, poseStack);
+    }
+
+    @Override
+    public TextureAtlasSprite getParticleIcon(IModelData modelData){
+        IModelData[] subModelData = modelData.getData(SUB_MODEL_DATA);
+        return this.getModels().get(0).getParticleIcon(subModelData == null ? EmptyModelData.INSTANCE : subModelData[0]);
+    }
+
+    @Override
+    public boolean isLayered(){
+        return this.getModels().get(0).isLayered();
+    }
+
+    @Override
+    public List<Pair<BakedModel,RenderType>> getLayerModels(ItemStack stack, boolean fabulous){
+        return this.getModels().get(0).getLayerModels(stack, fabulous);
+    }
+}

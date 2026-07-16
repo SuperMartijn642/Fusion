3ea3553f82a043e95713217e16804dbeba430f8a
SuperMartijn624 <martijnvandenbrand@outlook.com>
2026-07-16 20:57:51 +0200

    Reduce `MutableQuadImpl` memory footprint

--- src/main/java/com/supermartijn642/fusion/api/model/custom/quad/QuadAccess.java
+++ src/main/java/com/supermartijn642/fusion/api/model/custom/quad/QuadAccess.java
@@ -6,6 +6,7 @@ import net.minecraft.client.renderer.texture.TextureAtlasSprite;
 import net.minecraft.core.Direction;
 import org.jetbrains.annotations.ApiStatus;
 import org.jetbrains.annotations.Nullable;
+import org.joml.Vector3f;
 import org.joml.Vector3fc;
 
 /**
@@ -27,6 +28,11 @@ public interface QuadAccess {
      * Creates a baked quad instance from the properties.
      */
     BakedQuad toBakedQuad();
+
+    /**
+     * Position of the given vertex.
+     */
+    Vector3fc copyPosition(int vertexIndex, @Nullable Vector3f dest);
 
     /**
      * Position of the given vertex.

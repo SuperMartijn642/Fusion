1091cc02684918caa525a48b5aadb3616fd878b1
SuperMartijn624 <martijnvandenbrand@outlook.com>
2026-07-16 17:23:40 +0200

    Account for floating-point inaccuracies in `MatchBlockBlockStatePredicate#transformOffsets`

--- src/main/java/com/supermartijn642/fusion/model/predicates/blockstate/MatchBlockBlockStatePredicate.java
+++ src/main/java/com/supermartijn642/fusion/model/predicates/blockstate/MatchBlockBlockStatePredicate.java
@@ -277,9 +277,9 @@ public class MatchBlockBlockStatePredicate implements BlockStateModelPredicate {
                     newOffset.translate(dummy.x, dummy.y, dummy.z);
                 }
                 return new BlockPos(
-                    (int)Math.signum(newOffset.x),
-                    (int)Math.signum(newOffset.y),
-                    (int)Math.signum(newOffset.z)
+                    Math.abs(newOffset.x) < 1e-7 ? 0 : newOffset.x < 0 ? -1 : 1,
+                    Math.abs(newOffset.y) < 1e-7 ? 0 : newOffset.y < 0 ? -1 : 1,
+                    Math.abs(newOffset.z) < 1e-7 ? 0 : newOffset.z < 0 ? -1 : 1
                 );
             })
             .collect(Collectors.toList());

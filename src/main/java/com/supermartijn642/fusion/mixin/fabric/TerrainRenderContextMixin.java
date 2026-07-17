abc57ad02d7e742c580e2f3e6e68517206f1adc2
SuperMartijn624 <martijnvandenbrand@outlook.com>
2026-07-17 13:11:11 +0200

    Add mixin class constructors to avoid stupid Mixin bug #269

--- src/main/java/com/supermartijn642/fusion/mixin/fabric/TerrainRenderContextMixin.java
+++ src/main/java/com/supermartijn642/fusion/mixin/fabric/TerrainRenderContextMixin.java
@@ -28,6 +28,9 @@ public abstract class TerrainRenderContextMixin extends AbstractBlockRenderConte
     @Unique
     private final ModelsByRandomOffset modelsByRandomOffset = new ModelsByRandomOffset();
 
+    private TerrainRenderContextMixin(){
+    }
+
     @Shadow
     private void tessellateBlock(BlockState state, BlockPos pos, BakedModel model, PoseStack poseStack){
         throw new AssertionError();

24f9e6b3eb8a14b2f09e58206a0c1c27b9e7a706
SuperMartijn624 <martijnvandenbrand@outlook.com>
2026-07-17 13:13:02 +0200

    Add mixin class constructors to avoid stupid Mixin bug #269

--- src/main/java/com/supermartijn642/fusion/mixin/indium/NonTerrainBlockRenderContextMixinIndium.java
+++ src/main/java/com/supermartijn642/fusion/mixin/indium/NonTerrainBlockRenderContextMixinIndium.java
@@ -30,6 +30,9 @@ public class NonTerrainBlockRenderContextMixinIndium {
     @Unique
     private final ModelsByRandomOffset modelsByRandomOffset = new ModelsByRandomOffset();
 
+    private NonTerrainBlockRenderContextMixinIndium(){
+    }
+
     @Shadow
     private void render(BlockAndTintGetter level, BakedModel model, BlockState state, BlockPos pos, PoseStack poseStack, VertexConsumer buffer, boolean cull, RandomSource random, long seed, int overlay){
         throw new AssertionError();

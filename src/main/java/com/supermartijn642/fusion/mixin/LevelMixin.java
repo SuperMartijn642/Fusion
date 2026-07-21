aa5ffb46a9aeea8392012433e2b5d85a46b68172
SuperMartijn624 <martijnvandenbrand@outlook.com>
2026-07-21 13:04:41 +0200

    Fix `dimension` block state model predicate not being able to get dimension for render regions

---
+++ src/main/java/com/supermartijn642/fusion/mixin/LevelMixin.java
@@ -0,0 +1,20 @@
+package com.supermartijn642.fusion.mixin;
+
+import com.supermartijn642.fusion.util.Dimensional;
+import net.minecraft.resources.Identifier;
+import net.minecraft.world.level.Level;
+import org.spongepowered.asm.mixin.Mixin;
+
+/**
+ * Created 21/07/2026 by SuperMartijn642
+ */
+@Mixin(Level.class)
+public class LevelMixin implements Dimensional {
+
+    @Override
+    public Identifier fusionGetDimension(){
+        //noinspection DataFlowIssue
+        Level self = (Level)(Object)this;
+        return self.dimension().identifier();
+    }
+}

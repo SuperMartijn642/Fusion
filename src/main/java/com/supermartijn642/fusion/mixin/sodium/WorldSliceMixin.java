ee99cdf328898b9dc4ed5241ba1264130ce154a5
SuperMartijn624 <martijnvandenbrand@outlook.com>
2026-07-21 13:04:41 +0200

    Fix `dimension` block state model predicate not being able to get dimension for render regions

---
+++ src/main/java/com/supermartijn642/fusion/mixin/sodium/WorldSliceMixin.java
@@ -0,0 +1,25 @@
+package com.supermartijn642.fusion.mixin.sodium;
+
+import com.supermartijn642.fusion.util.Dimensional;
+import me.jellysquid.mods.sodium.client.world.WorldSlice;
+import net.minecraft.client.multiplayer.ClientLevel;
+import net.minecraft.resources.ResourceLocation;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+
+/**
+ * Created 21/07/2026 by SuperMartijn642
+ */
+@Mixin(WorldSlice.class)
+public class WorldSliceMixin implements Dimensional {
+
+    @Final
+    @Shadow
+    private ClientLevel world;
+
+    @Override
+    public ResourceLocation fusionGetDimension(){
+        return this.world.dimension().location();
+    }
+}

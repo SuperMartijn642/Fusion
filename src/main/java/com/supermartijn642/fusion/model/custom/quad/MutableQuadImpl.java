a75f547e7d81427ad74b12f235a7ed2a0054891d
SuperMartijn624 <martijnvandenbrand@outlook.com>
2026-07-28 20:13:49 +0200

    Switch translucent item render type based on `fabulous` argument

--- src/main/java/com/supermartijn642/fusion/model/custom/quad/MutableQuadImpl.java
+++ src/main/java/com/supermartijn642/fusion/model/custom/quad/MutableQuadImpl.java
@@ -32,7 +32,7 @@ public class MutableQuadImpl implements MutableQuad {
         return new MutableQuadImpl();
     }
 
-    static {
+    static{
         if(ChunkRenderTypeHelper.all().size() > 31)
             throw new AssertionError("More than 31 chunk render types!");
     }
@@ -258,11 +258,13 @@ public class MutableQuadImpl implements MutableQuad {
     }
 
     @Override
-    public RenderType itemRenderType(){
+    public RenderType itemRenderType(boolean fabulous){
         RenderType chunkRenderType = this.chunkRenderType();
         if(chunkRenderType == null)
             return null;
-        return chunkRenderType == RenderType.translucent() ? Sheets.translucentCullBlockSheet() : Sheets.cutoutBlockSheet();
+        return chunkRenderType == RenderType.translucent() ?
+            fabulous ? Sheets.translucentCullBlockSheet() : Sheets.translucentItemSheet() :
+            Sheets.cutoutBlockSheet();
     }
 
     @Override

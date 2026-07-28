601fe8b2430f4b30003e72f3cd1615fbb58209d9
SuperMartijn624 <martijnvandenbrand@outlook.com>
2026-07-28 20:13:49 +0200

    Switch translucent item render type based on `fabulous` argument

--- src/main/java/com/supermartijn642/fusion/model/types/base/BaseBakedModel.java
+++ src/main/java/com/supermartijn642/fusion/model/types/base/BaseBakedModel.java
@@ -230,7 +230,7 @@ public class BaseBakedModel implements BakedModel {
         if(stack.getItem() instanceof BlockItem && !ItemBlockRenderTypes.getRenderLayers(((BlockItem)stack.getItem()).getBlock().defaultBlockState()).contains(RenderType.translucent()))
             defaultRenderType = Sheets.cutoutBlockSheet();
         else
-            defaultRenderType = Sheets.translucentItemSheet();
+            defaultRenderType = fabulous ? Sheets.translucentCullBlockSheet() : Sheets.translucentItemSheet();
 
         PropertyStore propertyStore = FallbackPropertyStore.create(this.propertyStore);
 
@@ -239,7 +239,7 @@ public class BaseBakedModel implements BakedModel {
         List<List<BakedQuad>> quadsByRenderType = new ArrayList<>(4);
         Consumer<QuadAccess> submitter = quad -> {
             // Get render type
-            RenderType renderType = quad.itemRenderType();
+            RenderType renderType = quad.itemRenderType(fabulous);
             if(renderType == null)
                 renderType = defaultRenderType;
             // Get or quad list

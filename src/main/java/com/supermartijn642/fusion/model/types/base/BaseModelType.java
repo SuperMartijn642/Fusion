26449bd5c66f2f0ad3f10f738ae8a6a11269741c
SuperMartijn624 <martijnvandenbrand@outlook.com>
2026-07-28 20:56:43 +0200

    Fix item transform translation not being scaled to 1/16th in `BaseModelType#deserializeItemTransform`

--- src/main/java/com/supermartijn642/fusion/model/types/base/BaseModelType.java
+++ src/main/java/com/supermartijn642/fusion/model/types/base/BaseModelType.java
@@ -19,6 +19,7 @@ import com.supermartijn642.fusion.util.IdentifierUtil;
 import net.minecraft.client.renderer.block.model.*;
 import net.minecraft.core.Direction;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.util.Mth;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.*;
@@ -510,16 +511,22 @@ public abstract class BaseModelType<T extends BaseModelData, BUILDER extends Bas
         if(!json.get(type.getSerializeName()).isJsonObject())
             throw new JsonParseException("Display entry '" + type.getSerializeName() + "' must be an object!");
         JsonObject object = json.getAsJsonObject(type.getSerializeName());
+        Vector3f rotation = object.has("rotation") ?
+            this.deserializeVector3f(object.get("rotation"), () -> "Display '" + type.getSerializeName() + "' property 'rotation'") :
+            ItemTransform.Deserializer.DEFAULT_ROTATION.copy();
+        Vector3f translation = object.has("translation") ?
+            this.deserializeVector3f(object.get("translation"), () -> "Display '" + type.getSerializeName() + "' property 'translation'") :
+            ItemTransform.Deserializer.DEFAULT_TRANSLATION.copy();
+        translation.mul(1 / 16f);
+        translation.set(Mth.clamp(translation.x(), -5, 5), Mth.clamp(translation.y(), -5, 5), Mth.clamp(translation.z(), -5, 5));
+        Vector3f scale = object.has("scale") ?
+            this.deserializeVector3f(object.get("scale"), () -> "Display '" + type.getSerializeName() + "' property 'scale'") :
+            ItemTransform.Deserializer.DEFAULT_SCALE.copy();
+        scale.set(Mth.clamp(scale.x(), -4, 4), Mth.clamp(scale.y(), -4, 4), Mth.clamp(scale.z(), -4, 4));
         return new ItemTransform(
-            object.has("rotation") ?
-                this.deserializeVector3f(object.get("rotation"), () -> "Display '" + type.getSerializeName() + "' property 'rotation'") :
-                ItemTransform.Deserializer.DEFAULT_ROTATION,
-            object.has("translation") ?
-                this.deserializeVector3f(object.get("translation"), () -> "Display '" + type.getSerializeName() + "' property 'translation'") :
-                ItemTransform.Deserializer.DEFAULT_TRANSLATION,
-            object.has("scale") ?
-                this.deserializeVector3f(object.get("scale"), () -> "Display '" + type.getSerializeName() + "' property 'scale'") :
-                ItemTransform.Deserializer.DEFAULT_SCALE
+            rotation,
+            translation,
+            scale
         );
     }
 

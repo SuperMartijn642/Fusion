6d0e6b10db98a46f6e44467412c7a64228e84a08
SuperMartijn624 <martijnvandenbrand@outlook.com>
2026-07-29 15:29:53 +0200

    Store chunk render type of `MutableQuadImpl` in flags rather than by reference

--- src/main/java/com/supermartijn642/fusion/util/ChunkRenderTypeHelper.java
+++ src/main/java/com/supermartijn642/fusion/util/ChunkRenderTypeHelper.java
@@ -1,23 +1,58 @@
 package com.supermartijn642.fusion.util;
 
-import com.google.common.collect.ImmutableSet;
+import com.google.common.collect.ImmutableMap;
 import net.minecraft.client.renderer.RenderType;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.List;
-import java.util.Set;
+import java.util.Map;
 
 /**
  * Created 24/05/2026 by SuperMartijn642
  */
 public class ChunkRenderTypeHelper {
 
-    private static final Set<RenderType> CHUNK_RENDER_TYPE_SET = ImmutableSet.copyOf(allChunkRenderTypes());
+    private static final List<RenderType> RENDER_TYPES = RenderType.chunkBufferLayers();
+    private static final int COUNT;
+    private static final Map<RenderType,Integer> TO_ID;
+    private static final RenderType[] BY_ID;
 
-    public static List<RenderType> allChunkRenderTypes(){
+    static{
+        List<RenderType> renderTypes = ChunkRenderTypeHelper.all();
+        COUNT = renderTypes.size() + 1;
+        ImmutableMap.Builder<RenderType,Integer> renderTypeToId = ImmutableMap.builder();
+        BY_ID = new RenderType[renderTypes.size() + 1];
+        for(int i = 0; i < renderTypes.size(); i++){
+            RenderType renderType = renderTypes.get(i);
+            renderTypeToId.put(renderType, i + 1);
+            BY_ID[i + 1] = renderType;
+        }
+        TO_ID = renderTypeToId.build();
+    }
+
+    public static List<RenderType> all(){
         return RenderType.chunkBufferLayers();
     }
 
     public static boolean isChunkRenderType(RenderType chunkRenderType){
-        return CHUNK_RENDER_TYPE_SET.contains(chunkRenderType);
+        return RENDER_TYPES.contains(chunkRenderType);
+    }
+
+    public static int getCount(){
+        return COUNT;
+    }
+
+    public static int getId(@Nullable RenderType renderType){
+        if(renderType == null)
+            return 0;
+        Integer id = TO_ID.get(renderType);
+        if(id == null)
+            throw new IllegalArgumentException("Key must be a chunk render type!");
+        return id;
+    }
+
+    @Nullable
+    public static RenderType byId(int id){
+        return BY_ID[id];
     }
 }

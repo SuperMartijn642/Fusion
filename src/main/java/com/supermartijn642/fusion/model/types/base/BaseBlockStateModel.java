01818755247731bb33689e6dfedd3d009760a0f1
SuperMartijn624 <martijnvandenbrand@outlook.com>
2026-08-17 14:21:05 +0200

    Mark `BaseBakedModel#Quad` fields as nullable

--- src/main/java/com/supermartijn642/fusion/model/types/base/BaseBlockStateModel.java
+++ src/main/java/com/supermartijn642/fusion/model/types/base/BaseBlockStateModel.java
@@ -186,6 +186,6 @@ public class BaseBlockStateModel implements BlockStateModel {
         }
     }
 
-    public record Quad(QuadAccess quad, SpriteInstance sprite, BlockStateQuadProcessor<Object> processor) {
+    public record Quad(QuadAccess quad, @Nullable SpriteInstance sprite, @Nullable BlockStateQuadProcessor<Object> processor) {
     }
 }

b2c1d8dfaf42d200f0cb49346ca611ee1e737852
SuperMartijn624 <martijnvandenbrand@outlook.com>
2026-08-19 16:18:49 +0200

    Store parent and sub-textures in `TextureInstanceImpl`

--- src/main/java/com/supermartijn642/fusion/texture/TextureCreationHandler.java
+++ src/main/java/com/supermartijn642/fusion/texture/TextureCreationHandler.java
@@ -129,9 +129,11 @@ public class TextureCreationHandler {
     private static TextureInstance<?> createTextureInstance(DummyTextureSpriteContents contents, AtlasTexture textureAtlas, int atlasWidth, int atlasHeight, int mipmapLevels, Consumer<TextureAtlasSprite> spriteOutput){
         // Create sub-textures
         TextureInstance<?> defaultSubTexture = null;
+        List<TextureInstance<?>> subTextures = new ArrayList<>(contents.getSubTextures().size());
         for(DummyTextureSpriteContents subTexture : contents.getSubTextures()){
             try{
                 TextureInstance<?> textureInstance = createTextureInstance(subTexture, textureAtlas, atlasWidth, atlasHeight, mipmapLevels, spriteOutput);
+                subTextures.add(textureInstance);
                 if(subTexture.getTextureOutput().isMarkedDefault())
                     defaultSubTexture = textureInstance;
             }catch(Exception e){
@@ -145,8 +147,11 @@ public class TextureCreationHandler {
         TextureInstanceImpl<?> textureInstance = new TextureInstanceImpl(
             textureOutput.getTextureType(),
             textureOutput.getIdentifier(),
-            textureOutput.getCustomData()
+            textureOutput.getCustomData(),
+            subTextures
         );
+        for(TextureInstance<?> subTexture : subTextures)
+            ((TextureInstanceImpl<?>)subTexture).setParent(textureInstance);
 
         // Create the custom sprites
         SpriteInstance defaultSprite = null;

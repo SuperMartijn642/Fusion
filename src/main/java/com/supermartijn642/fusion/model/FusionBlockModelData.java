8f7f822c42d5684d98dd823514c9804d4063d1e7
SuperMartijn624 <martijnvandenbrand@outlook.com>
2026-08-20 17:17:31 +0200

    Use an empty model instead of missing model when model has no geometry

--- src/main/java/com/supermartijn642/fusion/model/FusionBlockModelData.java
+++ src/main/java/com/supermartijn642/fusion/model/FusionBlockModelData.java
@@ -93,8 +93,17 @@ public class FusionBlockModelData {
             else
                 throw new RuntimeException("Encountered an exception while baking untyped block model for '" + this.identifier + "'!", e);
         }
-        if(model == null)
-            model = context.getMissingBlockStateModel();
+        if(model == null){
+            // Get particle texture
+            TextureAtlasSprite particleSprite = ModelGeometry.MaterialKeyResolver.fromKeyLookup(
+                key -> ModelStack.empty().push(this.model, this.identifier).findMaterialIncludingParents(key, context),
+                context::getMaterial,
+                s -> {},
+                s -> {}
+            ).get("particle", false);
+            // Create empty model
+            model = new EmptyBlockStateModel(particleSprite);
+        }
         // Log warnings
         if(!warnings.isEmpty())
             LoggingHelper.logUserWarnings(warnings, "Warnings for block model '%s':", this.identifier);
@@ -131,7 +140,7 @@ public class FusionBlockModelData {
                 throw new RuntimeException("Encountered an exception while baking untyped item model for '" + this.identifier + "'!", e);
         }
         if(model == null)
-            model = context.getMissingItemModel();
+            model = EmptyItemModel.INSTANCE;
         // Log warnings
         if(!warnings.isEmpty())
             LoggingHelper.logUserWarnings(warnings, "Warnings for item model '%s':", this.identifier);

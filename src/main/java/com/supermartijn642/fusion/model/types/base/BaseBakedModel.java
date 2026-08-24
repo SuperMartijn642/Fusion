feb1773f3c46e9b360626f5dab7fe4b85d3dc3af
SuperMartijn624 <martijnvandenbrand@outlook.com>
2026-08-24 06:20:36 +0200

    Account for Forge/NeoForge model `render_type` property when collecting model render types

--- src/main/java/com/supermartijn642/fusion/model/types/base/BaseBakedModel.java
+++ src/main/java/com/supermartijn642/fusion/model/types/base/BaseBakedModel.java
@@ -75,6 +75,8 @@ public class BaseBakedModel implements BakedModel {
         ChunkRenderTypeSet renderTypes = ChunkRenderTypeSet.none();
         for(Direction cullDirection : CullingHelper.cullDirections()){
             for(Quad quad : quads.get(cullDirection)){
+                if(quad.quad.chunkRenderType() != null)
+                    renderTypes = ChunkRenderTypeSet.union(renderTypes, ChunkRenderTypeSet.of(quad.quad.chunkRenderType()));
                 SpriteInstance sprite = quad.sprite;
                 if(sprite == null)
                     continue;

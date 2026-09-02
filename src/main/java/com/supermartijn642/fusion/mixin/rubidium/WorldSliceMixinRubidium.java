3e9ba3a6a031c5c90f9b9a2bdb046de5dae901d7
SuperMartijn624 <martijnvandenbrand@outlook.com>
2026-09-03 01:01:33 +0200

    Add `BiomeGetter` so we can obtain biome for level wrappers

--- src/main/java/com/supermartijn642/fusion/mixin/rubidium/WorldSliceMixinRubidium.java
+++ src/main/java/com/supermartijn642/fusion/mixin/rubidium/WorldSliceMixinRubidium.java
@@ -1,9 +1,12 @@
 package com.supermartijn642.fusion.mixin.rubidium;
 
+import com.supermartijn642.fusion.util.BiomeGetter;
 import com.supermartijn642.fusion.util.Dimensional;
 import me.jellysquid.mods.sodium.client.world.WorldSlice;
 import net.minecraft.client.multiplayer.ClientLevel;
+import net.minecraft.core.BlockPos;
 import net.minecraft.resources.ResourceLocation;
+import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -12,7 +15,7 @@ import org.spongepowered.asm.mixin.Shadow;
  * Created 21/07/2026 by SuperMartijn642
  */
 @Mixin(WorldSlice.class)
-public class WorldSliceMixinRubidium implements Dimensional {
+public class WorldSliceMixinRubidium implements Dimensional, BiomeGetter {
 
     @Final
     @Shadow
@@ -21,5 +24,10 @@ public class WorldSliceMixinRubidium implements Dimensional {
     @Override
     public ResourceLocation fusionGetDimension(){
         return this.world.dimension().location();
+    }
+
+    @Override
+    public @Nullable ResourceLocation fusionGetBiome(BlockPos pos){
+        return ((BiomeGetter)this.world).fusionGetBiome(pos);
     }
 }

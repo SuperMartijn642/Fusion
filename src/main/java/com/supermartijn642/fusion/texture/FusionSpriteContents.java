9dfc6572de27afcb12f9ad933e855901bba83095
SuperMartijn624 <martijnvandenbrand@outlook.com>
2026-09-06 14:27:41 +0200

    Fix frame index used when uploading animated texture atlas sprites #305

--- src/main/java/com/supermartijn642/fusion/texture/FusionSpriteContents.java
+++ src/main/java/com/supermartijn642/fusion/texture/FusionSpriteContents.java
@@ -79,8 +79,7 @@ public class FusionSpriteContents extends SpriteContents {
     protected @Nullable AnimatedTexture createAnimatedTexture(FrameSize frameSize, int imageWidth, int imageHeight, AnimationMetadataSection animationMetadataSection){
         return new AnimatedTexture(this.frameInfos, 0, this.imageSource.shouldInterpolateFrames()) {
             @Override
-            protected void uploadFrame(int destinationX, int destinationY, int frame, GpuTexture destination){
-                int uniqueFrame = this.frames.get(frame).index();
+            protected void uploadFrame(int destinationX, int destinationY, int uniqueFrame, GpuTexture destination){
                 FusionSpriteContents.this.uploadUniqueFrame(destinationX, destinationY, destination, uniqueFrame);
             }
 
